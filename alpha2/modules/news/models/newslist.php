2edfcac538598a185ac3b54fc55191df968755f8
Jakub Matas <j.matas@cube-studio.cz>
2008-07-28 11:47:53 +0000

    Import nové verze 3.0alpha2

---
+++ alpha2/modules/news/models/newslist.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * Třída modelu s listem Novinek
+ * 
+ * note:
+ * první je vždy název modulu a potom název modelu
+ * první písmena dle standardu jsou velká 
+ */
+class NewsList extends Models {
+	public $allNewsArray = array();
+	
+	public $scroll = null;
+	
+}
+
+?>
