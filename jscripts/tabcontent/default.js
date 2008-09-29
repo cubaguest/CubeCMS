a47d6e12bcfe1d99030d6f343ea369ddfb57bc8e
Jakub Matas <j.matas@cube-studio.cz>
2008-09-29 18:36:33 +0000

    Přidání core souborů jspluginu tabscontent (jsplugins)

---
+++ jscripts/tabcontent/default.js
@@ -0,0 +1,5 @@
+var langs=new ddtabcontent("langstabs") //enter ID of Tab Container
+myflowers.setpersist(true) //toogle persistence of the tabs' state
+myflowers.setselectedClassTarget("link") //"link" or "linkparent"
+myflowers.init()
+
