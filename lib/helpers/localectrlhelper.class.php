7cc1c4b3db8f0dec02cb5f575af90da0623a2126
Jakub Matas <j.matas@cube-studio.cz>
2008-10-05 21:07:29 +0000

    Oprava helperu locales při generování polí (engine helper locale)

--- lib/helpers/localectrlhelper.class.php
+++ lib/helpers/localectrlhelper.class.php
@@ -32,18 +32,30 @@ class LocaleCtrlHelper extends CtrlHelper {
 	 * @param array -- Pole s hodnotymi elemntů 
 	 * @param array -- Pole s alternativními hodnotami, pokud hodnota není ve values
 	 */
-	public function generateArray($elements, $values = null, $alternativeValues = null) {
+	public function generateArray($arrayElements, $values = null, $alternativeValues = null) {
 		$returnArray = array();
+		
+//		Oddělání posledního znaku určeného pro oddělení elementu a jazyku
+		$elements = array();
+		foreach ($arrayElements as $el) {
+			if($el[strlen($el)-1] == self::ELEMENT_LANG_SEPARATOR){
+				array_push($elements, substr($el, 0, strlen($el)-1));
+			} else {
+				array_push($elements, $el);
+			}
+		}
 		
 		foreach (Locale::getAppLangs() as $lang) {
 			$returnArray[$lang] = array();
 			foreach ($elements as $element) {
 				$returnArray[$lang][$element] = null;
 				if(isset($values[$element.self::ELEMENT_LANG_SEPARATOR.$lang]) 
-					AND $values[$element.self::ELEMENT_LANG_SEPARATOR.$lang] != null){
+//					AND $values[$element.self::ELEMENT_LANG_SEPARATOR.$lang] != null
+					){
 					$returnArray[$lang][$element] = $values[$element.self::ELEMENT_LANG_SEPARATOR.$lang];
 				} else if (isset($alternativeValues[$element.self::ELEMENT_LANG_SEPARATOR.$lang])
-					AND $alternativeValues[$element.self::ELEMENT_LANG_SEPARATOR.$lang] != null) {
+//					AND $alternativeValues[$element.self::ELEMENT_LANG_SEPARATOR.$lang] != null
+					) {
 					$returnArray[$lang][$element] = $alternativeValues[$element.self::ELEMENT_LANG_SEPARATOR.$lang];
 				} else {
 					$returnArray[$lang][$element] = null;
@@ -63,8 +75,19 @@ class LocaleCtrlHelper extends CtrlHelper {
 	 * @param boolean -- jestli se mají klíče výstupního polu ukládat s prefixem
 	 * @return array -- pole upravených postů
 	 */
-	public function postsToArray($postsArray, $postPrefix = null, $encodeSpecialChars = true, $decodeSpecialChars = false, $withPrefix = false){
+	public function postsToArray($sendPostsArray, $postPrefix = null, $encodeSpecialChars = true, $decodeSpecialChars = false, $withPrefix = false){
 		$returnArray = array();
+		
+//		Uprava postu o doplnění znaku pro oddělení postu a jazyku
+		$postsArray = array();
+		foreach ($sendPostsArray as $post) {
+			if($post[strlen($post)-1] == self::ELEMENT_LANG_SEPARATOR){
+				array_push($postsArray, $post);
+			} else {
+				array_push($postsArray, $post.self::ELEMENT_LANG_SEPARATOR);
+			}
+		}
+		
 		
 		foreach (Locale::getAppLangs() as $lang) {
 			foreach ($postsArray as $post) {
