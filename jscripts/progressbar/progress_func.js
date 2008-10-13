3c5707d0980afbd2c907320a54441334de359ac7
Jakub Matas <j.matas@cube-studio.cz>
2008-10-13 16:08:41 +0000

    Oprava vyskakovacího okna s progressbarem (Eplugin progressbar)

--- jscripts/progressbar/progress_func.js
+++ jscripts/progressbar/progress_func.js
@@ -1,4 +1,5 @@
 function openProgress(link) 
 {
-	window.open(link,'Progress','menubar=no,width=400,height=60,toolbar=no');
+	window.open(link,'Progress','menubar=no,width=400,height=60,toolbar=no,resizable=no,left=0,top=0');
+	//window.open("index.html", "nove", "width=800,height=600,menubar=yes,resizable=yes,left=0,top=0")
 };
