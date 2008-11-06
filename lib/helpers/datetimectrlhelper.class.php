587d5584305541fb8d94e705f4f4e63abe714522
Jakub Matas <j.matas@cube-studio.cz>
2008-11-06 21:43:04 +0000

    Přidání funcí pro kontrolu data a času do datehelperu (engine helpers)

--- lib/helpers/datetimectrlhelper.class.php
+++ lib/helpers/datetimectrlhelper.class.php
@@ -20,6 +20,18 @@ class DateTimeCtrlHelper extends CtrlHelper {
 	const POST_DATE_ARRAY_YEAR_KEY = 'Date_Year';
 
 	/**
+	 * Separator ISO data
+	 * @var string
+	 */
+	const ISO_DATE_SEPARATOR = '/';
+	
+	/**
+	 * Separator času
+	 * @var string
+	 */
+	const TIME_SEAPRATOR = ':';
+	
+	/**
 	 * Konstruktor třídy
 	 *
 	 */
@@ -73,6 +85,62 @@ class DateTimeCtrlHelper extends CtrlHelper {
     		case 10: case 11: return 2;
     		case 12: if ($day < 21) return 2; else return 3;  
   		}
+	}
+	
+	/**
+	 * Metoda testuje zadané datum, pokud je v pořádku je vráceno v ISO podobe YYYY-MM-DD
+	 * @param string -- kontrolované datum
+	 * @param boolean(option) -- true pokud má být vrácen timestamp
+	 * @return string -- vrací datum v ISO podobě nebo false
+	 */
+	public function checkDate($date, $timestam = false) {
+		if (ereg("([0-9]{1,2}).([0-9]{1,2}).([0-9]{4})",$date, $regs) AND checkdate ($regs[2], $regs[1], $regs[3]) AND Locale::getLang() != 'en'){
+			$date = $regs[2].self::ISO_DATE_SEPARATOR.$regs[1].self::ISO_DATE_SEPARATOR.$regs[3];
+			$time = mktime(null,null,null,$regs[2],$regs[1],$regs[3]);
+		} else if (ereg ("([0-9]{1,2})/([0-9]{1,2})/([0-9]{4})",$date, $regs) AND checkdate ($regs[1], $regs[2], $regs[3])){
+			$date = $regs[1].self::ISO_DATE_SEPARATOR.$regs[2].self::ISO_DATE_SEPARATOR.$regs[3];
+			$time = mktime(null,null,null,$regs[1],$regs[2],$regs[3]);
+		} else {
+			$date = $time = false;
+		}
+		
+		if($timestam){
+			return $time;
+		} else {
+			return $date;
+		}
+	}
+	
+	/**
+	 * Metoda zkontroluje zadaný čas (hh:mm)
+	 *
+	 * @param string -- kontrolovaný čas
+	 * @param boolean(option) -- true pokud má být vrácen timestamp
+	 * @return string -- vrací čas v podobě HH:MM nebo false
+	 */
+	public function checkTime($time, $timestamp = false) {
+		$nums = explode(self::TIME_SEAPRATOR, $time, 2);
+		
+		$allOk = true;
+//		Jestli se jedná o hodinu
+		if(!isset($nums[0]) OR $nums[0] < 0 OR $nums[0] > 23){
+			 $allOk = false;
+		}
+
+//		Jestli se jedná o minuty
+		if(!isset($nums[1]) OR $nums[1] < 0 OR $nums[1] > 59){
+			 $allOk = false;
+		}
+		
+		if($allOk){
+			if(!$timestamp){
+				return $nums[0].self::TIME_SEAPRATOR.$nums[1]; 
+			} else {
+				return mktime($nums[0], $nums[1]);
+			}
+		} else {
+			return false;
+		}
 	}
 	
 }
