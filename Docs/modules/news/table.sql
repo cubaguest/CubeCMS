ccd7d3a34f67565350b37f3cff29e270dc3591a8
Jakub Matas <j.matas@cube-studio.cz>
2009-02-09 00:52:24 +0000

    Doplnění dokumentace k modulům a kompletního otisku db

---
+++ Docs/modules/news/table.sql
@@ -0,0 +1,36 @@
+-- ZMĚNIT PREFIX !!!!!!!!!!!!!!!!!
+
+INSERT INTO `dev`.`vypecky_modules` (
+`id_module` ,
+`name` ,
+`params` ,
+`datadir` ,
+`dbtable1` ,
+`dbtable2` ,
+`dbtable3`
+)
+VALUES (
+NULL , 'news', NULL , NULL, 'news', NULL, NULL
+);
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabulky `vypecky_news`
+--
+
+CREATE TABLE IF NOT EXISTS `vypecky_news` (
+  `id_new` smallint(6) NOT NULL auto_increment,
+  `id_item` smallint(5) unsigned NOT NULL,
+  `id_user` smallint(6) NOT NULL,
+  `label_cs` varchar(50) NOT NULL,
+  `text_cs` varchar(500) NOT NULL,
+  `label_en` varchar(50) default NULL,
+  `text_en` varchar(500) default NULL,
+  `label_de` varchar(50) default NULL,
+  `text_de` varchar(500) default NULL,
+  `time` int(11) NOT NULL,
+  `deleted` tinyint(1) NOT NULL default '0',
+  PRIMARY KEY  (`id_new`),
+  KEY `id_user` (`id_user`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
