240de6c8a5eeab6f2f48c26671b6914f89122c81
Jakub Matas <j.matas@cube-studio.cz>
2009-09-23 09:51:33 +0000

    Implementace nové formulářové třídy v modulu pokus - modul pro testování

--- modules/pokus/controller.class.php
+++ modules/pokus/controller.class.php
@@ -6,7 +6,18 @@
 
 class Pokus_Controller extends Controller {
 	public function mainController() {
+      $form = new Form('pokus_');
+      $textElement = new Form_Element_Text('name', $this->_('Popisek'));
+      $textElement->addValidation(new Form_Validator_NotEmpty());
+      $form->addElement($textElement);
 
+      $form->addElement(new Form_Element_Submit('send'));
+
+      if($form->isSend()){
+         $this->infoMsg()->addMessage('odeslano');
+      }
+
+      $this->view()->form = $form;
 	}
 
 	
