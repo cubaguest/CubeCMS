36690cca3e7e541c19e4daed0ab01c1c2484a202
Jakub Matas <j.matas@cube-studio.cz>
2009-11-13 13:20:21 +0000

    Import modulu configuration pro nasatvení webu

---
+++ modules/configuration/controller.class.php
@@ -0,0 +1,115 @@
+<?php
+/**
+ * Třída pro obsluhu akcí a kontrolerů modulu
+ *
+ */
+
+class Configuration_Controller extends Controller {
+   private $categoriesArray = array();
+
+   public function mainController() {
+      $this->checkWritebleRights();
+      // nastavení viewru
+      $this->view()->template()->addTplFile('list.phtml');
+      $this->view()->template()->addCssFile('style.css');
+   }
+
+   public function showController() {
+   }
+
+   public function editController() {
+      $form = new Form('option');
+
+      $m = new Model_Config();
+      $opt = $m->getOption($this->getRequest('id'));
+
+      switch ($opt->{Model_Config::COLUMN_TYPE}) {
+         case 'number':
+            $elem = new Form_Element_TextArea('value', $this->_('Hodnota'));
+            $elem->addValidation(new Form_Validator_IsNumber());
+            break;
+         case 'bool':
+            $elem = new Form_Element_Radio('value', $this->_('Hodnota'));
+            $elem->setOptions(array('true' => 'true', 'false' => 'false'));
+            $elem->setValues($opt->{Model_Config::COLUMN_VALUE});
+            break;
+         case 'list':
+            $elem = new Form_Element_Select('value', $this->_('Hodnota'));
+            $arrExpl = explode(';', $opt->{Model_Config::COLUMN_VALUES});
+            $arr = array();
+            foreach ($arrExpl as $o) {
+               $arr[$o] = $o;
+            }
+            $elem->setOptions($arr);
+            $elem->setValues($opt->{Model_Config::COLUMN_VALUE});
+            break;
+         case 'listmulti':
+            $elem = new Form_Element_Select('value', $this->_('Hodnota'));
+            $arrExpl = explode(';', $opt->{Model_Config::COLUMN_VALUES});
+            $arr = array();
+            foreach ($arrExpl as $o) {
+               $arr[$o] = $o;
+            }
+
+            $arrExplValue = explode(';', $opt->{Model_Config::COLUMN_VALUE});
+            $arrVal = array();
+            foreach ($arrExplValue as $o) {
+               $arrVal[$o] = $o;
+            }
+            $elem->setOptions($arr);
+            $elem->setValues($arrVal);
+            $elem->setMultiple();
+            break;
+         case 'string':
+         default:
+            $elem = new Form_Element_TextArea('value', $this->_('Hodnota'));
+            $elem->setValues($opt->{Model_Config::COLUMN_VALUE});
+            break;
+      }
+      $form->addElement($elem);
+      
+      $submit = new Form_Element_Submit('send', $this->_('Odeslat'));
+      $form->addElement($submit);
+
+      if($form->isValid()){
+         $saveValue = null;
+         if($form->value instanceof Form_Element_TextArea OR $form->value instanceof Form_Element_Radio){
+            $saveValue = $form->value->getValues();
+         } else if($form->value instanceof Form_Element_Select){
+            $vals = $form->value->getValues();
+            if(is_array($vals)){
+               $saveValue = implode(';', $vals);
+            } else {
+               $saveValue = $vals;
+            }
+         }
+
+         if($saveValue !== null){
+            $m->saveCfg($opt->{Model_Config::COLUMN_KEY}, $saveValue);
+            $this->infoMsg()->addMessage($this->_('Volba byla uložena'));
+            $this->link()->route()->reload();
+         }
+
+      }
+
+      $this->view()->template()->form = $form;
+      $this->view()->template()->option = $opt;
+      $this->view()->template()->addTplFile('edit.phtml');
+   }
+
+//   public function addController() {
+//      $form = new Form('option');
+//
+//      $m = new Model_Config();
+//      $opt = $m->getOption($this->getRequest('id'));
+//
+//      var_dump($opt);
+//
+//      $this->view()->template()->form = $form;
+//      $this->view()->template()->edit = false;
+//      $this->view()->template()->addTplFile('edit.phtml');
+//   }
+
+}
+
+?>
