2df899a0e4eb94fa3f8c2e1cbf9d07347de74c79
Jakub Matas <j.matas@cube-studio.cz>
2010-01-18 17:12:38 +0000

    Import první verze modulu pro správu webu (čištění, optimalizace, zálohy)

---
+++ modules/services/controller.class.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Třída pro obsluhu akcí a kontrolerů modulu
+ */
+
+class Services_Controller extends Controller {
+   /**
+    * Kontroler pro zobrazení textu
+    */
+   public function mainController() {
+      $this->checkWritebleRights();
+
+
+      /*
+       *  CACHE
+      */
+      // velikost
+      $cacheSize = 0;
+      foreach (new DirectoryIterator(AppCore::getAppCacheDir()) as $fileInfo) {
+         if($fileInfo->isDot() OR $fileInfo->getFilename() == '.svn'|'.git') continue;
+         $cacheSize += $fileInfo->getSize();
+      }
+
+      $formClearCache = new Form('cache_clear_');
+      $elemSub = new Form_Element_Submit('clear', $this->_('Vyčistit'));
+      $formClearCache->addElement($elemSub);
+      if($formClearCache->isValid()) {
+
+         $dir = new DirectoryIterator(AppCore::getAppCacheDir());
+         foreach ($dir as $fileinfo) {
+            if($fileinfo->isDot() OR $fileinfo->getFilename() == '.svn'|'.git') continue;
+            @unlink(AppCore::getAppCacheDir().$fileinfo->getFilename());
+         }
+         $this->infoMsg()->addMessage($this->_('Cache byla vyčištěna'));
+         $this->link()->reload();
+      }
+
+      $this->view()->cacheSize = $cacheSize;
+      if($cacheSize/(1024*1024) > 10) {
+         $this->view()->isBigSize = true;
+      }
+      $this->view()->formClearCache = $formClearCache;
+
+   }
+}
+
+?>
