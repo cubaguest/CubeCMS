ea151c96c70f78cc396f28e3f5de2116792f7391
Jakub Matas <j.matas@cube-studio.cz>
2010-03-22 15:23:54 +0000

    Implementace JSPluginu DataTables

---
+++ lib/jsplugin/jsplugin_datatables.class.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Třída JsPluginu DataTables.
+ * Třída pro vkládání pluginu pro práci s tabulkama (filtrování, řazení, ...).
+ *
+ * @copyright  	Copyright (c) 2008-2010 Jakub Matas
+ * @version    	$Id: $ VVE 6.0.1 $Revision: $
+ * @author			$Author: $ $Date: $
+ *						$LastChangedBy: $ $LastChangedDate: $
+ * @abstract 		Třída JsPluginu DataTables
+ * @see           http://www.datatables.net/
+ */
+
+class JsPlugin_DataTables extends JsPlugin {
+   protected $config = array();
+
+	protected function initJsPlugin() {
+      $this->setJsPluginName('jquerydatatables');
+	}
+	
+	protected function setFiles() {
+      $this->addDependJsPlugin(new JsPlugin_JQuery());
+//		Přidání css stylu
+		$this->addFile(new JsPlugin_CssFile('datatables.css'));
+		//		Přidání js soubrů pluginu
+		$this->addFile(new JsPlugin_JsFile("jquery.dataTables.min.js"));
+	}
+}
+?>
