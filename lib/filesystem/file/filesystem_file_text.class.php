8a622cfa6b4abbfc6794c74ca76015a9402279a1
Jakub Matas <j.matas@cube-studio.cz>
2010-03-29 12:54:09 +0000

    Implementace třídy pro práci s textovými soubory

---
+++ lib/filesystem/file/filesystem_file_text.class.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Třída pro obsluhu souborů.
+ * Třída poskytuje základní metody pro práci se soubory,
+ * zjišťování mime typu, ukládání do filesystému, kopírování, mazání.
+ *
+ * @copyright  	Copyright (c) 2009 Jakub Matas
+ * @version    	$Id: file.class.php 636 2009-07-07 20:17:18Z jakub $ VVE3.9.4 $Revision: 636 $
+ * @author        $Author: jakub $ $Date: 2009-07-07 22:17:18 +0200 (Út, 07 čec 2009) $
+ *                $LastChangedBy: jakub $ $LastChangedDate: 2009-07-07 22:17:18 +0200 (Út, 07 čec 2009) $
+ * @abstract 		Třída pro obsluhu souborů
+ * @todo          Doimplementovat metody "move" a "rename"
+ */
+
+class Filesystem_File_Text extends Filesystem_File {
+	private static $textMimeTypes = array(
+	       'txt' => 'text/plain',
+       'htm' => 'text/html',
+       'html' => 'text/html',
+       'php' => 'text/html',
+       'css' => 'text/css',
+       'js' => 'application/javascript',
+       'json' => 'application/json',
+       'xml' => 'application/xml'
+	);
+   
+   
+	public function isTextFile() {
+		if(in_array($this->getMimeType(), self::$textMimeTypes)){
+		   return true;
+		}
+      return false;
+	}
+	
+	/**
+	 * Metoda načte obsah souboru
+	 * @return string -- obsah souboru
+	 */
+	public function getContent() {
+		if($this->getName()){
+			return file_get_contents($this->getName(true));
+		}
+		return null;
+	}
+}
+?>
