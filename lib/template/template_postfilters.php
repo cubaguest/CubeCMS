a915ec0be1154084cc066cd30db1a115c13a444d
Jakub Matas <j.matas@cube-studio.cz>
2010-05-23 10:11:13 +0000

    Přidání template postfilteru pro emotikony.

--- lib/template/template_postfilters.php
+++ lib/template/template_postfilters.php
@@ -1,22 +1,42 @@
 <?php
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
- * Description of template_postfilters
+ * Třída s posrfiltry, aplikovanými na proměnné v šabloně. Měla by být implementována
+ * přímo do renderu šablon
  *
- * @author jakub
+ * @copyright  	Copyright (c) 2008-2009 Jakub Matas
+ * @version    	$Id$ VVE3.9.4 $Revision$
+ * @author        $Author$ $Date$
+ *                $LastChangedBy$ $LastChangedDate$
+ * @abstract 		Třída s postfiltry
  */
 class Template_Postfilters {
+   private static $emoticonsTranslate = array(
+      ':-D' => '<img src="images/smiles/face-laugh.png" alt=":-D" />',
+      ':-(' => '<img src="images/smiles/face-sad.png" alt=":-(" />',
+      ':-/' => '<img src="images/smiles/face-uncertain.png" alt=":-/" />',
+      ':-|' => '<img src="images/smiles/face-plain.png" alt=":-|" />',
+      ';-)' => '<img src="images/smiles/face-wink.png" alt=";-)" />',
+      ':-*' => '<img src="images/smiles/face-kiss.png" alt=":-*" />',
+      ':*' => '<img src="images/smiles/face-kiss.png" alt=":*" />',
+      'O:-)' => '<img src="images/smiles/face-angel.png" alt="O:-)" />',
+      'O:)' => '<img src="images/smiles/face-angel.png" alt="O:)" />',
+      '>:)' => '<img src="images/smiles/face-evil.png" alt=">:)" />',
+      'D:<' => '<img src="images/smiles/face-angry.png" alt="D:<" />',
+      'D:-<' => '<img src="images/smiles/face-angry.png" alt="D:-<" />',
+      ':-O' => '<img src="images/smiles/face-surprise.png" alt=":-O" />',
+      ':O' => '<img src="images/smiles/face-surprise.png" alt=":O" />',
+      ':)' => '<img src="images/smiles/face-smile.png" alt=":)" />',
+      ':-)' => '<img src="images/smiles/face-smile.png" alt=":-)" />'
+      );
+
+
     /**
 	 * Metoda převede předložky a některé znaky s normální mezerou na nezalomitelné mezery
 	 *
 	 * @param string -- zadaný text
 	 * @return string -- text. u kterého jsou převedeny předložky
 	 */
-	public function czechTypo($text) {
+	public static function czechTypo($text) {
 		$czechPripositions = "(k|s|v|z|a|i|o|u|ve|ke|ku|za|ze|na|do|od|se|po|pod|před|nad|bez|pro|při|Ing.|Bc.|Arch.|Mgr.)";
 
 		// překlad předložek na konci řádku
@@ -39,5 +59,15 @@ class Template_Postfilters {
 		$text = preg_replace($pattern, $replacement, $text);
 		return $text;
 	}
+
+   /**
+    * Metoda převede emotikony v řetězci na obrázky
+    * @param string $string -- řetězec
+    * @return string
+    */
+   public static function emoticons($string){
+      $string = strtr($string, self::$emoticonsTranslate);
+      return $string;
+   }
 }
 ?>
