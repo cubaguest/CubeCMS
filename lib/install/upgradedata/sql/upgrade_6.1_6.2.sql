250409235344fa4d495777e959a6ad927afea9d9
Jakub Matas <j.matas@cube-studio.cz>
2010-08-29 10:02:54 +0000

    Přidání SQL skruptu pro uprgrade z verze 6.1 na 6.2

---
+++ lib/install/upgradedata/sql/upgrade_6.1_6.2.sql
@@ -0,0 +1,5 @@
+INSERT INTO `{PREFIX}config` (`key`, `label`, `value`, `protected`, `type`) VALUES ('CM_SITEMAP_MAX_ITEMS_PAGE', 'Maximální počet položek v mapě stránek', '20', '0', 'number');
+INSERT INTO `{PREFIX}config` (`key`, `label`, `value`, `protected`, `type`) VALUES ('CM_SITEMAP_MAX_ITEMS', 'Maximální počet položek v mapě stránek (pro vyhledávače)', '50', '0', 'number');
+INSERT INTO `{PREFIX}config` (`key`, `label`, `value`, `protected`, `type`) VALUES ('CM_SITEMAP_CAT_ICON', 'Název ikony pro sitemap', 'sitemap.png', '0', 'string');
+INSERT INTO `{PREFIX}config` (`key`, `label`, `value`, `protected`, `type`) VALUES ('CM_ERR_CAT_ICON', 'Název ikony pro chybovou stránku', 'error.png', '0', 'string');
+INSERT INTO `{PREFIX}config` (`key`, `label`, `value`, `protected`, `type`) VALUES ('CM_RSS_CAT_ICON', 'Název ikony pro stránku s rss kanály', 'rsslist.png', '0', 'string');
