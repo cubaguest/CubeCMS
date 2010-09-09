6ef683d48a61c92a70902153d2403de342d7d8e1
Jakub Matas <j.matas@cube-studio.cz>
2010-09-09 14:36:24 +0000

    Přepracování objektu mail pro práci s knihovnou SwiftMailer

--- lib/email.class.php
+++ lib/email.class.php
@@ -11,6 +11,9 @@
  *                $LastChangedBy$ $LastChangedDate$
  * @abstract      Třída pro práci s emaily
  */
+// jádro
+require_once AppCore::getAppLibDir().AppCore::ENGINE_LIB_DIR.DIRECTORY_SEPARATOR
+                      .'nonvve'.DIRECTORY_SEPARATOR."swift".DIRECTORY_SEPARATOR."swift_required.php";
 class Email {
 
    /**
@@ -20,58 +23,19 @@ class Email {
    private $isHtmlMail = false;
 
    /**
-    * Předmět zprávy
-    * @var string
+    *
+    * @var string[]
     */
-   private $mailSubject = 'php mail';
+   private $mailsAddresses = array();
 
    /**
-    * Kontrolní řetězec mailu
-    * @var string
+    *
+    * @var Swift_Mailer
     */
-   private $mimeBoundary = null;
+   private $mailer = null;
 
-   /**
-    * Pole s adresami pro odeslání
-    * @var array
-    */
-   private $mailAddress = array();
+   private $message = null;
 
-   /**
-    * Název schránky ze které se odesílá
-    * @var string
-    */
-   private $mailFrom = null;
-
-   /**
-    * Název odesílatele mailu
-    * @var string
-    */
-   private $mailFromName = null;
-
-   /**
-    * Samotný obsah mailu
-    * @var string
-    */
-   private $mailContent = null;
-
-   /**
-    * Pole souborů pro připojení
-    * @var array
-    */
-   private $mailAttachments = array();
-
-   /**
-    * Kódování zprávy
-    * @var string
-    */
-   private $mailCharset = 'utf-8';
-
-   /**
-    * Základní hlavičky pro odeslání emailu
-    * @var array
-    */
-   private $mailHeaders = array();
 
    /**
     * Konstruktor. Vytvoří objekt emailu
@@ -79,9 +43,21 @@ class Email {
     */
    public function __construct($htmlMail = false) {
       $this->isHtmlMail = $htmlMail;
-      $semi_rand = md5(time());
-      $this->mimeBoundary = "==Multipart_Boundary_x{$semi_rand}x";
-      $this->mailFrom = 'noreplay@'.$_SERVER['SERVER_NAME'];
+      if(VVE_SMTP_SERVER != null){
+         $transport = Swift_SmtpTransport::newInstance(VVE_SMTP_SERVER, VVE_SMTP_SERVER_PORT);
+         if(VVE_SMTP_SERVER_USERNAME != null){
+            $transport->setUsername(VVE_SMTP_SERVER_USERNAME)->setPassword(VVE_SMTP_SERVER_PASSWORD);
+         }
+      } else {
+         $transport = Swift_MailTransport::newInstance();
+      }
+      $this->mailer = Swift_Mailer::newInstance($transport);
+      $this->message = Swift_Message::newInstance(null);
+      if(VVE_NOREPLAY_MAIL != null) {
+         $this->message->setFrom(VVE_NOREPLAY_MAIL);
+      } else {
+         $this->message->setFrom('noreplay@'.$_SERVER['SERVER_NAME']);
+      }
    }
 
    /**
@@ -89,18 +65,8 @@ class Email {
     * @param string $from -- adresa pro odesílání
     * @return Email -- vrací sebe
     */
-   public function setFrom($from) {
-      $this->mailFrom = $from;
-      return $this;
-   }
-
-   /**
-    * Metoda nastaví název odesílatele pro odesílání
-    * @param string $fromName -- název odesílatele
-    * @return Email -- vrací sebe
-    */
-   public function setFromName($fromName) {
-      $this->mailFromName = $fromName;
+   public function setFrom($address, $name) {
+      $this->message->setFrom($address, $name);
       return $this;
    }
 
@@ -110,7 +76,7 @@ class Email {
     * @return Email -- vrací sebe
     */
    public function setSubject($subject) {
-      $this->mailSubject = $subject;
+      $this->message->setSubject($subject);
       return $this;
    }
 
@@ -121,20 +87,16 @@ class Email {
     * @return Email -- vrací sebe
     */
    public function setContent($content, $merge = false) {
+      $cntType = 'text/plain';
+      if($this->isHtmlMail == true){
+         $cntType = 'text/html';
+      }
       if($merge){
-         $this->mailContent .= $content;
+         $this->message->setBody($this->message->getBody().$content, $cntType);
       } else {
-         $this->mailContent = $content;
+         $this->message->setBody($content, $cntType);
       }
       return $this;
-   }
-
-   /**
-    * Metoda nastaví kódování zprávy
-    * @param string $charset -- (option) kódování zprávy výchozí je 'utf-8'
-    */
-   public function setCharset($charset = 'utf-8') {
-      $this->mailCharset = $charset;
    }
 
    /**
@@ -142,11 +104,15 @@ class Email {
     * @param string/array $address -- adresa nebo pole adres pro příjem
     * @return Email -- vrací sebe
     */
-   public function addAddress($address) {
+   public function addAddress($address, $name = null) {
       if(is_array($address)){
-         $this->mailAddress = array_merge($this->mailAddress, $address);
+         $this->mailsAddresses = array_merge($this->mailsAddresses, $address);
       } else {
-         array_push($this->mailAddress, $address);
+         if($name == null){
+            array_push($this->mailsAddresses, $address);
+         } else {
+            $this->mailsAddresses[$address] = $name;
+         }
       }
       return $this;
    }
@@ -157,7 +123,6 @@ class Email {
     * @return Email -- vrací sebe
     */
    public function addHeader($header) {
-      array_push($this->mailHeaders, $header);
       return $this;
    }
 
@@ -167,7 +132,7 @@ class Email {
     * @return Email -- vrací sebe
     */
    public function addAttachment(Filesystem_File $file) {
-      array_push($this->mailAttachments, $file);
+      $this->message->attach(Swift_Attachment::fromPath($file->getName(true)));
       return $this;
    }
 
@@ -175,93 +140,16 @@ class Email {
     * Metoda odešle email
     */
    public function sendMail() {
-      // kontrola adresy příjemce a předmětu
-      if(empty ($this->mailAddress) OR $this->mailSubject == null){
-         throw new CoreException(_('Nebyla zadána cílová adresa nebo předmět emailu'));
-      }
-
-      // předmět
-      $encodedSubject="=?$this->mailCharset?B?".base64_encode($this->mailSubject)."?=\n";
-      // tělo emailu
-      $mailContent = $this->createMail();
-      // hlavičky
-      $headers = $this->createHeaders();
-      $this->mailAddress = array_unique($this->mailAddress);
-      foreach ($this->mailAddress as $address) {
-         if($address == null) continue;
-         if(!mail($address, $encodedSubject, $mailContent, $headers)){
-            throw new CoreException(sprintf(_('Chyba při odesílání emailu na adresu "%s"'),$address));
-         }
-      }
-      return true;
+      $this->send();
    }
 
-   /**
-    * Privátní metody pro tvorbu mailu
-    * @return string -- tělo mailu
-    */
-   private function createMail() {
-      $mail = null;
-
-      $mail = "\nThis is a multi-part message in MIME format.\n\n" . "--{$this->mimeBoundary}\n";
-      // jestli je html
-      if($this->isHtmlMail){
-         $mail .= "Content-Type: text/html; charset=\"{$this->mailCharset}\"\n";
-      } else {
-         $mail .= "Content-Type: text/plain; charset=\"{$this->mailCharset}\"\n";
-      }
-      $mail .= "Content-Transfer-Encoding: 8bit\n\n";
-//      $mail . "\n\n";
-
-      // Tělo zprávy e-mailu
-//      $mail .= "\n".$this->mailContent."\n\n";
-      $mail .= $this->mailContent."\r\n";
-
-      // přidání příloh
-      foreach ($this->mailAttachments as $attachment) {
-         $file = fopen($attachment->getName(true),'rb');
-         $data = fread($file,$attachment->getFileSize());
-         fclose($file);
-         $data = chunk_split(base64_encode($data));
-         $mail .= "--{$this->mimeBoundary}\n" .
-                  "Content-Type: {$attachment->getMimeType()};\n" .
-                  " name=\"{$attachment->getName()}\"\n" .
-                  "Content-Disposition: attachment;\n" .
-                  " filename=\"{$attachment->getName()}\"\n" .
-                  "Content-Transfer-Encoding: base64\n\n" . $data . "\n\n";
-      }
-      // konec zprávy
-      $mail .= "--{$this->mimeBoundary}--\n";
-      return $mail;
+   public function send() {
+      $this->message->setTo($this->mailsAddresses);
+      return $this->mailer->send($this->message);
    }
-
-   /**
-    * Metoda vytvoří řetězec s hlavičkami mailu
-    * @return string -- hlavičky
-    */
-   private function createHeaders() {
-      $headers = null;
-      if($this->mailFromName != null){
-         $headers = "From: " . $this->mailFromName . "<" . $this->mailFrom . ">\n";
-      } else {
-         $headers = "From: <" . $this->mailFrom . ">\n";
-      }
-
-      $headers .= "MIME-Version: 1.0\n" .
-                  "Content-Type: multipart/mixed;\n" .
-                  " boundary=\"{$this->mimeBoundary}\"\n";
-      $headers .= "X-Sender: ".$this->mailFrom."\n";
-      $headers .= "Reply-To: ".$this->mailFrom."\n";
-      $headers .= "X-Mailer: PHP\n";
-      $headers .= "X-Priority: 3";
-
-      // přidání uživatelských hlaviček
-      foreach ($this->mailHeaders as $header) {
-         $headers .= $header;
-      }
-
-      return $headers;
+   public function batchSend() {
+      $this->message->setTo($this->mailsAddresses);
+      return $this->mailer->batchSend($this->message);
    }
-
 }
 ?>
