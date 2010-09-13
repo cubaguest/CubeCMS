18cf4bc21a01f9a529939356bc9adf7038af69ce
Jakub Matas <j.matas@cube-studio.cz>
2010-09-13 20:43:25 +0000

    Přidání metod pro hledání do JQGrid requestu

--- lib/component/jqgrid/component_jqgrid_request.class.php
+++ lib/component/jqgrid/component_jqgrid_request.class.php
@@ -13,27 +13,74 @@ class Component_JqGrid_Request {
    const REQUEST_SEARCH_STRING = 'searchString';
    const REQUEST_SEARCH_OPERATOR = 'searchOper';
 
-   private $type = null;
-   public $rows = 0;
+   const SEARCH_EQUAL = 'eq';
+   const SEARCH_NOT_EQUAL = 'ne';
+   const SEARCH_CONTAIN = 'cn';
+   const SEARCH_NOT_CONTAIN = 'nc';
+
+
+   public $rows = 1000;
    public $page = 1;
    public $orderField = null;
    public $order = 'asc';
    private $isSearch = false;
 
+   private $searchFiled = null;
+   private $searchOper = null;
+   private $searchString = null;
+
+   private static $searchAvailOper = array('eq', 'ne', 'cn', 'nc');
+
    public function __construct() {
       if (isset($_POST[self::REQUEST_SEARCH]) AND $_POST[self::REQUEST_SEARCH] == 'true') {
          $this->isSearch = true;
+         $this->searchFiled = $_POST[self::REQUEST_SEARCH_FIELD];
+         if(in_array($_POST[self::REQUEST_SEARCH_OPERATOR], self::$searchAvailOper)){
+            $this->searchOper = $_POST[self::REQUEST_SEARCH_OPERATOR];
+         } else {
+            $this->searchOper = self::SEARCH_CONTAIN;
+         }
+         $this->searchString = $_POST[self::REQUEST_SEARCH_STRING];
       }
-
-      $this->rows = (int)$_POST[self::REQUEST_ROWS];
-      $this->page = (int)$_POST[self::REQUEST_PAGE];
-      $this->order = $_POST[self::REQUEST_SORT_ORD];
-      $this->orderField = $_POST[self::REQUEST_SORT_FIELD];
+      if(isset ($_POST[self::REQUEST_ROWS]))
+         $this->rows = (int)$_POST[self::REQUEST_ROWS];
+      if(isset ($_POST[self::REQUEST_PAGE]))
+         $this->page = (int)$_POST[self::REQUEST_PAGE];
+      if(isset ($_POST[self::REQUEST_SORT_ORD]))
+         $this->order = $_POST[self::REQUEST_SORT_ORD];
+      if(isset ($_POST[self::REQUEST_SORT_FIELD]))
+         $this->orderField = $_POST[self::REQUEST_SORT_FIELD];
    }
 
+   /**
+    * Metoda vrací jestli se jedná o hledání
+    * @return boolean
+    */
    public function isSearch() {
       return $this->isSearch;
    }
 
+   /**
+    * Metoda nastaví výchozí sloupec podle kterého se řadí
+    * @param string $field -- název sloupce
+    */
+   public function setDefaultOrderField($field) {
+      if($this->orderField == null){
+         $this->orderField = $field;
+      }
+   }
+
+   public function searchType() {
+      return $this->searchOper;
+   }
+
+   public function searchField() {
+      return $this->searchFiled;
+   }
+
+   public function searchString() {
+      return $this->searchString;
+
+   }
 }
 ?>
