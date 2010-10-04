bbf7c202c58388dc29607ca45afbb3221fea0608
Jakub Matas <j.matas@cube-studio.cz>
2010-10-04 10:34:38 +0000

    M_photogalery: Přidání ikonek pro toolbox.

--- modules/photogalery/view.class.php
+++ modules/photogalery/view.class.php
@@ -3,29 +3,22 @@ class Photogalery_View extends View {
    public function mainView() {
       $this->template()->addTplFile("list.phtml");
       if($this->category()->getRights()->isControll()) {
-         $toolbox = new Template_Toolbox2();
+         $toolbox1 = new Template_Toolbox2();
+         $toolbox1->setIcon(Template_Toolbox2::ICON_PEN);
          $toolEditText = new Template_Toolbox2_Tool_PostRedirect('edit_text', $this->_("Upravit text"),
          $this->link()->route('edittext'));
          $toolEditText->setIcon('page_edit.png')->setTitle($this->_('Upravit text galerie'));
-         $toolbox->addTool($toolEditText);
+         $toolbox1->addTool($toolEditText);
+         $this->toolboxText = $toolbox1;
 
-         $this->toolboxText = $toolbox;
          $toolbox = new Template_Toolbox2();
+         $toolbox->setIcon(Template_Toolbox2::ICON_IMAGE_WRENCH);
          $toolEditText = new Template_Toolbox2_Tool_PostRedirect('edit_text', $this->_("Spravovat obrázky"),
          $this->link()->route('editphotos'));
          $toolEditText->setIcon('image_edit.png')->setTitle($this->_('Spravovat kolekci obrázků'));
          $toolbox->addTool($toolEditText);
 
          $this->toolboxImages = $toolbox;
-
-//         $toolbox = new Template_Toolbox();
-//         $toolbox->addTool('edit_text', $this->_("Upravit text"),
-//                 $this->link()->route('edittext'),
-//                 $this->_("Upravit text galerie"), "page_edit.png");
-//         $toolbox->addTool('edit_images', $this->_("Upravit fotky"),
-//                 $this->link()->route('editphotos'),
-//                 $this->_("Upravit fotky galerie"), "image_edit.png");
-//         $this->template()->toolbox = $toolbox;
       }
    }
 
