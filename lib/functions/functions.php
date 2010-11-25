2d93fa04c00d066be1f58432c1f444a58ec33e64
Jakub Matas <j.matas@cube-studio.cz>
2010-11-25 14:59:18 +0000

    Core: Oprava funkce pro generování UrlKlíčů a funkce pro konverzi na ASCII (nyní využívá iconv)

--- lib/functions/functions.php
+++ lib/functions/functions.php
@@ -10,20 +10,21 @@
  * @return string -- převedený řetězec
  */
 function vve_to_ascii(&$string) {
-   $string = strtr($string,
-           Array(
-           "á"=>'a',"â"=>'a',"ă"=>'a',"ä"=>'a',"č"=>'c',"ć"=>'c',"ç"=>'c',
-           "ď"=>'d',"đ"=>'d',"é"=>'e',"ě"=>'e',"ę"=>'e',"ë"=>'e',
-           "í"=>'i',"î"=>'i',"ľ"=>'l',"ĺ"=>'l',"ň"=>'n',"ń"=>'n',
-           "ó"=>'o',"ô"=>'o',"ő"=>'o',"ö"=>'o',"ř"=>'r',"š"=>'s',"ś"=>'s',"ß" => 's',
-           "ť"=>'t',"ú"=>'u',"ů"=>'u',"ű"=>'u',"ü"=>'u',"ý"=>'y',"ž"=>'z',"ź"=>'z',
-
-           "Á"=>'A',"Â"=>'A',"Ă"=>'A',"Ä"=>'A',"Č"=>'C',"Ć"=>'C',"Ç"=>'C',
-           "Ď"=>'D',"Đ"=>'D',"É"=>'E',"Ě"=>'E',"Ę"=>'E',"Ë"=>'E',
-           "Í"=>'I',"Î"=>'I',"Ľ"=>'L',"Ĺ"=>'L',"Ň"=>'N',"Ń"=>'N',
-           "Ó"=>'O',"Ô"=>'O',"Ő"=>'O',"Ö"=>'O',"Ř"=>'R',"Š"=>'S',"Ś"=>'S',
-           "Ť"=>'T',"Ú"=>'U',"Ů"=>'U',"Ű"=>'U',"Ü"=>'U',"Ý"=>'Y',"Ž"=>'Z',"Ź"=>'Z'));
+//   $string = strtr($string,
+//           Array(
+//           "á"=>'a',"â"=>'a',"ă"=>'a',"ä"=>'a',"č"=>'c',"ć"=>'c',"ç"=>'c',
+//           "ď"=>'d',"đ"=>'d',"é"=>'e',"ě"=>'e',"ę"=>'e',"ë"=>'e',
+//           "í"=>'i',"î"=>'i',"ľ"=>'l',"ĺ"=>'l',"ň"=>'n',"ń"=>'n',
+//           "ó"=>'o',"ô"=>'o',"ő"=>'o',"ö"=>'o',"ř"=>'r',"š"=>'s',"ś"=>'s',"ß" => 's',
+//           "ť"=>'t',"ú"=>'u',"ů"=>'u',"ű"=>'u',"ü"=>'u',"ý"=>'y',"ž"=>'z',"ź"=>'z',
+//
+//           "Á"=>'A',"Â"=>'A',"Ă"=>'A',"Ä"=>'A',"Č"=>'C',"Ć"=>'C',"Ç"=>'C',
+//           "Ď"=>'D',"Đ"=>'D',"É"=>'E',"Ě"=>'E',"Ę"=>'E',"Ë"=>'E',
+//           "Í"=>'I',"Î"=>'I',"Ľ"=>'L',"Ĺ"=>'L',"Ň"=>'N',"Ń"=>'N',
+//           "Ó"=>'O',"Ô"=>'O',"Ő"=>'O',"Ö"=>'O',"Ř"=>'R',"Š"=>'S',"Ś"=>'S',
+//           "Ť"=>'T',"Ú"=>'U',"Ů"=>'U',"Ű"=>'U',"Ü"=>'U',"Ý"=>'Y',"Ž"=>'Z',"Ź"=>'Z'));
 //   $string = StrToLower($string); //velká písmena nahradí malými.
+   $string = iconv('UTF-8', 'ASCII//TRANSLIT', $string);
    return $string;
 }
 
@@ -32,7 +33,7 @@ function vve_to_ascii(&$string) {
  * @param string/array $string -- řetězec nebo pole pro převedení
  * @return string/array -- řetězec nebo pole s převedenými znaky
  */
-function vve_cr_url_key($string) {
+function vve_cr_url_key($string, $removeSlashes = true) {
    if(is_null($string)) {
       return $string;
    } else if(is_array($string)) {
@@ -40,16 +41,14 @@ function vve_cr_url_key($string) {
          $string[$key] = vve_cr_url_key($variable);
       }
    } else {
-      $string = vve_to_ascii($string);
-
-//      $regexp = array('/(^[^a-z]{1,})|([\\:;()"\'!?.,|<>\/]?)/i', '/[ \_-]{1,}/');
-      $regexp = array('/(^[^a-z0-9]{1,})|([^ \/a-z0-9\_-]?)/i', '/[ -]{1,}/');
-      $replacements = array('', '-');
+      $string = vve_to_ascii(strip_tags($string));
+      $slashes = null;
+      if($removeSlashes){
+         $slashes = '\/';
+      }
+      $regexp = array('/[^a-z0-9\/ _-]+/i', '/[ '.$slashes.'-]+/', '/[\/]+/');
+      $replacements = array('', '-', URL_SEPARATOR);
       $string = preg_replace($regexp, $replacements, $string);
-
-//      $string = preg_replace('/^[ :;\_()."\'!?<>,-]{1,}/', "", $string);
-//      $string = preg_replace("/[ \_-]{1,}/", "-", $string);
-//      $string = preg_replace("/[().\"\'!?<>,]?/", "", $string);
    }
    return $string;
 }
