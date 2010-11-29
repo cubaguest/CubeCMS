edeb79be1f2a56b759edf93e8dfa69548492ca42
Jakub Matas <j.matas@cube-studio.cz>
2010-11-29 12:29:34 +0000

    M_News: oprava podle modulu Articles

---
+++ modules/news/templates/tpls.php
@@ -0,0 +1,12 @@
+<?php
+/* 
+ * Seznam šablon
+ */
+
+$this->list = array('list.phtml' => array('cs' => 'Výchozí šablona', 'en' => 'Default template', 'sk' => 'Predvolená šablóna', 'de' => 'Standardvorlage'));
+$this->detail = array('articles:detail.phtml' => array('cs' => 'Výchozí šablona', 'en' => 'Default template', 'sk' => 'Predvolená šablóna', 'de' => 'Standardvorlage'));
+$this->archive = array('articles:archive.phtml' => array('cs' => 'Výchozí šablona', 'en' => 'Default template', 'sk' => 'Predvolená šablóna', 'de' => 'Standardvorlage'));
+
+$this->panel = array('panel.phtml' => array('cs' => 'Výchozí šablona', 'en' => 'Default template', 'sk' => 'Predvolená šablóna', 'de' => 'Standardvorlage'));
+
+?>
--- modules/news/view.class.php
+++ modules/news/view.class.php
@@ -1,7 +1,7 @@
 <?php
 class News_View extends Articles_View {
    public function mainView() {
-      $this->template()->addTplFile("list.phtml");
+      $this->template()->addFile('tpl://'.$this->category()->getParam(Articles_Controller::PARAM_TPL_LIST, 'news:list.phtml'));
       $this->createListToolbox();
       if($this->toolbox != null){
          $this->toolbox->add_article->setLabel($this->_('Přidat novinku'))->setTitle($this->_('Přidat novou novinku'));
