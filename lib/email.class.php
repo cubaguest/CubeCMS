f5c98845f951fc47ed597819196c59375f539c6f
Jakub Matas <j.matas@cube-studio.cz>
2011-01-21 11:46:45 +0000

    Core: Přidání getteru a setteru pro objekt zprávy, Přidání metody pro vrázení všech příjemců.

--- lib/email.class.php
+++ lib/email.class.php
@@ -42,7 +42,8 @@ class Email {
     * Konstruktor. Vytvoří objekt emailu
     * @param boolean $htmlMail -- (option) jestli se bude vytvářet html mail
     */
-   public function __construct($htmlMail = false) {
+   public function __construct($htmlMail = false)
+   {
       $this->isHtmlMail = $htmlMail;
       if(VVE_SMTP_SERVER != null){
          $transport = Swift_SmtpTransport::newInstance(VVE_SMTP_SERVER, VVE_SMTP_SERVER_PORT);
@@ -53,24 +54,11 @@ class Email {
          $transport = Swift_MailTransport::newInstance();
       }
       $this->mailer = Swift_Mailer::newInstance($transport);
-      $this->message = Swift_Message::newInstance();
-      $this->message->setEncoder(Swift_Encoding::get8BitEncoding());
-      switch (Locales::getLang()) {
-         case 'cs': // many czech people use very old mail interface
-            $this->message->setCharset('iso-8859-2');
-            $this->iconvParams = array(0 => 'UTF-8', 1 => 'iso-8859-2//TRANSLIT');
-            break;
-         default:
-            break;
-      }
-      if(VVE_NOREPLAY_MAIL != null) {
-         $this->message->setFrom(VVE_NOREPLAY_MAIL);
-      } else {
-         $this->message->setFrom('noreplay@'.$_SERVER['SERVER_NAME']);
-      }
+      $this->setMessage(Swift_Message::newInstance());
    }
    
-   private function safeStr($str) {
+   private function safeStr($str)
+   {
       if(empty ($this->iconvParams)) return $str;
       return iconv($this->iconvParams[0], $this->iconvParams[1], $str);
    }
@@ -80,7 +68,8 @@ class Email {
     * @param string $from -- adresa pro odesílání
     * @return Email -- vrací sebe
     */
-   public function setFrom($address, $name=null) {
+   public function setFrom($address, $name=null)
+      {
       $this->message->setFrom($address, $name);
       return $this;
    }
@@ -90,7 +79,8 @@ class Email {
     * @param string $subject -- předmět emailu
     * @return Email -- vrací sebe
     */
-   public function setSubject($subject) {
+   public function setSubject($subject)
+   {
       $this->message->setSubject($this->safeStr($subject));
       return $this;
    }
@@ -101,7 +91,8 @@ class Email {
     * @param boolean $merge -- (option) jestli se má obsah spojit s již existujícím obsahem
     * @return Email -- vrací sebe
     */
-   public function setContent($content, $merge = false) {
+   public function setContent($content, $merge = false)
+      {
       $cntType = 'text/plain';
       if($this->isHtmlMail == true){
          $cntType = 'text/html';
@@ -119,7 +110,8 @@ class Email {
     * @param string/array $address -- adresa nebo pole adres pro příjem
     * @return Email -- vrací sebe
     */
-   public function addAddress($address, $name = null) {
+   public function addAddress($address, $name = null)
+      {
       if(is_array($address)){
          $this->mailsAddresses = array_merge($this->mailsAddresses, $address);
       } else {
@@ -133,11 +125,21 @@ class Email {
    }
 
    /**
+    * Metoda vrací adresy, na které bude mail odeslán
+    * @return array
+    */
+   public function getAddresses()
+   {
+      return $this->mailsAddresses;
+   }
+
+   /**
     * Metoda přidá hlavičku k mailu
     * @param string $header -- hlavička mailu
     * @return Email -- vrací sebe
     */
-   public function addHeader($header) {
+   public function addHeader($header)
+   {
       return $this;
    }
 
@@ -146,7 +148,8 @@ class Email {
     * @param File $file -- objekt souboru
     * @return Email -- vrací sebe
     */
-   public function addAttachment(Filesystem_File $file) {
+   public function addAttachment(Filesystem_File $file)
+   {
       $this->message->attach(Swift_Attachment::fromPath($file->getName(true)));
       return $this;
    }
@@ -154,17 +157,52 @@ class Email {
    /**
     * Metoda odešle email
     */
-   public function sendMail() {
+   public function sendMail()
+   {
       $this->send();
    }
 
-   public function send() {
+   public function send(&$failures = null)
+   {
       $this->message->setTo($this->mailsAddresses);
-      return $this->mailer->send($this->message);
+      return $this->mailer->send($this->message, $failures);
    }
-   public function batchSend() {
+   public function batchSend(&$failures = null)
+   {
       $this->message->setTo($this->mailsAddresses);
-      return $this->mailer->batchSend($this->message);
+      return $this->mailer->batchSend($this->message, $failures);
+   }
+
+   /**
+    * metoda vrací objekt zprávy
+    * @return Swift_Message
+    */
+   public function getMessage()
+   {
+      return $this->message;
+   }
+
+   /**
+    * Metoda nastaví objekt zprávy (např z db/cache)
+    * @param Swift_Message $message
+    */
+   public function setMessage(Swift_Message $message)
+   {
+      $this->message = $message;
+      $this->message->setEncoder(Swift_Encoding::get8BitEncoding());
+      switch (Locales::getLang()) {
+         case 'cs': // many czech people use very old mail interface
+            $this->message->setCharset('iso-8859-2');
+            $this->iconvParams = array(0 => 'UTF-8', 1 => 'iso-8859-2//TRANSLIT');
+            break;
+         default:
+            break;
+      }
+      if(VVE_NOREPLAY_MAIL != null) {
+         $this->message->setFrom(VVE_NOREPLAY_MAIL);
+      } else {
+         $this->message->setFrom('noreplay@'.$_SERVER['SERVER_NAME']);
+      }
    }
 }
 ?>
