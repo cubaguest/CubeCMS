b49482b5eb9e2cc078c42f1d2592b360ea1154ba
Jakub Matas <j.matas@cube-studio.cz>
2011-02-10 11:54:50 +0000

    CoreJS: přidání slovenského překladu pro TinyMCE plugin imgmap

---
+++ jscripts/tinymce/plugins/imgmap/langs/sk.js
@@ -0,0 +1,21 @@
+// CS lang variables
+
+tinyMCE.addI18n('cs.imgmap', {
+	title : 'Editor Obrázkové Mapy',
+	desc : 'Editor Obrázkové Mapy',
+	remove : 'Odstranit',
+   imgmapareas : 'klikací oblasti obrázku',
+   addnewarea : 'Přidat novou oblast',
+   deleteselarea : 'Odstranit vybranou oblast',
+   zoom : 'Zvětšení',
+   image : 'Obrázek',
+   status : 'Stav',
+   code : 'Kód',
+   boundingbox : 'ohraničující box',
+   nolabeling : 'Bez popisku',
+   labelwnumbers : 'Popis s čísly',
+   labelwalttext : 'Popis se zástupným textem',
+   labelwalthref : 'Popis s odkazem',
+   labelwaltcoords : 'Popis se souřadnicemi',
+   getimghtml : 'Vybrat HTML mapy obrázku'
+});
