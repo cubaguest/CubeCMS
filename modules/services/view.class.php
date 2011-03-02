0033daf41cedb737e472b96ce1ceee490223b576
Jakub Matas <j.matas@cube-studio.cz>
2011-03-02 10:05:09 +0000

    M_Services: Přidání nastavení editace pro šablony.

--- modules/services/view.class.php
+++ modules/services/view.class.php
@@ -7,14 +7,17 @@
 class Services_View extends View {
    public function mainView() {
       $this->template()->addTplFile("dirs.phtml");
+      Template_Module::setEdit(true);
    }
 
    public function databaseView() {
       $this->template()->addTplFile("database.phtml");
+      Template_Module::setEdit(true);
    }
 
    public function backupView() {
       $this->template()->addTplFile("backup.phtml");
+      Template_Module::setEdit(true);
    }
 
    public function tablesListView(){
