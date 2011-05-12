cf1e0f64ddf512b12ff9903e68a869da37a5cf3a
Jakub Matas <j.matas@cube-studio.cz>
2011-05-12 08:35:05 +0000

    M_Actions: Oprava výběru aktuální akce

--- modules/actions/model/detail.class.php
+++ modules/actions/model/detail.class.php
@@ -144,14 +144,14 @@ class Actions_Model_Detail extends Model_PDO {
       $dbst = $dbc->prepare("SELECT *, ABS(DATEDIFF(".self::COLUMN_DATE_START.",CURDATE())) AS delta_days  FROM ".Db_PDO::table(self::DB_TABLE)
               ." WHERE (".self::COLUMN_ID_CAT." = :idcat) AND (".self::COLUMN_PUBLIC." = 1)"
               ." AND ((ISNULL(".self::COLUMN_DATE_STOP.") AND ".self::COLUMN_DATE_START." >= CURDATE())"
-              ." OR (ISNULL(".self::COLUMN_DATE_STOP.") = 0 AND ".self::COLUMN_DATE_START." < CURDATE() AND ".self::COLUMN_DATE_STOP." > CURDATE()))"
+              ." OR (ISNULL(".self::COLUMN_DATE_STOP.") = 0 AND ".self::COLUMN_DATE_START." <= CURDATE() AND ".self::COLUMN_DATE_STOP." > CURDATE()))"
               ." ORDER BY delta_days, `".self::COLUMN_TIME."` LIMIT 0, 1");
       $dbst->bindParam(':idcat', $idCat, PDO::PARAM_INT);
       $dbst->execute();
       $dbst->setFetchMode(PDO::FETCH_CLASS, 'Model_LangContainer');
       return $dbst->fetch();
    }
-   
+
    public function deleteAction($idAction) {
       $dbc = new Db_PDO();
       $dbst = $dbc->prepare("DELETE FROM ".Db_PDO::table(self::DB_TABLE)
