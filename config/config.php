39c59cc7a262b8e49d3904f2b7fa4f8cee10be29
Jakub Matas <j.matas@cube-studio.cz>
2011-05-22 20:08:00 +0000

    Core: Změna výchozího konfiguračního souboru

--- config/config.php
+++ config/config.php
@@ -1,5 +1,5 @@
 <?php
-/* 
+/*
  * Main configuration FILE
  */
 
@@ -10,9 +10,9 @@ if(!defined('VVE_APP_IS_RUN')){
 // SQL server connection
 define('VVE_DB_TYPE', 'mysqli');
 define('VVE_DB_SERVER', 'localhost');
-define('VVE_DB_NAME', 'dev');
-define('VVE_DB_USER', 'dev');
-define('VVE_DB_PASSWD', 'E58frT6X');
+define('VVE_DB_NAME', 'cube_cms');
+define('VVE_DB_USER', 'cube_cms');
+define('VVE_DB_PASSWD', 'cube_cms');
 define('VVE_DB_PREFIX', 'vypecky_');
 
 ?>
