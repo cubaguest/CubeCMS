7e4bfbf21783ba8e3a64a9440ee1e6c474528454
Jakub Matas <j.matas@cube-studio.cz>
2011-05-22 19:38:14 +0000

    Core: Import příkladové subdomény

---
+++ subdomain/config/config.php
@@ -0,0 +1,18 @@
+<?php
+/*
+ * Main configuration FILE
+ */
+
+if(!defined('VVE_APP_IS_RUN')){
+   die ('Aplication not running');
+}
+
+// SQL server connection
+define('VVE_DB_TYPE', 'mysqli');
+define('VVE_DB_SERVER', 'localhost');
+define('VVE_DB_NAME', 'cube_cms');
+define('VVE_DB_USER', 'cube_cms');
+define('VVE_DB_PASSWD', 'cube_cms');
+define('VVE_DB_PREFIX', 'vypecky_sub_');
+
+?>
---
+++ subdomain/index.php
@@ -0,0 +1,24 @@
+<?php
+/*Hlaseni chyb na obrazavku pro jednoduchou diagnostiku ve vyvojovem prostredi...*/
+/**
+ * Vložení hlavní třídy aplikace
+ */
+$maintenance = false;
+$maintenanceTitle = null;
+$maintenanceMessage = null;
+$maintenanceDeadTime = '01:00 12.1.2011';
+$maintenanceContact = 'jakubmatas@gmail.com';
+$maintenanceAllowAccess = array('127.0.0.2');
+if(!file_exists('data/lock.tmp')){
+   if($maintenance == false OR in_array($_SERVER['REMOTE_ADDR'], $maintenanceAllowAccess)){
+      require_once ('../app.php');
+      AppCore::setAppMainLibDir(realpath(dirname('../app.php')));
+      AppCore::setAppMainDir(realpath(dirname(__FILE__)));
+      AppCore::createApp();
+   } else {
+      include 'templates/update.phtml';
+   }
+} else {
+   include 'templates/update.phtml';
+}
+?>
