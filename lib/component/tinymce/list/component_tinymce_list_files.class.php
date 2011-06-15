54be23bca02454dcb60f0f752397ba5fedbec320
Jakub Matas <j.matas@cube-studio.cz>
2011-06-15 20:19:37 +0000

    ComponentTinyMCE: Oprava kontroly existen adresáře public při tvorbě seznamu odkazů

--- lib/component/tinymce/list/component_tinymce_list_files.class.php
+++ lib/component/tinymce/list/component_tinymce_list_files.class.php
@@ -21,16 +21,15 @@ class Component_TinyMCE_List_Files extends Component_TinyMCE_List {
          }
       }
 
-      $dirIter = new RecursiveDirectoryIterator(AppCore::getAppDataDir().Component_TinyMCE_Browser::DIR_PUBLIC);
-      $iterator = new RecursiveIteratorIterator($dirIter);
-      $files = new RegexIterator($iterator, '/^.+\.'.$this->filesFilter.'$/i',RecursiveRegexIterator::GET_MATCH);
-
-
-      // public files
-      foreach($files as $file)
-      {
-         $file = str_replace(array(AppCore::getAppDataDir(), DIRECTORY_SEPARATOR), array('', '/'), $file[0]);
-         $this->addItem($this->tr('Data veřejná: ').$file, Url_Request::getBaseWebDir().VVE_DATA_DIR.'/'.$file);
+      if(is_dir(AppCore::getAppDataDir().Component_TinyMCE_Browser::DIR_PUBLIC)){
+         $dirIter = new RecursiveDirectoryIterator(AppCore::getAppDataDir().Component_TinyMCE_Browser::DIR_PUBLIC);
+         $iterator = new RecursiveIteratorIterator($dirIter);
+         $files = new RegexIterator($iterator, '/^.+\.'.$this->filesFilter.'$/i',RecursiveRegexIterator::GET_MATCH);
+         // public files
+         foreach($files as $file) {
+            $file = str_replace(array(AppCore::getAppDataDir(), DIRECTORY_SEPARATOR), array('', '/'), $file[0]);
+            $this->addItem($this->tr('Data veřejná: ').$file, Url_Request::getBaseWebDir().VVE_DATA_DIR.'/'.$file);
+         }
       }
    }
 
