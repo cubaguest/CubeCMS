92b8695339feb02830a0752bf52a297db6f9ec74
Jakub Matas <j.matas@cube-studio.cz>
2011-07-20 05:49:11 +0000

    Core: Implementace form elementu captcha a komponenty pro generování obrázků. Oprava stylu seznamů v hlavním textu

---
+++ lib/component/captcha/component_captcha.class.php
@@ -0,0 +1,115 @@
+<?php
+/** 
+ * Třída Komponenty pro tvorbu obrázku pro captchu
+ *
+ * @copyright  	Copyright (c) 2008-2009 Jakub Matas
+ * @version    	$Id: $ VVE 6.0.0 $Revision: $
+ * @author        $Author: $ $Date: $
+ *                $LastChangedBy: $ $LastChangedDate: $
+ * @abstract 		Třída Komponenty pro tvorbu pdf souborů
+ */
+
+class Component_Captcha extends Component {
+
+   protected $config = array(
+      'rand' => null,
+      'salt' => null,
+      'width' => 100,
+      'height' => 25,
+      'baseimage' => null,
+      'session' => 'captcha',
+   );
+
+      /**
+    * Konstruktor třídy, spouští metodu init();
+    */
+   function __construct($runOnly = false)
+   {
+      $this->componentName = str_ireplace(__CLASS__.'_', '', get_class($this));
+      $this->setConfig('baseimage', AppCore::getAppWebDir().'images'.DIRECTORY_SEPARATOR.'captcha.jpg');
+   }
+
+   /**
+    * Metoda inicializace, je spuštěna pří vytvoření objektu
+    */
+   protected function init() {}
+
+   /**
+    * Metoda provede validaci captchy
+    * @param string $str -- řetězec
+    * @return bool
+    */
+   public function validate($str)
+   {
+      return $_SESSION[$this->getConfig('session')] == $str;
+   }
+
+   /**
+    * Metoda pro spouštění některých akcí přímo v kontroleru
+    */
+   public function runCtrlPart() {}
+
+   /**
+    * Spuštění komponenty (hlavní kontroler)
+    */
+   public function mainController() {}
+   
+   /**
+    * Metoda provede vykreslení komponenty
+    */
+   public function mainView() {}
+   
+   private function generateRandom($length=6) 
+   { 
+    $_rand_src = array( 
+        array(48,57) //digits 
+        , array(97,122) //lowercase chars 
+//        , array(65,90) //uppercase chars 
+    ); 
+    srand ((double) microtime() * 1000000); 
+    $random_string = ""; 
+    for($i=0;$i<$length;$i++){ 
+        $i1=rand(0,sizeof($_rand_src)-1); 
+        $random_string .= chr(rand($_rand_src[$i1][0],$_rand_src[$i1][1])); 
+    } 
+    return $random_string; 
+   } 
+   
+   /**
+    * Spuštění komponenty (hlavní kontroler)
+    */
+   public function imageJpgController()
+   {
+      $this->setConfig('rand', $this->generateRandom(3));
+      $this->setConfig('salt', $this->generateRandom(3));
+//      $_SESSION[$this->getConfig('session')][Category::getSelectedCategory()->getId()] = $this->getConfig('rand'); // pokud jich bude na stránce více
+      $_SESSION[$this->getConfig('session')] = $this->getConfig('rand');
+   }
+   
+   public function imageJpgView()
+   {
+      $image = @imagecreatefromjpeg($this->getConfig('baseimage'));  
+      
+      $rand = $this->getConfig('salt'); 
+      imagestring($image, 5, 2, 2, $rand[0]." ".$rand[1]." ".$rand[2]." ", imagecolorallocate($image, 0, 0, 0)); 
+      $rand = $this->getConfig('rand'); 
+      imagestring($image, 5, 2, 2, " ".$rand[0]." ".$rand[1]." ".$rand[2], imagecolorallocate($image, 255, 0, 0)); 
+      Template_Output::setOutputType('jpeg');
+      Template_Output::sendHeaders();
+      imagejpeg($image);
+      imagedestroy($image);
+      die();
+   }
+
+   /**
+    * Metoda vrací adresu k obrázku pro captchu
+    * @return type 
+    */
+   public static function getImage()
+   {
+      $link = new Url_Link_Component('captcha');
+      return $link->onlyAction('image', 'jpg');
+   }
+   
+}
+?>
---
+++ lib/form/element/form_element_captcha.class.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Třída pro obsluhu INPUT prvku typu TEXT s captchou
+ *
+ * @copyright  	Copyright (c) 2008 Jakub Matas
+ * @version    	$Id: $ VVE 6.0 $Revision: $
+ * @author        $Author: $ $Date: $
+ *                $LastChangedBy: $ $LastChangedDate: $
+ * @abstract      Třída pro obsluhu formulářového prvku typu Input-Text
+ */
+class Form_Element_Captcha extends Form_Element_Text {
+   
+   public function __construct($name, $label = null, $prefix = null)
+   {
+      if($label == null){
+         $label = $this->tr('Ověření');
+      }
+      parent::__construct($name, $label, $prefix);
+      $this->addValidation(new Form_Validator_NotEmpty($this->tr('Ověření formuláře nebylo vylpněno')));
+      $this->addValidationConditionLabel($this->tr('Opište červené znaky'));
+   }
+   
+   public function validate()
+   {
+      parent::validate();
+      // validace captchy
+      if($this->isValid){
+         $capctcha = new Component_Captcha();
+         if($capctcha->validate($this->getUnfilteredValues()) == false){
+            $this->isValid = false;
+            $this->errMsg()->addMessage($this->tr('Kontrolní kód nebyl správně opsán'));
+         }
+      }
+   }
+
+   /**
+ * Metoda vrací prvek (html element podle typu elementu - input, textarea, ...)
+ * @return string
+ */
+   public function controll() {
+      $this->html()->setAttrib('type', 'text');
+      $this->createValidationLabels();
+      $this->html()->clearContent();
+      if(!$this->isValid AND $this->isPopulated) {
+         $this->html()->addClass(self::$cssClasses['error']);
+         if(!self::$elementFocused){ $this->html()->setAttrib('autofocus','autofocus'); self::$elementFocused = true;}
+      }
+      $this->html()->addClass($this->getName()."_class")->addClass('captcha');
+      $this->html()->setAttrib('name', $this->getName());
+      $this->html()->setAttrib('id', $this->getName().'_'.$this->renderedId);
+      $this->html()->setAttrib('value', null);
+      
+      // image
+      $image = new Html_Element('img');
+      $image->setAttrib('src', Component_Captcha::getImage())
+         ->setAttrib('alt', 'captcha')
+         ->addClass('captcha');
+      
+      $this->renderedId++;
+      return $this->html().$image;
+   }
+}
+?>
