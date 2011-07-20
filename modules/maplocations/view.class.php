86a185f55afab441a10b09584799c86af68f67a5
Jakub Matas <j.matas@cube-studio.cz>
2011-07-20 09:43:49 +0000

    M_MapLocations: Změna ikon na dům (import ikon domů a jeho úprav)

--- modules/maplocations/view.class.php
+++ modules/maplocations/view.class.php
@@ -20,7 +20,7 @@ class MapLocations_View extends View {
          $this->toolbox->setIcon(Template_Toolbox2::ICON_WRENCH);
          $toolEdit = new Template_Toolbox2_Tool_PostRedirect('list_palces', 
                  $this->tr("Upravit místa"), $this->link()->route('list'));
-         $toolEdit->setIcon('page_edit.png')->setTitle($this->tr('Upravit místa na mapě'));
+         $toolEdit->setIcon('house_edit.png')->setTitle($this->tr('Upravit místa na mapě'));
          $this->toolbox->addTool($toolEdit);
       }
    }
@@ -31,7 +31,7 @@ class MapLocations_View extends View {
          $this->toolbox->setIcon(Template_Toolbox2::ICON_WRENCH);
          $toolAdd = new Template_Toolbox2_Tool_PostRedirect('add_location', $this->tr("Přidat místo"),
          $this->link()->route('add'));
-         $toolAdd->setIcon('page_add.png')->setTitle($this->tr('Přidat nové místo'));
+         $toolAdd->setIcon('house_add.png')->setTitle($this->tr('Přidat nové místo'));
          $this->toolbox->addTool($toolAdd);
          
          $toolClose = new Template_Toolbox2_Tool_PostRedirect('close_location_list', $this->tr("Zavřít seznam"), $this->link()->route());
@@ -42,11 +42,11 @@ class MapLocations_View extends View {
          $this->toolboxItem->setIcon(Template_Toolbox2::ICON_ADD);
          $toolEdit = new Template_Toolbox2_Tool_PostRedirect('edit_location', $this->tr("Upravit místo"),
          $this->link()->route('edit'));
-         $toolEdit->setIcon('page_edit.png')->setTitle($this->tr('Upravit vybrané místo'));
+         $toolEdit->setIcon('house_edit.png')->setTitle($this->tr('Upravit vybrané místo'));
          $this->toolboxItem->addTool($toolEdit);
          
          $toolDelete = new Template_Toolbox2_Tool_Form($this->formDel);
-         $toolDelete->setIcon('page_delete.png')->setTitle($this->tr('Smazat vybrané místo'))->setConfirmMeassage($this->tr('Opravdu smazat tuto položku?'));
+         $toolDelete->setIcon('house_delete.png')->setTitle($this->tr('Smazat vybrané místo'))->setConfirmMeassage($this->tr('Opravdu smazat tuto položku?'));
          $this->toolboxItem->addTool($toolDelete);
       }
    }
