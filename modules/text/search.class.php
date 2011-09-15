eba1cb2208fcc8de75ff805dbed9a488326fc8ab
Jakub Matas <j.matas@cube-studio.cz>
2011-09-15 08:37:24 +0000

    M_Text: Oprava hledání pouze na obsah hlavního textu

--- modules/text/search.class.php
+++ modules/text/search.class.php
@@ -3,7 +3,8 @@ class Text_Search extends Search {
 	public function runSearch() {
       $model = new Text_Model();
       $result = $model
-         ->where(Text_Model::COLUMN_ID_CATEGORY.' = :idc', array('idc' => $this->category()->getId()))
+         ->where(Text_Model::COLUMN_ID_CATEGORY.' = :idc AND '.Text_Model::COLUMN_SUBKEY.' = :sk', 
+            array('idc' => $this->category()->getId(), 'sk' => Text_Controller::TEXT_MAIN_KEY))
          ->search($this->getSearchString());
       if($result != false){
          foreach ($result as $res) {
