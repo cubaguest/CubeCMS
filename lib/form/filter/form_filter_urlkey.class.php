7aa4f2d6ba12588102235363fa410acb4ce50990
Jakub Matas <j.matas@cube-studio.cz>
2011-10-07 05:02:08 +0000

    Core: Přidání form filtru URL klíče

---
+++ lib/form/filter/form_filter_urlkey.class.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Třída přefiltruje element a vytvoří v něm validní url adresu (http,ftp)
+ *
+ * @copyright  	Copyright (c) 2008-2009 Jakub Matas
+ * @version    	$Id: $ VVE6.0.0 $Revision: $
+ * @author        $Author: $ $Date: $
+ *                $LastChangedBy: $ $LastChangedDate: $
+ * @abstract 		Třída fitru pro vytvoření validní url adresy
+ */
+class Form_Filter_UrlKey extends Form_Filter {
+
+   public function  __construct() {
+   }
+
+   /**
+    * Metoda aplikuje filtr na daný element
+    * @param Form_Element $elem
+    */
+   public function filter(Form_Element &$elem, &$values) {
+//      switch (get_class($elem)) {
+//         case "Form_Element_Text":
+//         case "Form_Element_TextArea":
+            $this->filterValue($values);
+//            break;
+//         default:
+//            break;
+//      }
+   }
+
+   /**
+    * Metoda provede filtraci
+    * @param string/array $variable
+    */
+   private function filterValue(&$variable) {
+      if(is_array($variable)){
+         foreach ($variable as &$var) {
+            $this->filterValue($var);
+         }
+      } else {
+         $variable = vve_cr_url_key($variable);
+      }
+   }
+}
+?>
