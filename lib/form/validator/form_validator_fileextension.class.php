3a6e0d1f0525b0571570bc1d00579d6a273ec156
Jakub Matas <j.matas@cube-studio.cz>
2011-10-24 19:33:02 +0000

    Core: Vylepšení validátoru přípon souborů (obsahuje nyní předdefinované typy pomocí konstant třídy)

--- lib/form/validator/form_validator_fileextension.class.php
+++ lib/form/validator/form_validator_fileextension.class.php
@@ -1,15 +1,34 @@
 <?php
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
- * Description of form_validate_noemptyclass
- *
- * @author jakub
+ * Validátor přípony nahrávaného souboru
+ * 
+ * @copyright  	Copyright (c) 2008-2011 Jakub Matas
+ * @version    	$Id: $ Cube CMS 7.5 $Revision: $
+ * @author        $Author: $ $Date: $
+ *                $LastChangedBy: $ $LastChangedDate: $
+ * @abstract 		Valídátor souborů po uploadu
+ * 
+ * @todo          Doplnit další formáty přípon
  */
 class Form_Validator_FileExtension extends Form_Validator implements Form_Validator_Interface {
+   const DOC = 1;
+   const IMG = 2;
+   const VID = 4;
+   const AUD = 8;
+   const BIN = 16;
+   const ARCH = 32;
+   
+   const ALL = 512;
+   
+   /**
+    * Soubory stránek (NENÍ BEZPEČNÉ !!!!)
+    */
+   const WEB = 1024;
+   /**
+    * Vše (NENÍ BEZPEČNÉ !!!!)
+    */
+   const ALL_NO_SAFE = 2048;
+
    /**
     * Pole s povolenými typ souborů
     * @var array
@@ -18,8 +37,42 @@ class Form_Validator_FileExtension extends Form_Validator implements Form_Valida
 
    public function  __construct($extensions, $errMsg = null) {
       if(!is_array($extensions)){
-         $extensions = str_replace(' ', null, $extensions);
-         $extensions = explode(';', $extensions);
+         if(is_int($extensions)){ // konstanty třídy
+            $cur = $extensions;
+            $extensions = array();
+            
+            if(($cur-self::WEB) >= 0){
+               $extensions = array_merge($extensions, array("htm","html","xhtml","xml","css","php","js"));
+               $cur -= self::WEB;
+            }
+            if(($cur-self::ARCH) >= 0){
+               $extensions = array_merge($extensions, array("zip", "rar", "7z", "bz2", "gz"));
+               $cur -= self::ARCH;
+            }
+            if(($cur-self::BIN) >= 0){
+               $extensions = array_merge($extensions, array("exe", "bat", "com", "bin", "sh"));
+               $cur -= self::BIN;
+            }
+            if(($cur-self::AUD) >= 0){
+               $extensions = array_merge($extensions, array("mp3","wav","wma","flac","ogg"));
+               $cur -= self::AUD;
+            }
+            if(($cur-self::VID) >= 0){
+               $extensions = array_merge($extensions, array("swf","flv","avi","wmv","mov"));
+               $cur -= self::VID;
+            }
+            if(($cur-self::IMG) >= 0){
+               $extensions = array_merge($extensions, array("swf","jpg","jpeg","gif","png","bmp","wmf"));
+               $cur -= self::IMG;
+            }
+            if(($cur-self::DOC) >= 0){
+               $extensions = array_merge($extensions, array("txt","doc","xls","rtf","ppt","pdf","csv","docx","odf"));
+               $cur -= self::DOC;
+            }
+         } else {
+            $extensions = str_replace(' ', null, $extensions);
+            $extensions = explode(';', $extensions);
+         }
       }
       $this->extensions = $extensions;
 
@@ -35,12 +88,11 @@ class Form_Validator_FileExtension extends Form_Validator implements Form_Valida
     * @param Form_Element $element -- samotný element
     */
    public function addHtmlElementParams(Form_Element $element) {
-      $mimes = null;
-      foreach ($this->extensions as $type) {
-         $mimes .= $type.", ";
+      if(count($this->extensions) > 5){
+         $mimes = implode(', ', array_slice($this->extensions, 0, 5) ).',<span title="'.implode(', ', $this->extensions).'"> ...</span>';
+      } else {
+         $mimes = implode(', ', $this->extensions);
       }
-      $mimes = substr($mimes, 0, strlen($mimes)-2);
-
       $element->addValidationConditionLabel(sprintf($this->tr("soubor s příponou %s"),$mimes));
    }
 
