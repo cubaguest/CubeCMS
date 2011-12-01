90fadef0233fbe16bfe07f73b7d65a842374ccb5
Jakub Matas <j.matas@cube-studio.cz>
2011-12-01 07:14:12 +0000

    M_Projects: Oprava instalačního sql dotazu

--- modules/projects/install/install.sql
+++ modules/projects/install/install.sql
@@ -3,29 +3,29 @@
 --
 -- Struktura tabulky `projects`
 --
-CREATE TABLE IF NOT EXISTS `{PREFIX}projects` (
+CREATE TABLE `{PREFIX}projects` (
   `id_project` int(11) NOT NULL AUTO_INCREMENT,
   `id_project_section` int(11) NOT NULL,
   `id_user` smallint(6) NOT NULL,
-  `project_name` varchar(200) CHARACTER SET utf8 COLLATE utf8_czech_ci NOT NULL,
+  `project_name` varchar(300) CHARACTER SET utf8 COLLATE utf8_czech_ci NOT NULL,
+  `project_name_short` varchar(100) CHARACTER SET utf8 COLLATE utf8_czech_ci DEFAULT NULL,
   `project_urlkey` varchar(200) CHARACTER SET utf8 COLLATE utf8_czech_ci NOT NULL,
   `project_text` text CHARACTER SET utf8 COLLATE utf8_czech_ci,
   `project_text_clear` text CHARACTER SET utf8 COLLATE utf8_czech_ci,
   `project_time_add` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `project_image` tinyint(1) NOT NULL DEFAULT '0',
   `project_related` varchar(200) DEFAULT NULL,
+  `project_weight` smallint(6) DEFAULT '0',
   PRIMARY KEY (`id_project`),
   KEY `id_user` (`id_user`,`project_urlkey`),
   FULLTEXT KEY `name` (`project_name`,`project_text_clear`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
+) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 --
 -- Struktura tabulky `projects_sections`
 --
 
-CREATE TABLE IF NOT EXISTS `{PREFIX}projects_sections` (
+CREATE TABLE `{PREFIX}projects_sections` (
   `id_project_section` smallint(6) NOT NULL AUTO_INCREMENT,
   `id_category` smallint(6) NOT NULL,
   `section_name` varchar(200) CHARACTER SET utf8 COLLATE utf8_czech_ci NOT NULL,
@@ -33,6 +33,8 @@ CREATE TABLE IF NOT EXISTS `{PREFIX}projects_sections` (
   `section_text` text CHARACTER SET utf8 COLLATE utf8_czech_ci,
   `section_text_clear` text CHARACTER SET utf8 COLLATE utf8_czech_ci,
   `section_time_add` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `section_weight` smallint(6) DEFAULT '0',
   PRIMARY KEY (`id_project_section`),
   KEY `id_category` (`id_category`,`section_urlkey`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+
