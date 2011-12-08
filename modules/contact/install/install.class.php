c5b811e88641860a1bb2af7df80c726eadc6e353
Jakub Matas <j.matas@cube-studio.cz>
2011-12-08 09:03:23 +0000

    M_Contact: Oprava instalátoru

--- modules/contact/install/install.class.php
+++ modules/contact/install/install.class.php
@@ -1,17 +1,6 @@
 <?php
-class Contact_Install extends Module_Install {
+class Contact_Install extends Install_Module {
    
-   public function install() {
-   }
-
-   public function  update($fromVersion, $toVersion) {
-      switch ($fromVersion) {
-         case '1.0':
-            $this->runSQLCommand($this->getSQLFileContent('install.sql'));
-            parent::update($fromVersion, $toVersion);
-            break;
-      }
-   }
 }
 
 ?>
