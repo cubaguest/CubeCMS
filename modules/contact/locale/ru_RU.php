87d0a9c9f7cd270515b3e46c6f7e1e662d00ed08
Jakub Matas <j.matas@cube-studio.cz>
2012-01-20 09:30:58 +0000

    M_Contact: Přidání ruského překladu

---
+++ modules/contact/locale/ru_RU.php
@@ -0,0 +1,28 @@
+<?php
+$singular = 
+array (
+  'Úprava kontaktu' => 'To edit a contact',
+  'Mapa jak se k nám dostanete' => 'Карта как нас найти',
+  'Mapa' => 'карта',
+  'Napište nám' => 'Пишите нам',
+  'Více' => 'больше',
+  'Jméno a přijmení' => 'Имя и фамилия',
+  'Váš e-mail' => 'Ваш адрес электронной почты',
+  'Téma' => 'тема',
+  '< Vlastní předmět >' => '< пользовательский объект >',
+  'Předmět' => 'тема',
+  'Text' => 'текст',
+  'Odeslat' => 'послать',
+  'Musí být zadán předmět zprávy.' => 'Должны быть указаны предмет.',
+  'Váš dotaz byl úspěšně odeslán. Co nejdříve Vám odpovíme.' => 'Ваш запрос был успешно отправлен. Как только вы ответите.',
+  'Text kontaktu' => 'Contact Text',
+  'Text panelu' => 'Text panel',
+  'Pokud není vyplněn, zkusí se použít první odstavec typu adresa z hlavního textu.' => 'If not filled, try to use the first paragraph of the type of address from the main text.',
+  'Změny byly zrušeny' => 'The changes were abolished',
+  'Text kontaktu byl uložen' => 'Text contact was deposited',
+  'Upravit kontakt' => 'Edit Contact',
+);
+$plural = 
+array (
+);
+?>
