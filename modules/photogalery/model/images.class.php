04dd3a3d85210efd308b5d67d95ef00c3bb3fbcb
Jakub Matas <j.matas@cube-studio.cz>
2012-01-20 09:30:30 +0000

    M_photogalery: opravy stylů admin textů, Implementace ORM modelu pro obrázky

--- modules/photogalery/model/images.class.php
+++ modules/photogalery/model/images.class.php
@@ -2,7 +2,7 @@
 /*
  * Třída modelu s detailem galerie
 */
-class PhotoGalery_Model_Images extends Model_PDO {
+class PhotoGalery_Model_Images extends Model_ORM {
    const DB_TABLE = 'photogalery_images';
    /**
     * Názvy sloupců v databázi pro tabulku s obrázky
@@ -17,6 +17,26 @@ class PhotoGalery_Model_Images extends Model_PDO {
    const COLUMN_FILE 				= 'file';
    const COLUMN_ORDER 				= 'ord';
 
+   protected function  _initTable() {
+      $this->setTableName(self::DB_TABLE, 't_ph_imgs');
+
+      $this->addColumn(self::COLUMN_ID, array('datatype' => 'smallint', 'ai' => true, 'nn' => true, 'pk' => true));
+      $this->addColumn(self::COLUMN_ID_CAT, array('datatype' => 'smallint', 'nn' => true, 'pdoparam' => PDO::PARAM_INT));
+      $this->addColumn(self::COLUMN_ID_ART, array('datatype' => 'smallint', 'nn' => true, 'pdoparam' => PDO::PARAM_INT));
+      
+      $this->addColumn(self::COLUMN_FILE, array('datatype' => 'varchar(200)', 'nn' => true, 'pdoparam' => PDO::PARAM_STR));
+      $this->addColumn(self::COLUMN_NAME, array('datatype' => 'varchar(300)', 'nn' => true, 'lang' => true, 'pdoparam' => PDO::PARAM_STR, 
+         'fulltext' => true, 'fulltextRel' => VVE_SEARCH_ARTICLE_REL_MULTIPLIER));
+      $this->addColumn(self::COLUMN_DESC, array('datatype' => 'text', 'lang' => true, 'pdoparam' => PDO::PARAM_STR));
+
+      $this->addColumn(self::COLUMN_TIME_EDIT, array('datatype' => 'timestamp', 'pdoparam' => PDO::PARAM_STR, 'default' => 'CURRENT_TIMESTAMP'));
+      $this->addColumn(self::COLUMN_ORDER, array('datatype' => 'smallint', 'pdoparam' => PDO::PARAM_INT, 'default' => 0));
+
+      $this->setPk(self::COLUMN_ID);
+      
+      $this->addForeignKey(self::COLUMN_ID_CAT, 'Model_Categories', Model_Category::COLUMN_CAT_ID);
+   }
+   
    /**
     *
     * @param <type> $idCat
@@ -165,6 +185,78 @@ class PhotoGalery_Model_Images extends Model_PDO {
       return $dbst->execute();
    }
 
+   
+   /* compatibility */
+   /**
+    * Metoda nastaví pole modelu pro vytvoření řetězců pro insert update
+    * @param array $columns -- pole s názvy sloupců a hodotami
+    * @param char $separator -- oddělovač mezi názvy sloupců pokud jsou v poli (option default: '_')
+    */
+   protected function setIUValues($columns, $separator = '_', $clPrefix = null) {
+      foreach ($columns as $clKey => $clVal) {
+         if(!is_null($clPrefix)) {
+            $prefix = $clPrefix.$separator;
+         } else {
+            $prefix = null;
+         }
+         if(is_array($clVal)) {
+            $this->setIUValues($clVal, $separator, $prefix.$clKey);
+         } else {
+            $this->insUpdtValues[$prefix.$clKey] = $clVal;
+         }
+      }
+   }
+   /**
+    * Metoda vrací řetězec s názvy sloupců pro vložení do insertu
+    * @return string
+    */
+   public function getInsertLabels($separator = '_') {
+      $returnStr = "(";
+      foreach (array_keys($this->insUpdtValues) as $variable) {
+         //         $returnStr .= '´'.$variable.'´, ';
+         $returnStr .= $variable.', ';
+      };
+      return substr($returnStr, 0, strlen($returnStr)-2).")";
+   }
+   /**
+    * Metoda vrací řetězec pro příkaz update
+    * @return string
+    */
+   public function getUpdateValues() {
+      $pdo = new Db_PDO();//`label_cs`= 'Saul Griffith's lofty',
+      $returnStr = null;
+      //      var_dump($this->insUpdtValues);
+      foreach ($this->insUpdtValues as $key => $variable) {
+         //         $returnStr .= '`'.$key.'` = '.$pdo->quote($variable).", ";
+         if($variable == null) {
+            $var = 'NULL';
+         } else {
+            $var = $pdo->quote($variable);
+         }
+         $returnStr .= $key.' = '.$var.", ";
+      };
+      return substr($returnStr, 0, strlen($returnStr)-2);
+   }
+   /**
+    * Metoda vrací řetězec s názvy sloupců pro vložení do insertu
+    * @return string
+    */
+   public function getInsertValues() {
+      $pdo = new Db_PDO();
+      $returnStr = "(";
+      foreach (array_values($this->insUpdtValues) as $variable) {
+         if(is_bool($variable) AND $variable) {
+            $returnStr .= '1, ';
+         } else if(is_bool($variable) AND !$variable) {
+            $returnStr .= '0, ';
+         } else if($variable == null OR $variable == '') {
+            $returnStr .= "NULL, ";
+         } else {
+            $returnStr .= $pdo->quote($variable).", ";
+         }
+      };
+      return substr($returnStr, 0, strlen($returnStr)-2).")";
+   }
 }
 
 ?>
