870d4f790788dcac354e66ccc0d0a6dfefd80cbe
Jakub Matas <j.matas@cube-studio.cz>
2012-01-28 10:32:55 +0000

    CoreLocales: Oprava překladů, přidání polského překladu (zatím nekompletního)

---
+++ modules/contact/locale/pl_PL.php
@@ -0,0 +1,28 @@
+<?php
+$singular = 
+array (
+//  'Úprava kontaktu' => '',
+  'Mapa jak se k nám dostanete' => 'Mapa jak do nas trafić',
+  'Mapa' => 'Mapa',
+  'Napište nám' => 'Napisz do nas',
+  'Více' => 'Więcej',
+  'Jméno a přijmení' => 'Imię i nazwiskoe',
+  'Váš e-mail' => 'Twój e-mail',
+  'Téma' => 'Temat',
+  '< Vlastní předmět >' => '< Własnego obiektu >',
+  'Předmět' => 'Temat',
+  'Text' => 'Tekst',
+  'Odeslat' => 'Wysłać',
+  'Musí být zadán předmět zprávy.' => 'Musi być określony przedmiot.',
+  'Váš dotaz byl úspěšně odeslán. Co nejdříve Vám odpovíme.' => 'Twoje zapytanie zostało wysłane z sukcesem. Jak tylko odpowiedź.',
+  'Text kontaktu' => 'Contact Text',
+  'Text panelu' => 'Text panel',
+  'Pokud není vyplněn, zkusí se použít první odstavec typu adresa z hlavního textu.' => 'If not filled, try to use the first paragraph of the type of address from the main text.',
+//  'Změny byly zrušeny' => 'The changes were abolished',
+//  'Text kontaktu byl uložen' => 'Text contact was deposited',
+//  'Upravit kontakt' => 'Edit Contact',
+);
+$plural = 
+array (
+);
+?>
