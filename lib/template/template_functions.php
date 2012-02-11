d2718e32fd76ae2a221cd96f74194ae013a7d847
Jakub Matas <j.matas@cube-studio.cz>
2012-02-11 08:58:25 +0000

    Core: přepsání funkce na omezení html řetězců

--- lib/template/template_functions.php
+++ lib/template/template_functions.php
@@ -34,99 +34,105 @@ function vve_get_tpl_file($file, $type) {
  * @param string $link -- (option) odkaz na více za textem
  * @param string $linkText -- (option) text textu pro více
  * @return string -- zkrácený řetězec se správně uzavřenými značkami
- * @author Jakub Vrána & Mike
- * @link http://php.vrana.cz/zkraceni-textu-s-xhtml-znackami.php#d-9638
+ * @author Gabi Solomon
+ * @link http://www.gsdesign.ro/blog/cut-html-string-without-breaking-the-tags/
  */
-function vve_tpl_xhtml_cut($string, $length, $addstring = '...', $link = null, $link_text = null)
+function vve_tpl_xhtml_cut($text, $length = 100, $ending = '...', $exact = false, $considerHtml = false)
 {
    // only execute if text is longer than desired length
-   if (strlen(strip_tags($string)) > $length) {
-      $isText = true;
-      $ret = "";
-      $i = 0;
-
-      $currentChar = "";
-      $lastSpacePosition = -1;
-      $lastChar = "";
-
-      $tagsArray = array();
-      $currentTag = "";
-      $tagLevel = 0;
-
-      $noTagLength = strlen(strip_tags($string));
-
-      // Parser loop
-      for ($j = 0; $j < strlen($string); $j++) {
-         $currentChar = substr($string, $j, 1);
-         $ret .= $currentChar;
-         // Lesser than event
-         if ($currentChar == "<")
-            $isText = false;
-         // Character handler
-         if ($isText) {
-            // Memorize last space position
-            if ($currentChar == " ") {
-               $lastSpacePosition = $j;
-            } else {
-               $lastChar = $currentChar;
-            }
-            $i++;
-         } else {
-            $currentTag .= $currentChar;
+   if (strlen(strip_tags($text)) > $length) {
+      if ($considerHtml) {
+         // if the plain text is shorter than the maximum length, return the whole text
+         if (strlen(preg_replace('/<.*?>/', '', $text)) <= $length) {
+            return $text;
          }
+         // splits all html-tags to scanable lines
+         preg_match_all('/(<.+?>)?([^<>]*)/s', $text, $lines, PREG_SET_ORDER);
 
-         // Greater than event
-         if ($currentChar == ">") {
-            $isText = true;
-            // Opening tag handler
-            if (( strpos($currentTag, "<") !== FALSE ) &&
-               ( strpos($currentTag, "/>") === FALSE ) &&
-               ( strpos($currentTag, "</") === FALSE )) {
-               // Tag has attribute(s)
-               if (strpos($currentTag, " ") !== FALSE) {
-                  $currentTag = substr($currentTag, 1, strpos($currentTag, " ") - 1);
-               } else {
-                  // Tag doesn't have attribute(s)
-                  $currentTag = substr($currentTag, 1, -1);
+         $total_length = strlen($ending);
+         $open_tags = array();
+         $truncate = '';
+
+         foreach ($lines as $line_matchings) {
+            // if there is any html-tag in this line, handle it and add it (uncounted) to the output
+            if (!empty($line_matchings[1])) {
+               // if it's an "empty element" with or without xhtml-conform closing slash (f.e. <br/>)
+               if (preg_match('/^<(\s*.+?\/\s*|\s*(img|br|input|hr|area|base|basefont|col|frame|isindex|link|meta|param)(\s.+?)?)>$/is', $line_matchings[1])) {
+                  // do nothing
+                  // if tag is a closing tag (f.e. </b>)
+               } else if (preg_match('/^<\s*\/([^\s]+?)\s*>$/s', $line_matchings[1], $tag_matchings)) {
+                  // delete tag from $open_tags list
+                  $pos = array_search($tag_matchings[1], $open_tags);
+                  if ($pos !== false) {
+                     unset($open_tags[$pos]);
+                  }
+                  // if tag is an opening tag (f.e. <b>)
+               } else if (preg_match('/^<\s*([^\s>!]+).*?>$/s', $line_matchings[1], $tag_matchings)) {
+                  // add tag to the beginning of $open_tags list
+                  array_unshift($open_tags, strtolower($tag_matchings[1]));
                }
-
-               array_push($tagsArray, $currentTag);
-            } else if (strpos($currentTag, "</") !== FALSE) {
-               array_pop($tagsArray);
+               // add html-tag to $truncate'd text
+               $truncate .= $line_matchings[1];
             }
+            // calculate the length of the plain text part of the line; handle entities as one character
+            $content_length = strlen(preg_replace('/&[0-9a-z]{2,8};|&#[0-9]{1,7};|&#x[0-9a-f]{1,6};/i', ' ', $line_matchings[2]));
+            if ($total_length + $content_length > $length) {
+               // the number of characters which are left
+               $left = $length - $total_length;
+               $entities_length = 0;
+               // search for html entities
+               if (preg_match_all('/&[0-9a-z]{2,8};|&#[0-9]{1,7};|&#x[0-9a-f]{1,6};/i', $line_matchings[2], $entities, PREG_OFFSET_CAPTURE)) {
+                  // calculate the real length of all entities in the legal range
+                  foreach ($entities[0] as $entity) {
+                     if ($entity[1] + 1 - $entities_length <= $left) {
+                        $left--;
+                        $entities_length += strlen($entity[0]);
+                     } else {
+                        // no more characters left
+                        break;
+                     }
+                  }
+               }
+               $truncate .= substr($line_matchings[2], 0, $left + $entities_length);
+               // maximum lenght is reached, so get off the loop
+               break;
+            } else {
+               $truncate .= $line_matchings[2];
+               $total_length += $content_length;
+            }
+            // if the maximum length is reached, get off the loop
+            if ($total_length >= $length) {
+               break;
+            }
+         }
+      } else {
+         if (strlen($text) <= $length) {
+            return $text;
+         } else {
+            $truncate = substr($text, 0, $length - strlen($ending));
+         }
+      }
+      // if the words shouldn't be cut in the middle...
+      if (!$exact) {
+         // ...search the last occurance of a space...
+         $spacepos = strrpos($truncate, ' ');
+         if (isset($spacepos)) {
+            // ...and cut the text in this position
+            $truncate = substr($truncate, 0, $spacepos);
+         }
+      }
+      // add the defined ending to the text
+      $truncate .= $ending;
 
-            $currentTag = "";
-         }
-         if ($i >= $length) {
-            break;
-         }
-      }
-      // Cut HTML string at last space position
-      if ($length < $noTagLength) {
-         if ($lastSpacePosition != -1) {
-            $ret = substr($string, 0, $lastSpacePosition);
-         } else {
-            $ret = substr($string, $j);
+      if ($considerHtml) {
+         // close all unclosed html-tags
+         foreach ($open_tags as $tag) {
+            $truncate .= '</' . $tag . '>';
          }
       }
-      // Close broken XHTML elements
-      while (sizeof($tagsArray) != 0) {
-         $aTag = array_pop($tagsArray);
-         if (sizeof($tagsArray) > 1) {
-            $ret .= "</" . $aTag . ">\n";
-         }
-         // You may add more tags here to put the link and added text before the closing tag
-         elseif ($aTag == 'p' || 'div') {
-            $aTag = array_pop($tagsArray);
-            $ret .= $addstring . "<a href=\"" . $link . "\" alt=\"" . $link_text . "\">" . $link_text . "</a></" . $aTag . ">\n";
-         } else {
-            $aTag = array_pop($tagsArray);
-            $ret .= "</" . $aTag . ">" . $addstring . "<a href=\"" . $link . "\" alt=\"" . $link_text . "\">" . $link_text . "</a>\n";
-         }
-      }
-      return( $ret );
+      return $truncate;
    } else {
-      return ( $string );
+      return ( $text );
    }
 }
 
