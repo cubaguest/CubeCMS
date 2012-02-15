68a6fd6695a3ee850e5dc8c98d68230ea0192415
Jakub Matas <j.matas@cube-studio.cz>
2012-02-15 11:05:31 +0000

    Core: Oprava systému uživatelských práv. Oprava detekce práva. Oprava výpisů podle práv. Přidání core modulu pro Zamíttnutí přístupu. Oprava Výpisu odkazů kategorí pro tinymce

--- lib/component/tinymce/list/component_tinymce_list_categories.class.php
+++ lib/component/tinymce/list/component_tinymce_list_categories.class.php
@@ -7,19 +7,25 @@ class Component_TinyMCE_List_Categories extends Component_TinyMCE_List {
 
    protected function  loadItems() {
       $model = new Model_Category();
-
-      $cats = $model->join(Model_Category::COLUMN_CAT_ID, array('t_r' => 'Model_Rights'), null,
-                  array(Model_Rights::COLUMN_ID_GROUP, Model_Rights::COLUMN_RIGHT))
-               ->where('t_r.' . Model_Rights::COLUMN_ID_GROUP . ' = :idgrp'
-                  .' AND t_r.' . Model_Rights::COLUMN_RIGHT . " LIKE 'r__'"
-                  .' AND '.Model_Category::COLUMN_URLKEY.' IS NOT NULL',
-                  array("idgrp" => Auth::getGroupId()))
+      
+//      $cats = $model->join(Model_Category::COLUMN_CAT_ID, array('t_r' => 'Model_Rights'), null,
+//                  array(Model_Rights::COLUMN_ID_GROUP, Model_Rights::COLUMN_RIGHT))
+//               ->where('t_r.' . Model_Rights::COLUMN_ID_GROUP . ' = :idgrp'
+//                  .' AND t_r.' . Model_Rights::COLUMN_RIGHT . " LIKE 'r__'"
+//                  .' AND '.Model_Category::COLUMN_URLKEY.' IS NOT NULL',
+//                  array("idgrp" => Auth::getGroupId()))
+//            ->order(array(Model_Category::COLUMN_NAME => Model_ORM::ORDER_ASC))->records();
+      
+      $cats = $model->onlyWithAccess()
             ->order(array(Model_Category::COLUMN_NAME => Model_ORM::ORDER_ASC))->records();
 
+//      Template_Output::setOutputType("html");
+//      echo ($model->getSQLQuery());flush();
+//      die;
+      
       $link = new Url_Link(true);
-
       foreach($cats as $cat) {
-         $this->addItem($this->tr('Kategorie: ').$cat->{Model_Category::COLUMN_NAME}, (string)$link->category($cat->{Model_Category::COLUMN_URLKEY}));
+         $this->addItem($this->tr('Kategorie: ').$cat->{Model_Category::COLUMN_NAME}, str_replace(Url_Link::getMainWebDir(), "/", (string)$link->category($cat->{Model_Category::COLUMN_URLKEY})) );
       }
    }
 
