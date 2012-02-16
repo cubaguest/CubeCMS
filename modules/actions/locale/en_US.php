3cb2964952a15a3ef9570e2afb83735bd7c0f76f
Jakub Matas <j.matas@cube-studio.cz>
2012-02-16 15:46:50 +0000

    M_Actions: Oprava překladů

--- modules/actions/locale/en_US.php
+++ modules/actions/locale/en_US.php
@@ -2,14 +2,13 @@
 $singular = 
 array(
    'd6f923c81153a342a0de38709999a82c' => 'Delete', /* Smazat */ 
-   '24fb1e5398fc2cb5ba0d8f7561c78644' => 'Event "%s" has been deleted', /* Akce "%s" byla smazána */ 
+   '2d622cf9dee19a2e8cd961e2b444ee6e' => 'Event "%s" has been deleted', /* Událost "%s" byla smazána */ 
    '92f2ef2455d3a95518169b62f78e7caa' => 'Final date ends before the start date', /* Konečné datum končí dříve než datum začátku */ 
-   '2125f6c6f86246816f621b78719fdeca' => 'The event has been saved', /* Akce byla uložena */ 
+   'eeb10ca2f3f5cd2657715ecbe2951c47' => 'The event has been saved', /* Událost byla uložena */ 
    '586f4a1c4ec07d7768c5d27565089f0a' => 'No picture', /* Źádný obrázek */ 
    '3514ee286f0fd7c90c7ee7aa52479928' => 'Name', /* Název */ 
    'c4a37080d2a72356ce57b3c6f8a7227b' => 'Subtitle', /* Pod název */ 
    'dd531f5ccc16b1173bc60e677b13b700' => 'Author / Performer', /* Autor/ účinkující */ 
-   '9dffbf69ffba8bc38bc4e01abf4b1675' => 'Text', /* Text */ 
    '3cd27736c81f67a5355e65453a2fc26f' => 'Note', /* Poznámka */ 
    'de80dced16b6d4413746e2ab75e9922e' => 'From', /* Od */ 
    '0567953871b1bf589b797d9b178d5a94' => 'To', /* Do */ 
@@ -27,8 +26,8 @@ array(
    '9f94789ecbdc850397c6307a902e6770' => 'Description', /* Popis */ 
    'b6d25b9a06a63cb491ee7c8fdb9788f9' => 'The introductory text has been saved', /* Úvodní text byl uložen */ 
    '8c85fb8aeaf05786cc77b629f5cf5a81' => 'archive', /* archiv */ 
-   '17bdfcd418d3529656103eb76701a28e' => 'Add event', /* Přidat akci */ 
-   '1d9f1b0ee8a897441fc41e16e45ee4b9' => 'Add new event', /* Přidat novou akci */ 
+   '1833267786da8c3ccb365ff908273798' => 'Add event', /* Přidat událost */ 
+   'c53a811e25496bbbe46a99b54c434e66' => 'Add new event', /* Přidat novou událost */ 
    'ada39d6eb9938f732528ca605822a44a' => 'Edit introductory text', /* Upravit úvodní text */ 
    'a69f9a863a00a55992c71610be3e0815' => 'Edit', /* Upravit */ 
    '14caca1ea91ee6ea11753cd97e5b7d12' => 'Edit event', /* Upravit akci */ 
@@ -40,14 +39,14 @@ array(
    'fa2570cbea575072277c1744b4688435' => 'price: <span title="In advance">%s</span> Kč', /* cena: <span title="V předporodeji">%s</span> Kč */ 
    '0a7a1abad77f68be5dbb3ff668ec37f6' => 'Price: <span title="In place">%s</span> Kč', /* cena: <span title="Na místě">%s</span> Kč */ 
    '74ee2676e23815575660263eacac735f' => 'More', /* Více */ 
-   'fcd302bd72ed5ea40c8992a4143b98bf' => 'edit text for events', /* úprava textu k akcím */ 
+   'bf49466412e2297eefb22fe0fa39f6d1' => 'edit events introducing text ', /* úprava textu k událostem */ 
    'a06f539e3e2bc61ccdb1d58b580d6738' => 'Back to list', /* Zpět na seznam */ 
    'e7f4827f4857380aaed3d6e248886806' => 'Archive', /* Archiv */ 
    '0a07d20601f994d18a52014226c6774a' => 'More about', /* Více o  */ 
    '5bcd72277deefd10c075d7e0514dc248' => 'More events from', /* Více akcí z  */ 
    'b2c480e3ec4a350040ab0a17fae127be' => 'More events', /* Více akcí */ 
-   '1792ba1004a78983d090225eb727b8c9' => 'event modification', /* úprava akce */ 
-   'b50f731bdb67c2ba01c51356c6f09f75' => 'add event', /* přidání akce */ 
+   'f1ae3dca7d3f26440e2cf43b92fa111c' => 'event edit', /* úprava události */ 
+   '3f8d79c26ce1c55f576f9f97c91b0393' => 'add an event', /* přidání události */ 
    '018db43d51120d05b8866f99fcf0180c' => 'List', /* Seznam */ 
    'f363da82f7b1e65f5ccc3b791d80ab1e' => 'show', /* zobraz */ 
 );
