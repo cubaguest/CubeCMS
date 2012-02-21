0b87de1442e0ff00dbde79459f3f1074a3e3ebed
Jakub Matas <j.matas@cube-studio.cz>
2012-02-21 08:56:56 +0000

    M_Teams: Implementace hledání osob v týmu

--- modules/teams/search.class.php
+++ modules/teams/search.class.php
@@ -4,17 +4,20 @@ class Teams_Search extends Search {
       $model = new Teams_Model_Persons();
       // join here
       
-      $result = $model->search($this->getSearchString());
+      $result = $model
+         ->joinFK(Teams_Model_Persons::COLUMN_ID_TEAM)
+         ->where(Teams_Model::COLUMN_ID_CATEGORY." = :idc", array('idc' => $this->category()->getId()))
+         ->search($this->getSearchString());
       if($result != false){
          foreach ($result as $res) {
             $name = null;
             if($res->{Teams_Model_Persons::COLUMN_DEGREE} != null) $name .= $res->{Teams_Model_Persons::COLUMN_DEGREE}.' ';
-            $name .= $res->{Teams_Model_Persons::COLUMN_NAME}.' '.$res->{Teams_Model_Persons::COLUMN_SURNAME};
-            if($res->{Teams_Model_Persons::COLUMN_DEGREE_AFTER} != null) $name .= ' '.$res->{Teams_Model_Persons::COLUMN_DEGREE_AFTER};
-
+            $name .= $res->{Teams_Model_Persons::COLUMN_NAME}.' '.$res->{Teams_Model_Persons::COLUMN_SURNAME}.' '.$res->{Teams_Model_Persons::COLUMN_DEGREE_AFTER};
+            if($res->{Teams_Model::COLUMN_NAME} != null) $name .= ' ('.$res->{Teams_Model::COLUMN_NAME}.')';
+            
             $this->addResult(
                $name,
-               $this->link()->param('sid', $res->{Teams_Model_Persons::COLUMN_ID}),
+               $this->link()->param('sid', $res->{Teams_Model_Persons::COLUMN_ID})->anchor('team-person-'. $res->{Teams_Model_Persons::COLUMN_ID}),
                $res->{Teams_Model_Persons::COLUMN_TEXT}, $res->{Search::COLUMN_RELEVATION});
          }
       }
