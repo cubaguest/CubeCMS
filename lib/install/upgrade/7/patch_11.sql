dbd3cd7262e9981f56f8c3431a56b61134f9b7b0
Jakub Matas <j.matas@cube-studio.cz>
2012-05-07 11:06:08 +0000

    Core: Přidání konstant pro kešování obrázků

--- lib/install/upgrade/7/patch_11.sql
+++ lib/install/upgrade/7/patch_11.sql
@@ -2,5 +2,7 @@
 -- new config values
 INSERT INTO `cubecms_global_config` (`key`, `label`, `value`, `protected`, `type`, `id_group`, `hidden_value`) 
 VALUES
-('ARTICLE_TITLE_IMG_C', 'Ořezávat titulní obrázky', "true", false, 'bool', 7, false);
+('ARTICLE_TITLE_IMG_C', 'Ořezávat titulní obrázky', "true", false, 'bool', 7, false),
+('CACHE_TEXT_IMAGES', 'Zapnutí kešování obrázků v textu', "true", false, 'bool', 7, false),
+('CACHE_TEXT_IMAGES_CROP', 'Ořezání kešovaného obrázku při zadání obou rozměrů', "false", false, 'bool', 7, false);
 /* END_UPDATE */
