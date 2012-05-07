bbd5b815e169d9d5b489f55b93f3dc08aa16029d
Jakub Matas <j.matas@cube-studio.cz>
2012-05-07 11:09:31 +0000

    Core: Update instalačního skriptu E-shopu

--- install/sql/install_shop.sql
+++ install/sql/install_shop.sql
@@ -1,4 +1,5 @@
--- Nahradit mamulka_ za prefix tabulek
+-- VERZE 7.7
+-- Nahradit za prefix tabulek
 
 -- nová skupina v konfigu
 INSERT INTO `{PREFIX}config_groups` ( `id_group` , `name_cs` , `name_sk` , `name_en` , `name_de` , `desc_cs` , `desc_sk` , `desc_en` , `desc_de` )
@@ -246,3 +247,55 @@ CREATE TABLE IF NOT EXISTS `{PREFIX}shop_zones` (
 
 INSERT INTO `{PREFIX}shop_zones` (`id_zone`, `zone_name`) VALUES
 (1, 'Česká Republika');
+
+ALTER TABLE `{PREFIX}shop_products_general` 
+ADD COLUMN `personal_pickup_only` TINYINT(1)  NULL DEFAULT 0  AFTER `date_add` , 
+ADD COLUMN `required_pickup_date` TINYINT(1)  NULL DEFAULT 0  AFTER `personal_pickup_only` ;
+
+-- datum odběru položky
+ALTER TABLE `{PREFIX}shop_orders` 
+ADD COLUMN `order_pickup_date` DATE NULL DEFAULT NULL  AFTER `order_is_new` ;
+
+-- nastavení atributů
+ALTER TABLE `{PREFIX}shop_order_items` 
+ADD COLUMN `order_product_attributes` VARCHAR(500) NULL DEFAULT NULL  AFTER `order_product_note` ;
+
+-- přidání volby osobního odběru
+ALTER TABLE `{PREFIX}shop_shippings` ADD COLUMN `shipping_is_personal_pickup` TINYINT(1)  NULL DEFAULT 0  AFTER `shipping_text_de` ;
+
+
+-- skupiny atributů
+CREATE  TABLE `{PREFIX}shop_attributes_groups` (
+  `id_attribute_group` INT NOT NULL ,
+  `atgroup_name_cs` VARCHAR(100) CHARACTER SET 'utf8' COLLATE 'utf8_czech_ci' NULL DEFAULT NULL ,
+  `atgroup_name_en` VARCHAR(100) NULL DEFAULT NULL ,
+  `atgroup_name_sk` VARCHAR(100) CHARACTER SET 'utf8' COLLATE 'utf8_slovak_ci' NULL DEFAULT NULL ,
+  `atgroup_name_de` VARCHAR(100) NULL DEFAULT NULL ,
+  PRIMARY KEY (`id_attribute_group`) )
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_general_ci;
+
+-- tabulka atributů
+CREATE  TABLE `{PREFIX}shop_attributes` (
+  `id_attribute` INT NOT NULL ,
+  `id_attribute_group` INT NOT NULL ,
+  `attribute_name_cs` VARCHAR(100) CHARACTER SET 'utf8' COLLATE 'utf8_czech_ci' NULL DEFAULT NULL ,
+  `attribute_name_en` VARCHAR(100) NULL DEFAULT NULL ,
+  `attribute_name_sk` VARCHAR(100) CHARACTER SET 'utf8' COLLATE 'utf8_slovak_ci' NULL DEFAULT NULL ,
+  `attribute_name_de` VARCHAR(100) NULL DEFAULT NULL ,
+  PRIMARY KEY (`id_attribute`) ,
+  INDEX `group` (`id_attribute_group` ASC) )
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_general_ci;
+
+-- propojení produktů s atributy
+CREATE  TABLE `{PREFIX}shop_products_variants` (
+  `id_variant` INT NOT NULL ,
+  `id_product` INT NOT NULL ,
+  `id_attribute` INT NOT NULL ,
+  `variant_price_add` FLOAT NULL DEFAULT 0 ,
+  `variant_product_code` VARCHAR(100) NULL DEFAULT NULL ,
+  PRIMARY KEY (`id_variant`) ,
+  INDEX `fkeys` (`id_product` ASC, `id_attribute` ASC) )
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_general_ci;
