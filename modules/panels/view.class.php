68a669e35dc8224acc23e5a2450daceb6950c1a4
Jakub Matas <j.matas@cube-studio.cz>
2012-05-07 11:14:43 +0000

    M_Panels: Přidání tlačítka pro nový panel přímo do tabulky panelů.

--- modules/panels/view.class.php
+++ modules/panels/view.class.php
@@ -11,7 +11,7 @@ class Panels_View extends View {
       if($this->category()->getRights()->isWritable()) {
          $toolbox = new Template_Toolbox2();
          $toolbox->setIcon(Template_Toolbox2::ICON_ADD);
-         $toolAdd = new Template_Toolbox2_Tool_PostRedirect('add_panel', $this->_("Přidat panel"),
+         $toolAdd = new Template_Toolbox2_Tool_Redirect('add_panel', $this->_("Přidat panel"),
          $this->link()->route('add'));
          $toolAdd->setIcon('page_add.png')->setTitle($this->_('Přidat nový panel'));
          $toolbox->addTool($toolAdd);
