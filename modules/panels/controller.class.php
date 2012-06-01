52f4ccb7858b00b94c3b5c790a04b1c49a52dff5
Jakub Matas <j.matas@cube-studio.cz>
2012-06-01 14:20:51 +0000

    M_Panels: Vylepšení práce s panely, hromadná úprava, lepší výpis, přiřazení do několika kategorií najednou.

--- modules/panels/controller.class.php
+++ modules/panels/controller.class.php
@@ -64,15 +64,19 @@ class Panels_Controller extends Controller {
 
       // načtení individuálních panelů
       $modelCat = new Model_Category();
-      $this->view()->individualPanelCats = $modelCat->getCategoriesWithIndPanels();
-
-
+      
+      $panels = $modelCat
+         ->columns(array('*', 'numPanels' => 
+               ' (SELECT count('.Model_Panel::COLUMN_ID.') FROM '.$model->getTableName().' WHERE '.Model_Panel::COLUMN_ID_SHOW_CAT." = ".$modelCat->getTableShortName().'.'.Model_Category::COLUMN_ID.")"))
+         ->where(Model_Category::COLUMN_INDIVIDUAL_PANELS." = 1", array())
+         ->records();
+      $this->view()->individualPanelCats = $panels;
    }
 
    public function addController() {
       $this->checkWritebleRights();
 
-      $this->createEditForm();
+      $this->createEditForm(true);
       // odstranění checkboxů
       $this->editForm->removeElement('icon_delete');
       $this->editForm->removeElement('background_delete');
@@ -101,10 +105,20 @@ class Panels_Controller extends Controller {
          $panel->{Model_Panel::COLUMN_POSITION} = $this->editForm->panel_box->getValues();
          $panel->{Model_Panel::COLUMN_NAME} = $this->editForm->panel_name->getValues();
          $panel->{Model_Panel::COLUMN_ORDER} = $this->editForm->panel_order->getValues();
-         $panel->{Model_Panel::COLUMN_ID_SHOW_CAT} = $this->editForm->panel_show_cat->getValues();
-
-         $model->save($panel);
-         $this->log(sprintf('Přidán nový panel %s kategorie %s do aplikace', $panel->{Model_Panel::COLUMN_NAME}, $panel->{Model_Panel::COLUMN_ID_CAT}));
+         
+         if($this->editForm->panel_show_cat->getValues() == null){
+            $panel->{Model_Panel::COLUMN_ID_SHOW_CAT} = 0;
+            $model->save($panel);
+         } else {
+            Debug::log($this->editForm->panel_show_cat->getValues());
+            foreach ($this->editForm->panel_show_cat->getValues() as $id) {
+               $panel->{Model_Panel::COLUMN_ID_SHOW_CAT} = $id;
+               $pForSave = clone $panel;
+               $model->save($pForSave);
+            }
+         }
+         
+         $this->log(sprintf('Přidán nový panel %s kategorie id:%s', $panel->{Model_Panel::COLUMN_NAME}, $panel->{Model_Panel::COLUMN_ID_CAT}));
          $this->infoMsg()->addMessage($this->tr('Panel byl uložen'));
          $this->link()->route()->reload();
       }
@@ -114,11 +128,17 @@ class Panels_Controller extends Controller {
    public function editController(){
       $this->checkWritebleRights();
       $model = new Model_Panel();
-      $panel = $model->record($this->getRequest('id'));
+      $panel = $model
+         ->joinFK(Model_Panel::COLUMN_ID_CAT, array(Model_Category::COLUMN_NAME, Model_Category::COLUMN_URLKEY, Model_Category::COLUMN_MODULE))
+         ->record($this->getRequest('id'));
       if($panel == false) return false;
 
       $this->createEditForm();
 
+      $elemUpdateAll = new Form_Element_Checkbox('updateAll', $this->tr('Aktualizovat vše'));
+      $elemUpdateAll->setSubLabel($this->tr('Aktualizovat všechny panely vybrané kategorie podle této úpravy.'));
+      $this->editForm->addElement($elemUpdateAll, 'settings',1);
+      
       $this->editForm->panel_cat->setValues($panel->{Model_Panel::COLUMN_ID_CAT});
       $this->editForm->panel_box->setValues($panel->{Model_Panel::COLUMN_POSITION});
       $this->editForm->panel_order->setValues($panel->{Model_Panel::COLUMN_ORDER});
@@ -144,8 +164,8 @@ class Panels_Controller extends Controller {
          // ikona
          if($panel->{Model_Panel::COLUMN_ICON} != null AND ($this->editForm->icon->getValues() != null
                  OR ($this->editForm->haveElement('icon_delete') AND $this->editForm->icon_delete->getValues() == true))){
-            $file = new Filesystem_File($icon, Panel_Obj::getIconDir(false));
-            if($file->exist()) $file->delete();
+//             $file = new Filesystem_File($icon, Panel_Obj::getIconDir(false));
+//             if($file->exist()) $file->delete();
             $panel->{Model_Panel::COLUMN_ICON} = null;
          }
          if($this->editForm->icon->getValues() != null) {
@@ -155,8 +175,8 @@ class Panels_Controller extends Controller {
          // pozadí
          if($panel->{Model_Panel::COLUMN_IMAGE} != null AND ($this->editForm->image->getValues() != null
                  OR ($this->editForm->haveElement('image_delete') AND $this->editForm->image_delete->getValues() == true))){
-            $file = new Filesystem_File($image, Panel_Obj::getImgDir(false));
-            $file->delete();
+//             $file = new Filesystem_File($image, Panel_Obj::getImgDir(false));
+//             $file->delete();
             $panel->{Model_Panel::COLUMN_IMAGE} = null;
          }
          if($this->editForm->image->getValues() != null) {
@@ -171,56 +191,69 @@ class Panels_Controller extends Controller {
          $panel->{Model_Panel::COLUMN_ID_SHOW_CAT} = $this->editForm->panel_show_cat->getValues();
 
          $model->save($panel);
+         
+         if($this->editForm->updateAll->getValues() == true){
+            $model->where(Model_Panel::COLUMN_ID_CAT." = :idc", array('idc' => $this->editForm->panel_cat->getValues()))
+            ->update(array(
+                  Model_Panel::COLUMN_NAME => $this->editForm->panel_name->getValues(),
+                  Model_Panel::COLUMN_IMAGE => $panel->{Model_Panel::COLUMN_IMAGE},
+                  Model_Panel::COLUMN_ICON => $panel->{Model_Panel::COLUMN_ICON},
+                  Model_Panel::COLUMN_ORDER => $panel->{Model_Panel::COLUMN_ORDER},
+            ));
+            $this->log(sprintf('Upraveny panely kategorie id:%s', $panel->{Model_Panel::COLUMN_ID_CAT}));
+         }
 
-         $this->log(sprintf('Přidán nový panel %s kategorie %s do aplikace', $panel->{Model_Panel::COLUMN_NAME}, $panel->{Model_Panel::COLUMN_ID_CAT}));
+         $this->log(sprintf('Upraven panel %s kategorie id:%s', $panel->{Model_Panel::COLUMN_NAME}, $panel->{Model_Panel::COLUMN_ID_CAT}));
          $this->infoMsg()->addMessage($this->tr('Panel byl uložen'));
          $this->link()->route()->reload();
       }
 
       $this->view()->panelIcon = $panel->{Model_Panel::COLUMN_ICON};
       $this->view()->panelbackImg = $panel->{Model_Panel::COLUMN_BACK_IMAGE};
+      $this->view()->panel = $panel;
       $this->view()->form = $this->editForm;
    }
 
-   private function createEditForm() {
+   private function createEditForm($multipleCatSelect = false) {
       // kategorie a šablony
       $panelPositions = vve_parse_cfg_value(VVE_PANEL_TYPES);
 
       $catModel = new Model_Category();
       $categories = $catModel->getCategoryList(true);
-      $catArr = array();
-      foreach ($categories as $cat) {
-         if(file_exists(AppCore::getAppLibDir().AppCore::MODULES_DIR.DIRECTORY_SEPARATOR
-         .$cat->{Model_Category::COLUMN_MODULE}.DIRECTORY_SEPARATOR
-         .DIRECTORY_SEPARATOR.'panel.class.php')) {
-            $catArr[(string)$cat->{Model_Category::COLUMN_CAT_LABEL}] = $cat->{Model_Category::COLUMN_CAT_ID};
-         }
-      }
-      ksort($catArr);//řazení
+      
+      $struct = Category_Structure::getStructure();
+      $struct->withHidden(true);
+      $struct->setCategories($categories);
+      
+      $panelCats = $this->createArray($struct);
+      
 
-      $form = new Form('panel_', true);
+      $form = new Form('panel_'/*, true*/);
       $form->addGroup('settings', $this->tr('Základní'), $this->tr('Přiřazení panelu ke kategorii a jeho umístění'));
 
       $panelCategory = new Form_Element_Select('panel_cat', $this->tr('Panel kategorie'));
-      $panelCategory->setOptions($catArr);
+      foreach ($panelCats as $cat) {
+         if(file_exists(AppCore::getAppLibDir().AppCore::MODULES_DIR.DIRECTORY_SEPARATOR
+            .$cat['module'].DIRECTORY_SEPARATOR.'panel.class.php')) {
+               $panelCategory->setOptions(array($cat['structname'] => $cat['id']), true);
+         }
+      }
       $form->addElement($panelCategory,'settings');
 
       // panel zobrazit u kategorie
-      $showCat = array ();
-
-      $individualPanelsCats = $catModel->getCategoriesWithIndPanels();
-      $arr = array();
-      foreach ($individualPanelsCats as $cat) {
-         $arr[(string)$cat->{Model_Category::COLUMN_CAT_LABEL}] = $cat->{Model_Category::COLUMN_CAT_ID};
-      }
-      ksort($arr); // řazení
-      $showCat = array_merge(array ($this->tr('Globálně') => 0),$arr);
-
+      
+      
+      $panelForCats = $this->createArray($struct, true);
 
       $panelShowCategory = new Form_Element_Select('panel_show_cat', $this->tr('Určení pro'));
-      $panelShowCategory->setOptions($showCat);
+      $panelShowCategory->setOptions(array($this->tr('Globálně') => 0));
+      foreach ($panelForCats as $cat) {
+         $panelShowCategory->setOptions(array($cat['structname'] => $cat['id']), true);
+      }
+      
       $panelShowCategory->setSubLabel($this->tr('Pokud je nastaveno globálně, panel je
          zobrazen u všech kategorií, v opačném případě pouze u vybrané kategorie'));
+      $panelShowCategory->setMultiple($multipleCatSelect);
       $form->addElement($panelShowCategory,'settings');
 
       $panelType = new Form_Element_Select('panel_box', $this->tr('Box panelu'));
@@ -261,6 +294,25 @@ class Panels_Controller extends Controller {
       $this->editForm = $form;
    }
 
+   protected function createArray(Category_Structure $struct, $onlyIndividual = false, $level = 0){
+      $a = array();
+      foreach ($struct as $cat) {
+         if($onlyIndividual == false || $cat->getCatObj()->isIndividualPanels() ){
+            $a[] = array(
+               'id' => $cat->getId(), 
+               'name' => $cat->getCatObj()->getName(),
+               'structname' => str_repeat('. ', $level).$cat->getCatObj()->getName()." ID:".$cat->getId(),
+               'module' => (string)$cat->getCatObj()->getModule());
+            
+         }
+         
+         if(!$cat->isEmpty()){
+            $a = array_merge($a, $this->createArray($cat, $onlyIndividual, $level+1));
+         }
+      }
+      return $a;
+   } 
+   
    private function catsToArrayForForm($categories) {
       // pokud je hlavní kategorie
       if($categories->getLevel() != 0) {
