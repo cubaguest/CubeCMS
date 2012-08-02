ce18f689bb3d7c9023f90925a48f599f383480ec
Jakub Matas <j.matas@cube-studio.cz>
2012-08-02 06:06:02 +0000

    Core: Import třídy Face pro přístup k parametrům vzhledu. (oprvava)

---
+++ lib/face/face.class.php
@@ -0,0 +1,109 @@
+<?php
+class Face {
+   
+   protected static $current = null;
+
+   protected $name = 'default';
+   protected $desc = null; 
+   protected $version = null;
+    
+   protected $params = null;
+   protected $modulesParams = null;
+   
+   /**
+    * 
+    * @param unknown_type $name
+    */
+   public function __construct($name = null) {
+      // load face config
+      if($name == null){
+         $name = VVE_TEMPLATE_FACE; 
+      }
+      $this->name = $name;
+      $this->loadParams();
+
+      if(self::$current == null && $name == VVE_TEMPLATE_FACE){
+         self::$current = $this;
+      }
+   }
+   
+   protected function loadParams() {
+      if($this->params == null){
+         $file = AppCore::getAppWebDir().Template::FACES_DIR.DIRECTORY_SEPARATOR.$this->name.DIRECTORY_SEPARATOR."face.php";
+         if(is_file($file)){
+            $face = $modules = array();
+            include_once $file;
+            $this->params = $face;
+            $this->modulesParams = $modules;
+         }
+      }
+   } 
+   
+   /**
+    * Metoda vrací název vzhledu
+    * @return string
+    */
+   public function getName()
+   {
+      return $this->name;
+   }
+   
+   /**
+    * Metoda vrací parametr vzhledu
+    * @param string $name -- název parametru
+    * @param string $module -- název modulu pokud se má vybrat modul
+    * @param mixed $defaultValue -- výchozí hodnota
+    */
+   public function getParam($name, $module = null, $defaultValue = null)
+   {
+      if($module == null){ // face param
+         if(isset($this->params[$name])){
+            return $this->params[$name];
+         }
+      } else { // module param
+         if(isset($this->modulesParams[$module]) && isset($this->modulesParams[$module][$name]) ){
+            return $this->modulesParams[$module][$name];
+         }
+      }
+      return $defaultValue;
+   }
+   
+   /* MAGIC METHODS */
+   public function __get($name)
+   {
+      if (array_key_exists($name, $this->params)) {
+         return $this->params[$name];
+      }
+      return null;
+   }
+   
+   public function __isset($name)
+   {
+      return isset($this->params[$name]);
+   }
+   
+   /**
+    * Statická metoda pro parametr aktuální vzhledu
+    * @param string $name -- název parametru
+    * @param string $module -- název modulu pokud se má vybrat modul
+    * @param mixed $defaultValue -- výchozí hodnota
+    */
+   public static function getParamStatic($name, $module = null, $defaultValue = null)
+   {
+      return self::getCurrent()->getParam($name, $module, $defaultValue);
+   }
+   
+   /**
+    * Metoda vrací aktuální objek vzhledu
+    * @return Face
+    */
+   public static function getCurrent()
+   {
+      if(self::$current == null){
+         self::$current = new self();
+      }
+      return self::$current;
+   }
+   
+   
+}
