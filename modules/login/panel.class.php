2105e05d85f30b7df06fc3e6c3971e227005f8e5
Jakub Matas <j.matas@cube-studio.cz>
2012-08-05 09:43:49 +0000

    M_Login: Přidání detailu o uživateli do panelu při přihlášení.

--- modules/login/panel.class.php
+++ modules/login/panel.class.php
@@ -1,11 +1,18 @@
 <?php
 class Login_Panel extends Panel {
 	
+   protected $userRecord;
+   
 	public function panelController() {
+	   if(Auth::isLogin()){
+	      $m = new Model_Users();
+	      $this->userRecord = $m->record(Auth::getUserId());
+	   }
 	}
 	
 	public function panelView() {
       $this->template()->currentLink = (string)new Url_Link();
+      $this->template()->user = $this->userRecord;
       $this->template()->addTplFile("panel.phtml");
 	}
 }
