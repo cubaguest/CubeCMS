555b978f4f5baba0b9e4db51fdb69b9b35f86030
Jakub Matas <j.matas@cube-studio.cz>
2012-09-25 13:39:02 +0000

    M_Text: Odstranění závislosti instalace na slovenské jazykové mutaci

--- modules/text/install/upgrade_1.3_1.4.sql
+++ modules/text/install/upgrade_1.3_1.4.sql
@@ -1,3 +1,3 @@
 ALTER TABLE `{PREFIX}texts` 
-ADD COLUMN `data` TEXT NULL DEFAULT NULL  AFTER `text_clear_sk` 
-, ADD INDEX `subkey` (`id_item` ASC, `subkey` ASC) ;
+ADD COLUMN `data` TEXT NULL DEFAULT NULL,
+ADD INDEX `subkey` (`id_item` ASC, `subkey` ASC) ;
