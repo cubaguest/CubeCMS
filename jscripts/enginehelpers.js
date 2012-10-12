b85a867cf18ebdd9ed7515448a0f8cb42f798b02
Jakub Matas <j.matas@cube-studio.cz>
2012-10-12 13:25:45 +0000

    CoreJS: Práce na nové verzi engine JS

--- jscripts/enginehelpers.js
+++ jscripts/enginehelpers.js
@@ -1,87 +1,270 @@
-function vveShowMessages(dataObj){
-   clearInfoMsg();
-   if(typeof(dataObj.infomsg) == 'object'){
-      for(var i=0; i < dataObj.infomsg.length; i++){
-         infoMsg(dataObj.infomsg[i], false);
+/** NEW USE THIS!!! */
+/**
+ * Vytvoření pomocných funkcí pro Cube CMS systém
+ * 
+ * Msg - zprávy
+ * Dom - dom objekty
+ * Tools - různé nástroje
+ * Loader - loadery a ajax
+ * Toolbox - toolbox a nástroje
+ * Toolbox.Tool - toolbox a nástroje ???????????
+ * 
+ **/
+var CubeCMS = CubeCMS || {};
+
+CubeCMS = {
+   // base object
+   contentSelector : '.main-content',
+   lang : null,
+   primaryLang : null,
+   domain : null,
+   
+   baseOpt : {},
+   // caled in index header
+   init : function(opt){
+      $.extend(this, opt);
+      
+      document.domain = this.domain;
+      document.lang = this.lang;
+      document.primaryLang = this.primaryLang ;
+   },
+   setup : function(){
+      // init messages
+      this.Msg.init();
+      this.Loader.init();
+   }
+};
+
+// msg class
+CubeCMS.Msg = {
+   infoBox : null, 
+   errBox : null, 
+   
+   init : function(){
+      this.infoBox = $('#infoMessages');
+      this.errBox = $('#errMessages');
+   },
+   
+   /**
+    *  Zobrazí info hlášku
+    */
+   info : function(msg, clear){
+      if(msg == null) return this;
+      if(typeof (clear) == 'undefined') clear = true;
+      if(clear == true){
+         this.clear('info');
       }
-   } else if(typeof(dataObj.infomsg) == 'string'){
-      infoMsg(dataObj.infomsg);
-   }
-   clearErrMsg();
-   if(typeof(dataObj.errmsg) == 'object'){
-      for(var y=0; y < dataObj.errmsg.length; y++){
-         errMsg(dataObj.errmsg[y], false);
+      this.infoBox.prepend("<p>"+msg+"</p>").slideDown();
+      return this;
+   },
+   err : function(msg, clear){
+      if(msg == null) return this;
+      if(typeof (clear) == 'undefined') clear = true;
+      if(clear == true){
+         this.clear('err');
       }
-   } else if(typeof(dataObj.infomsg) == 'string'){
-      errMsg(dataObj.errmsg);
+      this.errBox.prepend("<p>"+msg+"</p>").slideDown();
+      return this;
+   },
+   /**
+    * Metoda zobrazí zprávy aplikace
+    */
+   show : function(dataObj){
+      this.clear('info');
+      this.clear('error');
+      if(typeof(dataObj.infomsg) == 'object'){
+         for(var i=0; i < dataObj.infomsg.length; i++){
+            this.info(dataObj.infomsg[i], false);
+         }
+      } else if(typeof(dataObj.infomsg) == 'string'){
+         this.info(dataObj.infomsg);
+      }
+      if(typeof(dataObj.errmsg) == 'object'){
+         for(var y=0; y < dataObj.errmsg.length; y++){
+            this.err(dataObj.errmsg[y], false);
+         }
+      } else if(typeof(dataObj.infomsg) == 'string'){
+         this.err(dataObj.errmsg);
+      }
+      
+   },
+   /**
+    * Vymaže info zprávy
+    */
+   clear : function (type){
+      if(type == 'info'){
+         this.infoBox.html(null).hide();
+      } else if(type == "err" || type == "error"){
+         this.errBox.html(null).hide();
+      }
+      
+      return this;
+   }   
+};
+
+CubeCMS.Tools = {
+   /**
+    * Metoda převede řetězec na url adresu a odstraní nepovolené znaky
+    */
+   str2url : function(str,ucfirst) {
+      str = str.toUpperCase();
+      str = str.toLowerCase();
+
+      str = str.replace(/[\u0105\u0104\u00E0\u00E1\u00E2\u00E3\u00E4\u00E5]/g,'a');
+      str = str.replace(/[\u00E7\u010D\u0107\u0106]/g,'c');
+      str = str.replace(/[\u010F]/g,'d');
+      str = str.replace(/[\u00E8\u00E9\u00EA\u00EB\u011B\u0119\u0118]/g,'e');
+      str = str.replace(/[\u00EC\u00ED\u00EE\u00EF]/g,'i');
+      str = str.replace(/[\u0142\u0141]/g,'l');
+      str = str.replace(/[\u00F1\u0148]/g,'n');
+      str = str.replace(/[\u00F2\u00F3\u00F4\u00F5\u00F6\u00F8\u00D3]/g,'o');
+      str = str.replace(/[\u0159]/g,'r');
+      str = str.replace(/[\u015B\u015A\u0161]/g,'s');
+      str = str.replace(/[\u00DF]/g,'ss');
+      str = str.replace(/[\u0165]/g,'t');
+      str = str.replace(/[\u00F9\u00FA\u00FB\u00FC\u016F]/g,'u');
+      str = str.replace(/[\u00FD\u00FF]/g,'y');
+      str = str.replace(/[\u017C\u017A\u017B\u0179\u017E]/g,'z');
+      str = str.replace(/[\u00E6]/g,'ae');
+      str = str.replace(/[\u0153]/g,'oe');
+      str = str.replace(/[\u013E\u013A]/g,'l');
+      str = str.replace(/[\u0155]/g,'r');
+
+      str = str.replace(/[^a-z0-9\s\'\:\/\[\]-]/g,'');
+      str = str.replace(/[\s\'\:\/\[\]-]+/g,' ');
+      str = str.replace(/[ ]/g,'-');
+      str = str.replace(/[\/]/g,'-');
+      if (ucfirst == 1) {
+         var c = str.charAt(0);
+         str = c.toUpperCase()+str.slice(1);
+      }
+      return str;
+   },
+   
+   /**
+    * Metoda odstraní html tagy
+    */
+   stripHtml : function(html){
+      var tmp = document.createElement("DIV");
+      tmp.innerHTML = html;
+      return tmp.textContent || tmp.innerText;
+   },
+   /**
+    * metoda provede kontrolu url klíče nad zadanou url adresou
+    */ 
+   checkUrlKey : function(checkerUrl, elem, callback){
+      var $e = $(element);
+      $.ajax({
+         type: "POST", url: checkerUrl, cache: false,
+         data: {key : $e.val(), lang : $e.attr("lang")},
+         success: function(data){
+            if( typeof data.urlkey != "undefined"
+            && $.isFunction(callback) )
+               callback(data.urlkey);
+            }
+      });
+   },
+   /**
+    * Metoda detekuje jestli je barva tmavá nebo světlá. Podle toho vrací bílou nebo černou barvu.
+    */
+   contrastColor : function(color){
+      // Counting the perceptive luminance - human eye favors green color... 
+      var a = 1 - ( 
+         0.299 * parseInt((color).substring(0,2),16) 
+         + 0.587 * parseInt((color).substring(2,4),16) 
+         + 0.114 * parseInt((color).substring(4,6),16) )/255;
+      if (a < 0.5){
+         return "#000000"; // bright colors - black font
+      } else{
+         return "#ffffff"; // dark colors - white font
+      }  
    }
-}
+};
 
-function infoMsg(msg, clear){
-   if(msg == null) return;
-   if(typeof (clear) == 'undefined') clear = true;
-   if(clear == true){
-      clearInfoMsg();
+CubeCMS.ToolBox = {
+   init : function() {
+      
    }
-   $('#infoMessages').prepend("<p>"+msg+"</p>").show();
-}
+};
 
-function clearInfoMsg(){
-   $('#infoMessages').html(null).hide();
-}
+CubeCMS.ToolBox.Tool = {
+   
+};
 
-function clearErrMsg(){
-   $('#errMessages').html(null).hide();
-}
+CubeCMS.Images = {
+      slideShowFade : function(selector, delay){
+         $(selector+' .image:gt(0)').hide();
+         setInterval(function(){ $(selector+' .image:first').fadeOut().next('.image').fadeIn().end().appendTo(selector);}, delay); 
+      },
+      loadImage : function(src, callback){
+         var cacheImage = document.createElement('img');
+         //set the onload method before the src is called otherwise will fail to be called in IE
+         cacheImage.onload = function(){
+            if ($.isFunction(callback)) {
+               callback.call(cacheImage);
+            }
+         };
+         cacheImage.src = src;
+      },
+};
 
-function errMsg(msg, clear){
-   if(msg == null) return;
-   if(typeof (clear) == 'undefined') clear = true;
-   if(clear == true){
-      $('#errMessages').html(null);
-   }
-   $('#errMessages').prepend("<p>"+msg+"</p>").show();
-}
-
-function showLoadBox(box, timeout){
-   if(typeof(timeout) == "undefined"){
-      timeout = 5000;
-   }
-   var jbox = $(box);
-   var parentBox = $(box).parent('div,p');
-//   var overBox = $('<div id="loadingBox"><img src="images/progress.gif" alt="Loading..." /></div>')
-   var overBox = $('<div id="loadingBox"><div class="loader-w"><span class="loader-text">loading...</span></div>')
-   .css({
-      position : 'absolute',
-      top : 0,
-      left : 0,
-      opacity : 0,
-      width : jbox.width()+parseInt(parentBox.css('padding-left'))+parseInt(parentBox.css('padding-right'))
-                          +parseInt(jbox.css('margin-left'))+parseInt(jbox.css('margin-right')),
-      height : jbox.height()+parseInt(parentBox.css('padding-top'))+parseInt(parentBox.css('padding-bottom'))
-                          +parseInt(jbox.css('margin-top'))+parseInt(jbox.css('margin-bottom'))
-   });
-   parentBox.css('position', 'relative').prepend(overBox.hide());
-   parentBox.children('#loadingBox').fadeTo(500, 1);
-   setTimeout("hideLoadBox()", timeout); // zrušení
-}
-
-function hideLoadBox(){
-   $('#loadingBox').fadeOut(500,function(){
-      $('#loadingBox').remove();
-   })
-}
-
-function vveLoadImage(src, callback){
-   var cacheImage = document.createElement('img');
-   //set the onload method before the src is called otherwise will fail to be called in IE
-   cacheImage.onload = function(){
-      if ($.isFunction(callback)) {
-         callback.call(cacheImage);
+CubeCMS.Loader = {
+   timeout : 10000,
+   loaderCnt : '<div id="loadingBox" class="loadingBox"><div class="loader-w"><span class="loader-text">loading...</span></div>',
+   loadBoxClass : "loadingBox",
+   
+   init : function(){
+      
+   },
+   
+   loadUrl : function(box, url){
+      
+   },
+   
+   showLoadBox : function(box){
+      var rId = 'loadingBox-'+ (Math.floor( Math.random(0)*10000 ) );
+      var parentBox = $(box).parent('div,p');
+      var overBox = $(this.loaderCnt)
+      .addClass(this.loadBoxClass)
+      .attr('id', rId)
+      .css({
+         position : 'absolute',
+         top : 0,
+         left : 0,
+         opacity : 0,
+         width : parentBox.width()+parseInt(parentBox.css('padding-left'))+parseInt(parentBox.css('padding-right'))
+         +parseInt(parentBox.css('margin-left'))+parseInt(parentBox.css('margin-right')),
+         height : parentBox.height()+parseInt(parentBox.css('padding-top'))+parseInt(parentBox.css('padding-bottom'))
+         +parseInt(parentBox.css('margin-top'))+parseInt(parentBox.css('margin-bottom'))
+      });
+      parentBox.css('position', 'relative').prepend(overBox.hide());
+      overBox.fadeTo(500, 1);
+      var _loader = this;
+      setTimeout(function() { _loader.hideLoadBox(rId); }, this.timeout); // zavření
+   },
+   hideLoadBox : function(id){
+      if(typeof(id) == "undefined"){
+         $('.'+this.loadBoxClass).remove();
+      } else {
+         $('#'+id).remove();
       }
    }
-   cacheImage.src = src;
+};
+
+//base vars
+if(typeof(contentSelector) === "undefined"){
+   var contentSelector = CubeCMS.contentSelector;
 }
+
+/* DEPRECATED */
+function vveShowMessages(dataObj){ CubeCMS.Mgs.show(dataObj);}
+function infoMsg(msg, clear){ CubeCMS.Mgs.info(msg, clear); }
+function clearInfoMsg(){ CubeCMS.Mgs.clear('info'); }
+function clearErrMsg(){ CubeCMS.Mgs.clear('error'); }
+function errMsg(msg, clear){ CubeCMS.Mgs.err(msg, clear); }
+function showLoadBox(box, timeout){ CubeCMS.Loader.showLoadBox(box); }
+function hideLoadBox(){ CubeCMS.Loader.hideLoadBox(); }
+function vveLoadImage(src, callback){ CubeCMS.Images.loadImage(src, callback); }
 
 function initToolboxEvents(){
    /**
@@ -164,41 +347,7 @@ function initToolboxEvents(){
    });
 }
 
-function str2url(str,encoding,ucfirst)
-{
-	str = str.toUpperCase();
-	str = str.toLowerCase();
-
-	str = str.replace(/[\u0105\u0104\u00E0\u00E1\u00E2\u00E3\u00E4\u00E5]/g,'a');
-	str = str.replace(/[\u00E7\u010D\u0107\u0106]/g,'c');
-	str = str.replace(/[\u010F]/g,'d');
-	str = str.replace(/[\u00E8\u00E9\u00EA\u00EB\u011B\u0119\u0118]/g,'e');
-	str = str.replace(/[\u00EC\u00ED\u00EE\u00EF]/g,'i');
-	str = str.replace(/[\u0142\u0141]/g,'l');
-	str = str.replace(/[\u00F1\u0148]/g,'n');
-	str = str.replace(/[\u00F2\u00F3\u00F4\u00F5\u00F6\u00F8\u00D3]/g,'o');
-	str = str.replace(/[\u0159]/g,'r');
-	str = str.replace(/[\u015B\u015A\u0161]/g,'s');
-	str = str.replace(/[\u00DF]/g,'ss');
-	str = str.replace(/[\u0165]/g,'t');
-	str = str.replace(/[\u00F9\u00FA\u00FB\u00FC\u016F]/g,'u');
-	str = str.replace(/[\u00FD\u00FF]/g,'y');
-	str = str.replace(/[\u017C\u017A\u017B\u0179\u017E]/g,'z');
-	str = str.replace(/[\u00E6]/g,'ae');
-	str = str.replace(/[\u0153]/g,'oe');
-	str = str.replace(/[\u013E\u013A]/g,'l');
-	str = str.replace(/[\u0155]/g,'r');
-
-	str = str.replace(/[^a-z0-9\s\'\:\/\[\]-]/g,'');
-	str = str.replace(/[\s\'\:\/\[\]-]+/g,' ');
-	str = str.replace(/[ ]/g,'-');
-	str = str.replace(/[\/]/g,'-');
-	if (ucfirst == 1) {
-		var c = str.charAt(0);
-		str = c.toUpperCase()+str.slice(1);
-	}
-	return str;
-}
+function str2url(str,encoding,ucfirst){ return CubeCMS.Tools.str2url(str, ucfirst); }
 
 function vveCheckUrlKey(checkerUrl, element, callback, params){
    var $e = $(element); var postParams = {key : $e.val(), lang : $e.attr("lang")};
@@ -220,7 +369,22 @@ function vveCheckUrlKey(checkerUrl, element, callback, params){
    });
 }
 
+function loadPageLang(url, lang) {
+   showLoadBox(contentSelector);
+   $(contentSelector).load(url, function(cnt){
+     var lang = url.match(/(l=[a-z]{2})/g);
+     if(lang != null){
+        $(contentSelector).html(cnt.replace(lang[0], "") );
+     }
+      hideLoadBox();
+      initToolboxEvents();
+      $(document).scroll();
+   });
+};
+
 $(document).ready(function(){
+   CubeCMS.setup(); // init base class when document ready
+
    // when language is changed
    $('.form-link-lang-container a.form-link-lang').live('click',function(event, focus){
       if(typeof focus == 'undefined') focus = true;
@@ -254,7 +418,10 @@ $(document).ready(function(){
       return false;
    });
    // načítání jazykových mutací
-   $('a.toolbox-changelang-button').live('click', function(){
+   $('a.toolbox-changelang-button').live('click', function(e){
+      e.preventDefault();
+      loadPageLang(this.href);
+      
       showLoadBox('.main-content');
       var cntUrl = this.href;
       $('.main-content').load(cntUrl, function(cnt){
@@ -289,7 +456,9 @@ $(document).ready(function(){
 			   if (input.val() == input.attr('placeholder')) {
 				   input.val('');
 			   }
-		   })
+		   });
 	   });
    }
 });
+
+
