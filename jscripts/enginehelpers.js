dbdc24d256b83054ce42bd2155f8d475ce3df39c
Jakub Matas <j.matas@cube-studio.cz>
2013-02-04 07:40:09 +0000

    CoreJS: Opravy jquery pluginu cooki, enginehelpers a prettyPhoto. Nový soubor pro práci s eshopem. shop.js

--- jscripts/enginehelpers.js
+++ jscripts/enginehelpers.js
@@ -1,14 +1,14 @@
 /** NEW USE THIS!!! */
 /**
  * Vytvoření pomocných funkcí pro Cube CMS systém
- * 
+ *
  * Msg - zprávy
  * Dom - dom objekty
  * Tools - různé nástroje
  * Loader - loadery a ajax
  * Toolbox - toolbox a nástroje
  * Toolbox.Tool - toolbox a nástroje ???????????
- * 
+ *
  **/
 var CubeCMS = CubeCMS || {};
 
@@ -18,12 +18,12 @@ CubeCMS = {
    lang : null,
    primaryLang : null,
    domain : null,
-   
+
    baseOpt : {},
    // caled in index header
    init : function(opt){
       $.extend(this, opt);
-      
+
       document.domain = this.domain;
       document.lang = this.lang;
       document.primaryLang = this.primaryLang ;
@@ -37,14 +37,14 @@ CubeCMS = {
 
 // msg class
 CubeCMS.Msg = {
-   infoBox : null, 
-   errBox : null, 
-   
+   infoBox : null,
+   errBox : null,
+
    init : function(){
       this.infoBox = $('#infoMessages');
       this.errBox = $('#errMessages');
    },
-   
+
    /**
     *  Zobrazí info hlášku
     */
@@ -98,9 +98,9 @@ CubeCMS.Msg = {
       } else if(type == "err" || type == "error"){
          this.errBox.html(null).hide();
       }
-      
+
       return this;
-   }   
+   }
 };
 
 CubeCMS.Tools = {
@@ -141,7 +141,7 @@ CubeCMS.Tools = {
       }
       return str;
    },
-   
+
    /**
     * Metoda odstraní html tagy
     */
@@ -152,7 +152,7 @@ CubeCMS.Tools = {
    },
    /**
     * metoda provede kontrolu url klíče nad zadanou url adresou
-    */ 
+    */
    checkUrlKey : function(checkerUrl, elem, callback){
       var $e = $(element);
       $.ajax({
@@ -160,9 +160,9 @@ CubeCMS.Tools = {
          data: {key : $e.val(), lang : $e.attr("lang")},
          success: function(data){
             if( typeof data.urlkey != "undefined"
-            && $.isFunction(callback) )
+               && $.isFunction(callback) )
                callback(data.urlkey);
-            }
+         }
       });
    },
    /**
@@ -170,58 +170,58 @@ CubeCMS.Tools = {
     */
    contrastColor : function(color){
       // Counting the perceptive luminance - human eye favors green color... 
-      var a = 1 - ( 
-         0.299 * parseInt((color).substring(0,2),16) 
-         + 0.587 * parseInt((color).substring(2,4),16) 
-         + 0.114 * parseInt((color).substring(4,6),16) )/255;
+      var a = 1 - (
+         0.299 * parseInt((color).substring(0,2),16)
+            + 0.587 * parseInt((color).substring(2,4),16)
+            + 0.114 * parseInt((color).substring(4,6),16) )/255;
       if (a < 0.5){
          return "#000000"; // bright colors - black font
       } else{
          return "#ffffff"; // dark colors - white font
-      }  
+      }
    }
 };
 
 CubeCMS.ToolBox = {
    init : function() {
-      
+
    }
 };
 
 CubeCMS.ToolBox.Tool = {
-   
+
 };
 
 CubeCMS.Images = {
-      slideShowFade : function(selector, delay){
-         $(selector+' .image:gt(0)').hide();
-         setInterval(function(){ $(selector+' .image:first').fadeOut().next('.image').fadeIn().end().appendTo(selector);}, delay); 
-      },
-      loadImage : function(src, callback){
-         var cacheImage = document.createElement('img');
-         //set the onload method before the src is called otherwise will fail to be called in IE
-         cacheImage.onload = function(){
-            if ($.isFunction(callback)) {
-               callback.call(cacheImage);
-            }
-         };
-         cacheImage.src = src;
-      }
+   slideShowFade : function(selector, delay){
+      $(selector+' .image:gt(0)').hide();
+      setInterval(function(){ $(selector+' .image:first').fadeOut().next('.image').fadeIn().end().appendTo(selector);}, delay);
+   },
+   loadImage : function(src, callback){
+      var cacheImage = document.createElement('img');
+      //set the onload method before the src is called otherwise will fail to be called in IE
+      cacheImage.onload = function(){
+         if ($.isFunction(callback)) {
+            callback.call(cacheImage);
+         }
+      };
+      cacheImage.src = src;
+   }
 };
 
 CubeCMS.Loader = {
    timeout : 10000,
    loaderCnt : '<div id="loadingBox" class="loadingBox"><div class="loader-w"><span class="loader-text">{MSG}</span></div>',
    loadBoxClass : "loadingBox",
-   
+
    init : function(){
-      
+
    },
-   
+
    loadUrl : function(box, url){
-      
+
    },
-   
+
    showLoadBox : function(box, msg){
       if(typeof(msg) == "undefined" ){
          msg = "loading...";
@@ -229,18 +229,18 @@ CubeCMS.Loader = {
       var rId = 'loadingBox-'+ (Math.floor( Math.random(0)*10000 ) );
       var parentBox = $(box).parent('div,p');
       var overBox = $(this.loaderCnt.replace('{MSG}', msg))
-      .addClass(this.loadBoxClass)
-      .attr('id', rId)
-      .css({
-         position : 'absolute',
-         top : 0,
-         left : 0,
-         opacity : 0,
-         width : parentBox.width()+parseInt(parentBox.css('padding-left'))+parseInt(parentBox.css('padding-right'))
-         +parseInt(parentBox.css('margin-left'))+parseInt(parentBox.css('margin-right')),
-         height : parentBox.height()+parseInt(parentBox.css('padding-top'))+parseInt(parentBox.css('padding-bottom'))
-         +parseInt(parentBox.css('margin-top'))+parseInt(parentBox.css('margin-bottom'))
-      });
+         .addClass(this.loadBoxClass)
+         .attr('id', rId)
+         .css({
+            position : 'absolute',
+            top : 0,
+            left : 0,
+            opacity : 0,
+            width : parentBox.width()+parseInt(parentBox.css('padding-left'))+parseInt(parentBox.css('padding-right'))
+               +parseInt(parentBox.css('margin-left'))+parseInt(parentBox.css('margin-right')),
+            height : parentBox.height()+parseInt(parentBox.css('padding-top'))+parseInt(parentBox.css('padding-bottom'))
+               +parseInt(parentBox.css('margin-top'))+parseInt(parentBox.css('margin-bottom'))
+         });
       if(parentBox.css('position') != "absolute" && parentBox.css('position') != "relative"){
          parentBox.css('position', 'relative');
       }
@@ -248,6 +248,7 @@ CubeCMS.Loader = {
       overBox.fadeTo(500, 1);
       var _loader = this;
       setTimeout(function() { _loader.hideLoadBox(rId); }, this.timeout); // zavření
+      return rId;
    },
    hideLoadBox : function(id){
       if(typeof(id) == "undefined"){
@@ -260,17 +261,51 @@ CubeCMS.Loader = {
    }
 };
 
+CubeCMS.Form = {
+   addRow : function(button){
+      var $original = $(button).parent();
+      var $new = $original.clone(true);
+      var $input = $new.find('input');
+      $input.attr('id', $input.attr('id')+Math.floor((Math.random()*10)+1))
+         .attr('name', $input.attr('name').replace(/\[.*\]/, '[]'))
+         .val("");
+      $original.removeClass('form-input-multiple-last').find('a.button-add-multiple-line').hide();
+      $original.after($new);
+
+      if($original.parent().find('.form-input-multiple').length > 1){
+         $original.parent().find('a.button-remove-multiple-line').show();
+      }
+      return false;
+   },
+   removeRow : function(button){
+      var $row = $(button).parent();
+      var $container = $row.parent();
+      if($row.hasClass('form-input-multiple-last')){
+         // add prev class
+         $row.prev().addClass('form-input-multiple-last').find('a.button-add-multiple-line').show();
+         // show prev add button
+         $row.prev().addClass('form-input-multiple-last');
+      }
+      $row.remove();
+
+      if($container.find('.form-input-multiple').length == 1){
+         $container.find('a.button-remove-multiple-line').hide();
+      }
+      return false;
+   }
+}
+
 //base vars
 if(typeof(contentSelector) === "undefined"){
    var contentSelector = CubeCMS.contentSelector;
 }
 
 /* DEPRECATED */
-function vveShowMessages(dataObj){ CubeCMS.Mgs.show(dataObj);}
-function infoMsg(msg, clear){ CubeCMS.Mgs.info(msg, clear); }
-function clearInfoMsg(){ CubeCMS.Mgs.clear('info'); }
-function clearErrMsg(){ CubeCMS.Mgs.clear('error'); }
-function errMsg(msg, clear){ CubeCMS.Mgs.err(msg, clear); }
+function vveShowMessages(dataObj){ CubeCMS.Msg.show(dataObj);}
+function infoMsg(msg, clear){ CubeCMS.Msg.info(msg, clear); }
+function clearInfoMsg(){ CubeCMS.Msg.clear('info'); }
+function clearErrMsg(){ CubeCMS.Msg.clear('error'); }
+function errMsg(msg, clear){ CubeCMS.Msg.err(msg, clear); }
 function showLoadBox(box, timeout){ CubeCMS.Loader.showLoadBox(box); }
 function hideLoadBox(){ CubeCMS.Loader.hideLoadBox(); }
 function vveLoadImage(src, callback){ CubeCMS.Images.loadImage(src, callback); }
@@ -282,7 +317,7 @@ function initToolboxEvents(){
    var toolboxButtons = $('a.toolbox-button');
    // remove all previous toolboxes
    $('div.toolbox-temp').remove();
-   
+
    $("div.toolbox").parent('div').css({position: 'relative'});
    $(".toolbox-tool").hover(function(){
       $(this).addClass('ui-state-highlight');
@@ -291,16 +326,16 @@ function initToolboxEvents(){
       $(this).removeClass('ui-state-highlight');
       $(this).find('input').removeClass('ui-state-highlight');
    });
-   
+
    /* events */
    toolboxButtons.hover(function(){
          $(this).css({'z-index': 3});
          $(this).next('div.toolbox').trigger('showToolbox', [$(this)]);
-      }, 
+      },
       function(){
          $(this).css({'z-index': 10000});
-   });
-   
+      });
+
    $("div.toolbox").bind('showToolbox', function(event, $button){
       // podklad pro editaci
       $button.parent().addClass('toolbox-active-content');
@@ -319,19 +354,19 @@ function initToolboxEvents(){
       } else {
          $toolbox = $('#'+toolId);
       }
-      
+
       $toolbox.css({
-         opacity     : 1, 
+         opacity     : 1,
          top         : $button.offset().top-2,
          left        : $button.offset().left-$toolbox.width()+22,
          width       : $toolbox.width(),
          'z-index'   : 10000
       }).show();
    });
-   
+
    // move toolbox with document when scrolling
    $(document).unbind('scroll');
-   
+
    if(typeof document.toolboxTop == "undefined"){
       document.toolboxTop = 30;
    }
@@ -369,7 +404,7 @@ function vveCheckUrlKey(checkerUrl, element, callback, params){
       success: function(data){
          if( (callback || callback != '' ) && (typeof callback !== 'undefined') ) {
             if(typeof data.urlkey !== "undefined" ){
-               callback(data.urlkey);   
+               callback(data.urlkey);
             } else {
                callback(null);
             }
@@ -381,10 +416,10 @@ function vveCheckUrlKey(checkerUrl, element, callback, params){
 function loadPageLang(url, lang) {
    showLoadBox(contentSelector);
    $(contentSelector).load(url, function(cnt){
-     var lang = url.match(/(l=[a-z]{2})/g);
-     if(lang != null){
-        $(contentSelector).html(cnt.replace(lang[0], "") );
-     }
+      var lang = url.match(/(l=[a-z]{2})/g);
+      if(lang != null){
+         $(contentSelector).html(cnt.replace(lang[0], "") );
+      }
       hideLoadBox();
       initToolboxEvents();
       $(document).scroll();
@@ -430,43 +465,43 @@ $(document).ready(function(){
    $('a.toolbox-changelang-button').live('click', function(e){
       e.preventDefault();
       loadPageLang(this.href);
-      
+
       showLoadBox('.main-content');
       var cntUrl = this.href;
       $('.main-content').load(cntUrl, function(cnt){
-    	  var lang = cntUrl.match(/(l=[a-z]{2})/g);
-    	  if(lang != null){
-    	     $('.main-content').html(cnt.replace(lang[0], "") );
-    	  }
+         var lang = cntUrl.match(/(l=[a-z]{2})/g);
+         if(lang != null){
+            $('.main-content').html(cnt.replace(lang[0], "") );
+         }
          hideLoadBox();
          initToolboxEvents();
          $(document).scroll();
       });
       return false;
-   }); 
+   });
    // placeholder html5 ie
    if ( $.browser.msie ) {
-	   $('[placeholder]').focus(function() {
-		   var input = $(this);
-		   if (input.val() == input.attr('placeholder')) {
-			   input.val('');
-			   input.removeClass('placeholder');
-		   }
-	   }).blur(function() {
-		   var input = $(this);
-		   if (input.val() == '' || input.val() == input.attr('placeholder')) {
-			   input.addClass('placeholder');
-			   input.val(input.attr('placeholder'));
-		   }
-	   }).blur();
-	   $('[placeholder]').parents('form').submit(function() {
-		   $(this).find('[placeholder]').each(function() {
-			   var input = $(this);
-			   if (input.val() == input.attr('placeholder')) {
-				   input.val('');
-			   }
-		   });
-	   });
+      $('[placeholder]').focus(function() {
+         var input = $(this);
+         if (input.val() == input.attr('placeholder')) {
+            input.val('');
+            input.removeClass('placeholder');
+         }
+      }).blur(function() {
+            var input = $(this);
+            if (input.val() == '' || input.val() == input.attr('placeholder')) {
+               input.addClass('placeholder');
+               input.val(input.attr('placeholder'));
+            }
+         }).blur();
+      $('[placeholder]').parents('form').submit(function() {
+         $(this).find('[placeholder]').each(function() {
+            var input = $(this);
+            if (input.val() == input.attr('placeholder')) {
+               input.val('');
+            }
+         });
+      });
    }
 });
 
---
+++ jscripts/shop.js
@@ -0,0 +1,62 @@
+CubeCMS.Shop = {
+   combinations : new Array(),
+   currency : '',
+   roundDecimals : 0,
+   decimals : 0,
+   product : {
+      code: null,
+      price: 0,
+      tax: 0,
+      weight: 0,
+      qty: 0,
+      unit: 'ks'
+   },
+   setProduct : function (product){
+      this.product = $.extend(this.product, product);
+   },
+
+   addCombination : function(id, varArray, priceAdd, weightAdd, codesArr, qty){
+      var comb = { id : id, variants : varArray, price : priceAdd, weight : weightAdd, codes : codesArr, qty : qty };
+      this.combinations.push(comb);
+   },
+
+   getProductPrice: function(price, tax){
+      return this.roundPrice(price*(1+tax), 10);
+   },
+
+   roundPrice : function(price, decimals){
+      if(typeof (decimals) == "undefined"){
+         decimals = 1;
+      }
+      decimals = decimals*10;
+      return decimals == 0 ? Math.round(price) : Math.round(price*10*decimals)/(10*decimals);
+   },
+   getProductCode : function(variantCodes){
+      var code = this.product.code;
+      $.each(variantCodes, function(id, c){
+         var replacement = "{"+id+"}";
+         console.log(replacement, c, code.replace(replacement, c));
+         code = code.replace(replacement, c);
+      });
+      return code;
+   },
+   getFormattedPrice : function(price){
+      price = parseFloat(price);
+      // zaokrouhlení
+      if(this.roundDecimals > 0){
+         price = Math.round(price * 10 * this.roundDecimals) / (10 * this.roundDecimals);
+      } else {
+         price = Math.round(price);
+      }
+      // úprava desetiných míst
+      return price.toFixed(this.decimals).toString().replace('.', ',') + " " + CubeCMS.Shop.currency;
+   }
+}
+
+// extend string for replace
+String.prototype.format = function() {
+   var args = arguments;
+   return this.replace(/{(\d+)}/g, function(match, number) {
+      return typeof args[number] != 'undefined' ? args[number] : match;
+   });
+};
