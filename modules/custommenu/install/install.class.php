28d8b3bb23153533ac4947e108b679bc821b46ca
Jakub Matas <j.matas@cube-studio.cz>
2013-02-04 07:54:13 +0000

    M_CustomMenu: Oprava instalace modulu

--- modules/custommenu/install/install.class.php
+++ modules/custommenu/install/install.class.php
@@ -2,7 +2,7 @@
 class CustomMenu_Install extends Module_Install {
    
    public function install() {
-//      $this->runSQLCommand($this->replaceDBPrefix($this->getSQLFileContent('install.sql')));
+      $this->runSQLCommand($this->replaceDBPrefix($this->getSQLFileContent('install.sql')));
    }
 }
 
