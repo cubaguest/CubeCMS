6665c0751016475823409673e1f70a8e8071caac
Jakub Matas <j.matas@cube-studio.cz>
2013-05-29 09:08:09 +0000

    Core: Oprava přepsaného systémového modulu pro generování rss kanálů

---
+++ lib/module/rss/module_rss.class.php
@@ -0,0 +1,74 @@
+<?php
+
+/**
+ * Třída Core Modulu pro obsluhu mapy stránek
+ *
+ * @copyright  	Copyright (c) 2008-2009 Jakub Matas
+ * @version    	$Id: $ VVE 6.2 $Revision: $
+ * @author        $Author: $ $Date: $
+ *                $LastChangedBy: $ $LastChangedDate: $
+ * @abstract 		Třída pro obsluhu mapy stránek
+ */
+class Module_Rss extends Module_Core {
+   private $links = arraY();
+   public function runController() {
+      switch (AppCore::getUrlRequest()->getOutputType()) {
+         case 'xml':
+            $this->links;
+            $model = new Model_Category();
+            $cats = $model->withRights()->where(Model_Category::COLUMN_FEEDS." = 1", array())->records();
+            $rssComp = new Component_Feed();
+            foreach ($cats as $cat) {
+               if ($cat->{Model_Category::COLUMN_FEEDS} != true) continue;
+               // načtení a kontrola cest u modulu
+               $routesClassName = ucfirst($cat->{Model_Category::COLUMN_MODULE}).'_Routes';
+               if(!class_exists($routesClassName)) {
+                  throw new BadClassException(sprintf($this->tr("Nepodařilo se načíst třídu cest (routes) modulu \"%s\"."),
+                     $cat->{Model_Category::COLUMN_MODULE}), 10);
+               }
+               //	Vytvoření objektu s cestama modulu
+               $routes = new $routesClassName(null);
+               $rssClassName = ucfirst($cat->{Model_Category::COLUMN_MODULE}).'_Rss';
+               $rssCore = new $rssClassName(new Category_Core($cat->{Model_Category::COLUMN_URLKEY},
+                  false, $cat), $routes);
+               $rssCore->setRssComp($rssComp);
+               $rssCore->runController();
+            }
+            $rssComp->setConfig('link', $this->link());
+            $rssComp->setConfig('title', null);
+            $rssComp->flush();
+            break;
+         default:
+            $model = new Model_Category();
+            $cats = $model->withRights()->where(Model_Category::COLUMN_FEEDS." = 1", array())->records();
+            $link = new Url_Link(true);
+            $link->clear(true);
+            foreach ($cats as $cat) {
+               if ($cat[Model_Category::COLUMN_FEEDS] != true) continue;
+               array_push($this->links, array(
+                  'name' => (string) $cat->{Model_Category::COLUMN_CAT_LABEL},
+                  'link' => (string) $link->category((string) $cat->{Model_Category::COLUMN_URLKEY})
+                     ->file(Url_Request::URL_FILE_RSS)
+               ));
+            }
+            asort($this->links);
+            break;
+      }
+   }
+
+   public function runView() {
+      $this->template()->setPVar('CURRENT_CATEGORY_PATH', array($this->tr('Přehled rss zdrojů')));
+      $this->template()->addTplFile('rss.phtml');
+      $this->template()->sources = $this->links;
+
+      $link = new Url_Link(true);
+      $this->template()->sourcesAll = $link->clear(true)->file(Url_Request::URL_FILE_RSS);
+   }
+
+   public function runXmlView() {
+   }
+
+   public function runTxtView() {
+      AppCore::setErrorPage(true);
+   }
+}
--- lib/module/rss/module_rss_category.class.php
+++ lib/module/rss/module_rss_category.class.php
@@ -1,15 +1,15 @@
 <?php
 /**
- * Třída kategorií pro automatický spouštěč
+ * Třída kategorií mvýpisu rss kanálů
  *
  * @copyright  	Copyright (c) 2008-2009 Jakub Matas
  * @version    	$Id: category.class.php 1390 2010-08-09 06:40:37Z jakub $ VVE3.9.4 $Revision: 1390 $
  * @author        $Author: jakub $ $Date: 2010-08-09 08:40:37 +0200 (Po, 09 srp 2010) $
  *                $LastChangedBy: jakub $ $LastChangedDate: 2010-08-09 08:40:37 +0200 (Po, 09 srp 2010) $
- * @abstract 		Třída pro vytvoření kategorie automatického spouštěče
+ * @abstract 		Třída pro vytvoření kategorie rss kanálů
  */
 
-class Module_AutoRun_Category extends Category_Core {
+class Module_Rss_Category extends Category_Core {
    /**
     * Konstruktor načte informace o kategorii
     * @string $catKey --  klíč kategorie
@@ -24,14 +24,17 @@ class Module_AutoRun_Category extends Category_Core {
       // Je třeba více?
       $category->{Model_Category::COLUMN_CAT_ID} = 0;
       $category->{Model_Rights::COLUMN_RIGHT} = 'r--';
-      $category->{Model_Category::COLUMN_MODULE} = 'autorun';
-      $category->{Model_Category::COLUMN_URLKEY} = 'autorun.php';
-      $category->{Model_Category::COLUMN_CAT_LABEL} = $this->tr('Plánované úlohy');
+      $category->{Model_Category::COLUMN_MODULE} = 'rss';
+      $category->{Model_Category::COLUMN_URLKEY} = 'rss.html';
+      $category->{Model_Category::COLUMN_CAT_LABEL} = $this->tr('Přehled rss kanálů');
       $category->{Model_Category::COLUMN_INDIVIDUAL_PANELS} = false;
-      $category->{Model_Category::COLUMN_DESCRIPTION} = $this->tr('Spouštěč plánovaných úloh');
+      $category->{Model_Category::COLUMN_DESCRIPTION} = $this->tr('Kompletní rss kanálů vyvedených ze stránek');
       $category->{Model_Category::COLUMN_KEYWORDS} = null;
-      $category->{Model_Category::COLUMN_ICON} = null;
+      if(defined('VVE_CM_RSS_CAT_ICON')){
+         $category->{Model_Category::COLUMN_ICON} = VVE_CM_RSS_CAT_ICON;
+      } else {
+         $category->{Model_Category::COLUMN_ICON} = 'rsslist.png';
+      }
       return $category;
    }
 }
-?>
