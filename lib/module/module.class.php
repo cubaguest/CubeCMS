ce8eba2a85d210277e55e1d987104925508c816b
Jakub Matas <j.matas@cube-studio.cz>
2013-10-11 12:14:37 +0000

    Core: Přidání podpory pro načítání šablon modulu

--- lib/module/module.class.php
+++ lib/module/module.class.php
@@ -11,11 +11,13 @@
 
 class Module
 {
+   const TPLS_LIST_FILE = 'tpls.php';
+
    protected $name = null;
    protected $params = null;
    protected $dataDir = null;
 
-   protected $version = null;
+   protected $version = '1.0.0';
    protected $depModules = array();
 
    protected $author = null;
@@ -23,21 +25,38 @@ class Module
 
    protected $coreModule = false;  // vypíná aktualizace, protože ty jsou obsaženy v aktualizaci jádra
 
+
+   protected $customTemplates = false;
+   /**
+    * Pole s šablonama
+    * @var array
+    */
+   protected $templatesMain = array();
+   /**
+    * Pole s šablonama
+    * @var array
+    */
+   protected $templatesPanel = array();
+   /**
+    * Pole s parametry šablon
+    * @var array
+    */
+   protected $templatesParams = array();
+
+   /**
+    * @param $name
+    * @param array $params
+    * @param null $version - verze instalovaného modulu
+    */
    public function  __construct($name, $params = array(), $version = null)
    {
       $this->name = $name;
       $this->params = $params;
       $this->dataDir = $name;
-
-      if($version == null){
-         if(is_file($this->getModuleDir().'docs'.DIRECTORY_SEPARATOR.'version.txt')){
-            $version = file_get_contents($this->getModuleDir().'docs'.DIRECTORY_SEPARATOR.'version.txt').'.0';
-         }
-         if($version == null){
-            $this->install();
-         }
-      }
       $this->checkVersion($version);
+      if($this->customTemplates){
+         $this->loadTemplates();
+      }
    }
 
    public function __toString()
@@ -92,9 +111,9 @@ class Module
     * @return string
     * @todo -- ověřit vytváření adresáře
     */
-   public function getDataDir($webAddres = false)
+   public function getDataDir($webAddress = false)
    {
-      if ($webAddres) {
+      if ($webAddress) {
          return Url_Request::getBaseWebDir() . VVE_DATA_DIR . URL_SEPARATOR . $this->dataDir . URL_SEPARATOR;
       } else {
          $dir = new Filesystem_Dir(AppCore::getAppWebDir() . VVE_DATA_DIR . DIRECTORY_SEPARATOR . $this->dataDir . DIRECTORY_SEPARATOR);
@@ -112,6 +131,15 @@ class Module
       return AppCore::getAppLibDir() . AppCore::MODULES_DIR . DIRECTORY_SEPARATOR . $this->getName() . DIRECTORY_SEPARATOR;
    }
 
+   /**
+    * Metoda vrací adresář modulu (ve face)
+    * @return string -- cesta do modulu
+    */
+   public function getFaceDir()
+   {
+      return Face::getCurrent()->getDir() . AppCore::MODULES_DIR . DIRECTORY_SEPARATOR . $this->getName() . DIRECTORY_SEPARATOR;
+   }
+
    public function getVersion()
    {
       return $this->version;
@@ -119,8 +147,11 @@ class Module
 
    protected function checkVersion($currentVersion)
    {
-      if(version_compare($this->version, $currentVersion) == 1){
-//         Debug::log('Update module: '.$this->getName()." from: ".$currentVersion." to: ".$this->version);
+      if($currentVersion == null){
+         if(!Model_Module::isInstalled($this->getName())){
+            $this->install();
+         }
+      } else if(version_compare($this->version, $currentVersion) == 1){
          $this->update($currentVersion);
       }
    }
@@ -144,7 +175,6 @@ class Module
       if (is_file($installDir.$phpFile)) {
          include $installDir.$phpFile;
       }
-
       // register module
       $model = new Model_Module();
       $model->registerInstaledModule($this->name, $this->version);
@@ -153,8 +183,10 @@ class Module
    /**
     * Metoda pro instalaci závislostí
     */
-   protected function installDependentModules() {
+   protected function installDependentModules()
+   {
       foreach ($this->depModules as $module) {
+         Debug::log($module, Model_Module::isInstalled($module));
          if(!Model_Module::isInstalled($module)){
             $mClass = ucfirst($module) . '_Module';
             if(!class_exists($mClass)){
@@ -225,7 +257,8 @@ class Module
       }
    }
 
-   protected function replaceDBPrefix($cnt) {
+   protected function replaceDBPrefix($cnt)
+   {
       return str_replace(Install_Core::SQL_TABLE_PREFIX_REPLACEMENT, VVE_DB_PREFIX, $cnt);
    }
 
@@ -233,9 +266,111 @@ class Module
     * Metoda pro instalaci SQL patchů
     * @param string $SQL -- SQL patch
     */
-   protected function runSQLCommand($SQL) {
+   protected function runSQLCommand($SQL)
+   {
       $model = new Model_DbSupport();
       $model->runSQL($SQL);
+   }
+
+   /**
+    * Metoda provede načtení šablon a jejich parametrů
+    */
+   protected function loadTemplates()
+   {
+      $someLoaded = false;
+      $class = get_class($this);
+      while ($class != 'Module') {
+         $module = strtolower( substr($class, 0, -7));
+         // zkus existenci souborů
+         // soubor z face
+         if(is_file(Template::faceDir().AppCore::MODULES_DIR.DIRECTORY_SEPARATOR
+            .$module.DIRECTORY_SEPARATOR.Template::TEMPLATES_DIR.DIRECTORY_SEPARATOR.self::TPLS_LIST_FILE)){
+
+            include Template::faceDir().AppCore::MODULES_DIR.DIRECTORY_SEPARATOR
+               .$module.DIRECTORY_SEPARATOR.Template::TEMPLATES_DIR.DIRECTORY_SEPARATOR.self::TPLS_LIST_FILE;
+            $someLoaded = true;
+            break;
+         }
+         // soubor z hlavního vzhledu
+         else if(is_file(Template::faceDir(true).AppCore::MODULES_DIR.DIRECTORY_SEPARATOR
+            .$module.DIRECTORY_SEPARATOR.Template::TEMPLATES_DIR.DIRECTORY_SEPARATOR.self::TPLS_LIST_FILE)){
+
+            include Template::faceDir(true).AppCore::MODULES_DIR.DIRECTORY_SEPARATOR
+               .$module.DIRECTORY_SEPARATOR.Template::TEMPLATES_DIR.DIRECTORY_SEPARATOR.self::TPLS_LIST_FILE;
+            $someLoaded = true;
+            break;
+         }
+            // soubor z modulu
+         else if(is_file(AppCore::getAppLibDir().AppCore::MODULES_DIR.DIRECTORY_SEPARATOR.$module
+            .DIRECTORY_SEPARATOR.Template::TEMPLATES_DIR.DIRECTORY_SEPARATOR.self::TPLS_LIST_FILE)){
+
+            include AppCore::getAppLibDir().AppCore::MODULES_DIR.DIRECTORY_SEPARATOR.$module
+                  .DIRECTORY_SEPARATOR.Template::TEMPLATES_DIR.DIRECTORY_SEPARATOR.self::TPLS_LIST_FILE;
+            $someLoaded = true;
+            break;
+         }
+         // načti rodiče
+         $class = get_parent_class($class);
+      }
+      Debug::log($someLoaded);
+      if($someLoaded){
+         // přeřazení proměnných pro kompatibilitu
+         if(isset($this->main) && $this->main != null){
+             $this->templatesMain = $this->main;
+         }
+         if(isset($this->panel) && $this->panel != null){
+             $this->templatesPanel = $this->panel;
+         }
+         $tpls = $this->templatesMain;
+         $this->templatesMain = array();
+         foreach($tpls as $tpl => $labels){
+            $this->templatesMain[$tpl] = $labels[Locales::getLang()];
+         }
+         $tpls = $this->templatesPanel;
+         $this->templatesPanel = array();
+         foreach($tpls as $tpl => $labels){
+            $this->templatesPanel[$tpl] = $labels[Locales::getLang()];
+         }
+      }
+
+   }
+
+   /**
+    * Metoda vrací šablony modulu
+    * @return array
+    */
+   public function getTemplates()
+   {
+      if($this->templatesMain == false) {
+         $this->loadTemplates();
+      }
+      return $this->templatesMain;
+   }
+
+   /**
+    * Metoda vrací šablony modulu pro panel
+    * @return array
+    */
+   public function getPanelTemplates()
+   {
+      if($this->templatesPanel == false) {
+         $this->loadTemplates();
+      }
+      return $this->templatesPanel;
+   }
+
+   /**
+    * Metoda vrací parametr šablony
+    * @param $tpl
+    * @param $param
+    * @return mixed
+    */
+   public function getTemplateParam($tpl, $param, $default = null)
+   {
+      if(isset($this->templatesParams[$tpl]) && isset($this->templatesParams[$tpl][$param])){
+         return $this->templatesParams[$tpl][$param];
+      }
+      return $default;
    }
 
    /**
