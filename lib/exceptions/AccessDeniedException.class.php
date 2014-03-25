6f401f4a35e62301b90516f97b4e80242049c7ce
Jakub Matas <j.matas@cube-studio.cz>
2014-03-25 17:43:49 +0000

    Core: Přidání vyjímky pro nautorizovaný přístup.

---
+++ lib/exceptions/AccessDeniedException.class.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Třída AccessDeniedException pro zamezení přístupu klienta do sekce.
+ *
+ * @copyright  	Copyright (c) 2008-2010 Jakub Matas
+ * @version    	$Id:  $ VVE 7.18 $Revision: $
+ * @author        $Author: $ $Date: $
+ * @author        cuba
+ *                $LastChangedBy: $ $LastChangedDate: $
+ * @abstract 		Třída 
+ */
+
+class AccessDeniedException extends Exception {}
