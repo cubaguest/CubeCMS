7c7ff5e62c8beff9cd22b9bd2b7ca0f913e8d427
Jakub Matas <j.matas@cube-studio.cz>
2014-04-02 06:52:50 +0000

    M_Contact: Doplnění německého překladu

---
+++ modules/contact/locale/de_DE.php
@@ -0,0 +1,28 @@
+<?php
+$singular = 
+array (
+  'Úprava kontaktu' => 'To edit a contact',
+  'Mapa jak se k nám dostanete' => 'Map how to reach us',
+  'Mapa' => 'Karte',
+  'Napište nám' => 'Schreibe uns',
+  'Více' => 'Mehr',
+  'Jméno a přijmení' => 'Kontaktname',
+  'Váš e-mail' => 'Email-Adresse',
+  'Téma' => 'Thema',
+  '< Vlastní předmět >' => '< Das eigentliche Thema >',
+  'Předmět' => 'Betreff',
+  'Text' => 'Text',
+  'Odeslat' => 'Senden',
+  'Musí být zadán předmět zprávy.' => 'Der Betreff muss eingegeben werden.',
+  'Váš dotaz byl úspěšně odeslán. Co nejdříve Vám odpovíme.' => 'Your request has been sent successfully. As soon as you respond.',
+  'Text kontaktu' => 'Contact Text',
+  'Text panelu' => 'Text panel',
+  'Pokud není vyplněn, zkusí se použít první odstavec typu adresa z hlavního textu.' => 'If not filled, try to use the first paragraph of the type of address from the main text.',
+  'Změny byly zrušeny' => 'The changes were abolished',
+  'Text kontaktu byl uložen' => 'Text contact was deposited',
+  'Upravit kontakt' => 'Edit Contact',
+);
+$plural = 
+array (
+);
+?>
