0bc3c4043a90396e5bc6b0379f110315d2326671
Jakub Matas <j.matas@cube-studio.cz>
2014-04-02 07:02:27 +0000

    M_Redirect: Přidání podpory pro volbu kategori ze systému pro přesměrování.

--- modules/redirect/controller.class.php
+++ modules/redirect/controller.class.php
@@ -5,6 +5,9 @@
 
 class Redirect_Controller extends Controller {
    const DEFAULT_RESIRECT_CODE = 301;
+   
+   private $categoriesArray = array();
+   
 /**
  * Kontroler pro zobrazení textu
  */
@@ -12,7 +15,11 @@ class Redirect_Controller extends Controller {
       //		Kontrola práv
       $this->checkReadableRights();
       $url = $this->category()->getParam('url', Url_Link::getMainWebDir());
-      if(strpos($url, "http") !== 0){
+      $category = Model_Category::getRecord((int)$this->category()->getParam('idcat', 0));
+
+      if($category && !$category->isNew()){
+         $url = $this->link(true)->category($category->{Model_Category::COLUMN_URLKEY});
+      } else if(strpos($url, "http") !== 0){
          $url = Url_Link::getMainWebDir(). ( strpos($url,'/') === 0 ? substr($url, 1) : $url );
       }
       if($url != null){
@@ -33,9 +40,21 @@ class Redirect_Controller extends Controller {
     * @param Form $form Metoda pro nastavení
     */
    protected function settings(&$settings, Form &$form) {
+      $elemCats = new Form_Element_Select('category', $this->tr('Kategorie'));
+      $this->createArray(Category_Structure::getStructure(Category_Structure::ALL));
+      $elemCats->addOption('URL adresa níže', 0);
+      foreach ($this->categoriesArray as $id => $name) {
+         $elemCats->addOption($name, $id);
+      }
+      $form->addElement($elemCats,'basic');
+      
+      if(isset($settings['idcat'])) {
+         $form->category->setValues($settings['idcat']);
+      }
+      
       $elemURL = new Form_Element_Text('url', 'Adresa pro přesměrování');
       $elemURL->setSubLabel($this->tr('Zadává se celá url adresa nebo jenom struktura (např. aktuality/aktualita/)'));
-      $elemURL->addValidation(new Form_Validator_NotEmpty());
+//      $elemURL->addValidation(new Form_Validator_NotEmpty());
       $form->addElement($elemURL,'basic');
 
       if(isset($settings['url'])) {
@@ -52,8 +71,19 @@ class Redirect_Controller extends Controller {
 
       // znovu protože mohl být už jednou validován bez těchto hodnot
       if($form->isValid()) {
+         $settings['idcat'] = $form->category->getValues();
          $settings['url'] = $form->url->getValues();
          $settings['code'] = $form->code->getValues();
+      }
+   }
+   
+   private function createArray(Category_Structure $struct, $level = 0)
+   {
+      foreach ($struct as $item) {
+         $this->categoriesArray[$item->getId()] = str_repeat('|…', $level).($level != 0 ? ' ' : null).$item->getCatObj()->getName().' '.$item->getCatObj()->getName(true);
+         if(!$item->isEmpty()){
+            $this->createArray($item, $level+1);
+         }
       }
    }
 }
