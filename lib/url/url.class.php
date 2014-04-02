eca38c6fcc10329c6c566971f5b82e4132e04090
Jakub Matas <j.matas@cube-studio.cz>
2014-04-02 07:43:14 +0000

    Core: Orpava použití výchozí url pokud je předaná url prázdná

--- lib/url/url.class.php
+++ lib/url/url.class.php
@@ -88,7 +88,18 @@ class Url
 
    public function __construct($url = null)
    {
-      $this->parseUrl($url != null ? $url : $_SERVER['HTTP_REFERER']);
+      $this->parseUrl($url != null ? $url : $this->getCurrentUrl());
+   }
+   
+   protected function getCurrentUrl()
+   {
+      $ssl = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') ? true:false;
+      $sp = strtolower($_SERVER['SERVER_PROTOCOL']);
+      $protocol = substr($sp, 0, strpos($sp, '/')) . (($ssl) ? 's' : '');
+      $port = $_SERVER['SERVER_PORT'];
+      $port = ((!$ssl && $port=='80') || ($ssl && $port=='443')) ? '' : ':'.$port;
+      $host = isset($_SERVER['HTTP_X_FORWARDED_HOST']) ? $_SERVER['HTTP_X_FORWARDED_HOST'] : isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : $_SERVER['SERVER_NAME'];
+      return $protocol . '://' . $host . $port . $_SERVER['REQUEST_URI'];
    }
 
    /**
