a22a3be8c76bb988852d54e7b17dcceee1efdf7f
Jakub Matas <j.matas@cube-studio.cz>
2014-06-02 14:21:18 +0000

    Core: Orpava instalačního SQL dotazu. Doplnění updatu délky řetězce uživatelského jména.

--- lib/install/upgrade/sql/8.0.3.sql
+++ lib/install/upgrade/sql/8.0.3.sql
@@ -1,4 +1,7 @@
+/* UPDATE_MAIN_SITE */
 -- doplnění callback funkcé pro konfigurace
 UPDATE `cubecms_global_config` SET `values`='cs;en;de;ru;sk;au;us;da;es;pl;lv;is;sl;et;lt;hu;sv;fr', `callback_func`='Install_Core::updateInstalledLangs' WHERE `key` = 'APP_LANGS';
 UPDATE `cubecms_global_config` SET `values`='cs;en;de;ru;sk;au;us;da;es;pl;lv;is;sl;et;lt;hu;sv;fr' WHERE `key` = 'DEFAULT_APP_LANG';
-
+/* END_UPDATE */
+-- delší uživatelská jména
+ALTER TABLE `{PREFIX}users` CHANGE `username` `username` VARCHAR(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT 'Uzivatelske jmeno';
