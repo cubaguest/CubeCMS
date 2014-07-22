73d50b3cfc95fecd2f422c089ec7ff1045a479ac
Jakub Matas <j.matas@cube-studio.cz>
2014-07-22 06:24:43 +0000

    Core: Implementace vertikálního dekorátoru formuláře

---
+++ lib/form/decorator/form_decorator_vertical.class.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * Třída dekorátoru formuláře pro vertikální zobrazení
+ *
+ * @copyright  	Copyright (c) 2014 Jakub Matas
+ * @version    	$Id: $ VVE 8.0.3 $Revision: $
+ * @author        $Author: $ $Date: $
+ *                $LastChangedBy: $ $LastChangedDate: $
+ * @abstract      Třída dekorátoru pro formulář
+ */
+class Form_Decorator_Vertical extends Form_Decorator implements Form_Decorator_Interface {
+
+   /**
+    * Renderuje ovládací prvek
+    * @param Html_Element $param
+    */
+   public function createForm(Form $form)
+   {
+      $html = clone $form->html();
+      $html
+          ->addClass('form-vertical')
+          ->setAttrib('role', 'form');
+      // kontrolní prvky
+      $pHtml = new Html_Element('div', $form->elementCheckForm->control());
+      if($form->protectForm && $form->elementToken instanceof Form_Element_Token){
+         $pHtml->addContent((string)$form->elementToken->controll());
+      }
+      $pHtml->addClass('inline');
+      $html->addContent($pHtml);
+      return $html;
+   }
+   
+}
