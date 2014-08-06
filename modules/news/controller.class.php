d915461c020fc095a9ed51f366f6ddd192753f1a
Jakub Matas <j.matas@cube-studio.cz>
2014-08-06 05:54:41 +0000

    M_News: Oprava parametru dědičnostin

--- modules/news/controller.class.php
+++ modules/news/controller.class.php
@@ -1,6 +1,6 @@
 <?php
 class News_Controller extends Articles_Controller {
-   public function showController() {
+   public function showController($urlkey) {
       $this->setOption('deleteMsg', $this->_('Novinka byla smazána'));
       $this->setOption('publicMsg', $this->_('Novinka byla zveřejněna'));
       if(parent::showController() === false) return false;
