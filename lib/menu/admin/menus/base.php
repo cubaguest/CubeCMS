054d41dc23b7bed15fb3de00863d5c0f767f5b18
Jakub Matas <j.matas@cube-studio.cz>
2014-08-28 06:28:13 +0000

    Core: Oprava názvu pro slideshow

--- lib/menu/admin/menus/base.php
+++ lib/menu/admin/menus/base.php
@@ -93,7 +93,7 @@ $this->addItem(Menu_Admin::SECTION_CONTENT, new Menu_Admin_Item(
    'content/forms', 'templates', 'application_form.png'
 ));
 $this->addItem(Menu_Admin::SECTION_CONTENT, new Menu_Admin_Item(
-    32794, array( 'cs' => 'SlideShow', 'en' => 'SlideShow'),
+    32794, array( 'cs' => 'Banner na úvodní stránce', 'en' => 'Homepage SlideShow'),
    'content/slideshow', 'hpslideshow', 'images.png'
 ));
 $this->addItem(Menu_Admin::SECTION_CONTENT, new Menu_Admin_Item(
