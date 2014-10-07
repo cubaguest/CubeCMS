085879cfd4469346e17b1895c406f2afe2b3a15c
Jakub Matas <j.matas@cube-studio.cz>
2014-10-07 08:43:32 +0000

    M_CustomMenu: implementace detekce povolené kategorie pro daný jazyk

--- modules/custommenu/view.class.php
+++ modules/custommenu/view.class.php
@@ -17,13 +17,17 @@ class CustomMenu_View extends View {
          return null;
       }
 
-      $items = $model
+      $model
          ->joinFK(CustomMenu_Model_Items::COLUMN_ID_CATEGORY)
          ->where(CustomMenu_Model_Items::COLUMN_BOX." = :box AND ".CustomMenu_Model_Items::COLUMN_ACTIVE." = 1",
             array('box' => $name))
-         ->order(CustomMenu_Model_Items::COLUMN_ORDER)
-         ->records();
-
+         ->order(CustomMenu_Model_Items::COLUMN_ORDER);
+      if(!Auth::isAdmin()){
+         $model->where(" AND ".Model_Category::COLUMN_DISABLE.'_'.Locales::getLang(). " = 0", array(), true);
+      }
+      
+      $items = $model->records();
+    
       if(empty($items)){
          return false;
       }
