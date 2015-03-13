2f1d7bdf7fe4703cd75051af1d52bc51d08685ae
Jakub Matas <j.matas@cube-studio.cz>
2015-03-13 13:35:10 +0000

    Core: Práce na streamu pro tpl šablony

--- lib/template/stream/template_stream_tpl.class.php
+++ lib/template/stream/template_stream_tpl.class.php
@@ -5,34 +5,31 @@ class Template_Stream_Tpl {
    private $varname;
    public $context;
 
+   protected $vars = array(
+   );
+   
    public function stream_open($path, $mode, $options, &$opened_path)
    {
+      echo 'Soubor: '.$path."<br />";
       $url = parse_url($path);
-      var_dump($path, $mode, $options, $opened_path, $url);
-      flush();
+//      var_dump($path, $mode, $options, $opened_path, $url);
+//      flush();
       return true;
    }
 
-//   public function stream_read($count) {
-//      return 0;
-//   }
-//   public function stream_write($data){
-//      return false;
-//   }
-//
-//   public function stream_tell() {
-//      return 0;
-//   }
-//   
-//   public function stream_eof() {
-//      return true;
-//   }
-//   
-//   public function stream_stat() {
-//      return array();
-//   }
-//   
-//   public function stream_seek($offset, $whence) {
-//      return null;
-//   }
+   public function stream_read($count) {
+      extract($this->vars);
+      include AppCore::getAppLibDir().'templates'.DIRECTORY_SEPARATOR."test.phtml";
+      echo '================================<br /><br /><br /><br />';
+      return false;
+   }
+
+   public function stream_eof() {
+      return true;
+   }
+   
+   public function stream_stat() {
+      return array();
+   }
+
 }
