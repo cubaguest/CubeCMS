eea479561f53ed0a406eed2db227e5b92983dac4
Jakub Matas <j.matas@cube-studio.cz>
2015-10-13 11:29:01 +0200

    Import prvních ruských překladů

---
+++ locale/ru_RU.php
@@ -0,0 +1,14 @@
+<?php
+$singular = 
+array(
+    // global
+   'c0114e757d557a1374123f0667963009' => 'наложить', /* Uložit */ 
+   '008c916501b97c38e6850e4d45573ca2' => 'Oтменить', /* Zrušit */ 
+   '4c8eba4e6078aa71ceff1911a9149a2e' => 'например: info@domain.com', /* např: info@domena.cz */ 
+   '59f3d9b9f07f1d0de98c1d59190aef3c' => 'Пункт "%s" не правильный адрес электронной почты', /* Položka "%s" není korektní emailová adresa */ 
+   '9069a7fce402b96e71207aff960b86b0' => 'Ошибка:', /* Chyba: */ 
+   '3ef7091b2a003f3be759ef90a878a930' => 'Не завершена обязательное пункт "%s"', /* Nebyla vyplněna povinná položka "%s" */ 
+   '3f235d4b6eb4afaf28a61842a2ef0220' => 'Проверьте, не была завершена должным образом', /* Kontrola nebyla správně vyplněna */ 
+);
+$plural = array(
+);
