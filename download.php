44e4d615b9cd4df96f4dc1901983ef36d8cb16a4
Jakub Matas <j.matas@cube-studio.cz>
2015-12-01 13:33:23 +0100

    Oprava skriptu pro stahování souborů

--- download.php
+++ download.php
@@ -4,11 +4,14 @@
  */
 
 $url= urldecode($_GET["url"]);
-$file= urldecode($_GET["file"]);
+if(isset($_GET['file'])){
+   $file= urldecode($_GET["file"]);
+} else {
+   $file = basename($url);
+}
 
 header("Content-Description: File Transfer");
 header("Content-Type: application/force-download");
 header("Content-Disposition: attachment; filename=\"$file\"");
 
 readfile ($url."/".$file);
-?>
