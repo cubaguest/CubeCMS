8d423eb1d33a934c44019b68903806202db5d2c0
Jakub Matas <j.matas@cube-studio.cz>
2016-06-10 11:54:45 +0200

    Core: Přidání gc pro mazání starých souborů.

--- lib/cache/file/cache_file.class.php
+++ lib/cache/file/cache_file.class.php
@@ -1,8 +1,8 @@
 <?php
+
 class Cache_File {
 
    protected $file = null;
-
    protected $expire = 3600;
 
    public function __construct($key, $expire = 3600)
@@ -23,12 +23,15 @@ class Cache_File {
       }
       return false;
    }
-   
+
    public function set($value)
    {
+      if(rand(0, 100) == 50){
+         $this->gc();
+      }
       return file_put_contents($this->file, serialize($value), LOCK_EX);
    }
-   
+
    public function delete()
    {
       if(is_file($this->file)){
@@ -36,4 +39,18 @@ class Cache_File {
       }
       return true;
    }
+
+   protected function gc()
+   {
+      $files = glob(AppCore::getAppCacheDir() . 'cachefiles' . DIRECTORY_SEPARATOR . "*");
+      $now = time();
+      foreach ($files as $file) {
+         if (is_file($file)) {
+            if ($now - filemtime($file) >= 60 * 60 * 24 * 2) { // 2 days
+               unlink($file);
+            }
+         }
+      }
+   }
+
 }
