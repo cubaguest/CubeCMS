0e4279d191d24b19423b668568ccd09c33c8dd45
Jakub Matas <j.matas@cube-studio.cz>
2016-12-09 15:48:31 +0100

    Core: Orpava stahování pouze povolených souborů

--- download.php
+++ download.php
@@ -2,13 +2,23 @@
 /**
  * Soubor pro stazeni zadaneho souboru
  */
-
 $url= urldecode($_GET["url"]);
 if(isset($_GET['file'])){
    $file= urldecode($_GET["file"]);
 } else {
    $file = basename($url);
    $url = str_replace('/'.$file, '', $url);
+}
+
+if(strpos($url, 'lib') !== false 
+    || strpos($url, 'config') !== false 
+    || strpos($url, 'modules')  !== false 
+    || strpos($file, '.php') !== false
+    || strpos($file, '.phtml') !== false
+    || strpos($file, '.htaccess') !== false
+    ){
+   header('HTTP/1.0 403 Forbidden');
+   echo 'Denied';die;
 }
 
 header("Content-Description: File Transfer");
