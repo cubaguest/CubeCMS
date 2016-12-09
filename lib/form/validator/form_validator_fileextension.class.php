d8b7e171214365f6d81e7a47a1921320e7556967
Jakub Matas <j.matas@cube-studio.cz>
2016-12-09 15:45:20 +0100

    Core: Přidání kontroly jestli je soubor pole nebo jenom řetězec. Cleanup.

--- lib/form/validator/form_validator_fileextension.class.php
+++ lib/form/validator/form_validator_fileextension.class.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Validátor přípony nahrávaného souboru
  * 
@@ -11,19 +12,20 @@
  * @todo          Doplnit další formáty přípon
  */
 class Form_Validator_FileExtension extends Form_Validator implements Form_Validator_Interface {
+
    const DOC = 1;
    const IMG = 2;
    const VID = 4;
    const AUD = 8;
    const BIN = 16;
    const ARCH = 32;
-   
    const ALL = 512;
-   
+
    /**
     * Soubory stránek (NENÍ BEZPEČNÉ !!!!)
     */
    const WEB = 1024;
+
    /**
     * Vše (NENÍ BEZPEČNÉ !!!!)
     */
@@ -34,51 +36,50 @@ class Form_Validator_FileExtension extends Form_Validator implements Form_Valida
     * @var array
     */
    private $extensions = array();
-
    private $currentExtType = 0;
-
    protected $notAllowedExtensions = array('php', 'php3', 'php4', 'php5', 'cgi', 'asp');
 
-   public function  __construct($extensions, $errMsg = null) {
-      if(!is_array($extensions)){
-         if(is_int($extensions)){ // konstanty třídy
+   public function __construct($extensions, $errMsg = null)
+   {
+      if (!is_array($extensions)) {
+         if (is_int($extensions)) { // konstanty třídy
             $this->currentExtType = $extensions;
             $cur = $extensions;
             $extensions = array();
 
-            if(($cur-self::WEB) >= 0){
-               $extensions = array_merge($extensions, array("htm","html","xhtml","xml","css","php","js"));
+            if (($cur - self::WEB) >= 0) {
+               $extensions = array_merge($extensions, array("htm", "html", "xhtml", "xml", "css", "php", "js"));
                $cur -= self::WEB;
             }
-            if(($cur-self::ARCH) >= 0){
+            if (($cur - self::ARCH) >= 0) {
                $extensions = array_merge($extensions, array("zip", "rar", "7z", "bz2", "gz"));
                $cur -= self::ARCH;
             }
-            if(($cur-self::BIN) >= 0){
+            if (($cur - self::BIN) >= 0) {
                $extensions = array_merge($extensions, array("exe", "bat", "com", "bin", "sh"));
                $cur -= self::BIN;
             }
-            if(($cur-self::AUD) >= 0){
-               $extensions = array_merge($extensions, array("mp3","wav","wma","flac","ogg"));
+            if (($cur - self::AUD) >= 0) {
+               $extensions = array_merge($extensions, array("mp3", "wav", "wma", "flac", "ogg"));
                $cur -= self::AUD;
             }
-            if(($cur-self::VID) >= 0){
-               $extensions = array_merge($extensions, array("swf","flv","avi","wmv","mov"));
+            if (($cur - self::VID) >= 0) {
+               $extensions = array_merge($extensions, array("swf", "flv", "avi", "wmv", "mov"));
                $cur -= self::VID;
             }
-            if(($cur-self::IMG) >= 0){
-               $extensions = array_merge($extensions, array("swf","jpg","jpeg","gif","png","bmp","wmf"));
+            if (($cur - self::IMG) >= 0) {
+               $extensions = array_merge($extensions, array("swf", "jpg", "jpeg", "gif", "png", "bmp", "wmf"));
                $cur -= self::IMG;
             }
-            if(($cur-self::DOC) >= 0){
+            if (($cur - self::DOC) >= 0) {
                $extensions = array_merge($extensions, array(
-                  "txt", "csv",
-                  "doc", "rtf", "docx", "dotx", "dot",
-                  "xls", "xlt", "xlsx", "xltx",
-                  "ppt",
-                  "pdf",
-                  "odf", "odt", "ott", "ots"
-                  ));
+                   "txt", "csv",
+                   "doc", "rtf", "docx", "dotx", "dot",
+                   "xls", "xlt", "xlsx", "xltx",
+                   "ppt",
+                   "pdf",
+                   "odf", "odt", "ott", "ots"
+               ));
                $cur -= self::DOC;
             }
          } else {
@@ -88,7 +89,7 @@ class Form_Validator_FileExtension extends Form_Validator implements Form_Valida
       }
       $this->extensions = $extensions;
 
-      if($errMsg == null) {
+      if ($errMsg == null) {
          parent::__construct($this->tr('V položce "%s" nebyl odeslán povolený typ souboru'));
       } else {
          parent::__construct($errMsg);
@@ -99,44 +100,39 @@ class Form_Validator_FileExtension extends Form_Validator implements Form_Valida
     * Metoda přidá do elementu prvky z validace
     * @param Form_Element $element -- samotný element
     */
-   public function addHtmlElementParams(Form_Element $element) {
-      if($this->currentExtType == self::ALL){
-
-      } else if(count($this->extensions) > 5){
-         $element->addValidationConditionLabel(sprintf($this->tr("soubor s příponou %s"),
-            implode(', ', array_slice($this->extensions, 0, 5) ).',<span title="'.implode(', ', $this->extensions).'"> ...</span>'));
+   public function addHtmlElementParams(Form_Element $element)
+   {
+      if ($this->currentExtType == self::ALL) {
+         
+      } else if (count($this->extensions) > 5) {
+         $element->addValidationConditionLabel(sprintf($this->tr("soubor s příponou %s"), implode(', ', array_slice($this->extensions, 0, 5)) . ',<span title="' . implode(', ', $this->extensions) . '"> ...</span>'));
       } else {
-         $element->addValidationConditionLabel(sprintf($this->tr("soubor s příponou %s"),
-            implode(', ', $this->extensions)));
+         $element->addValidationConditionLabel(sprintf($this->tr("soubor s příponou %s"), implode(', ', $this->extensions)));
       }
    }
 
-   public function validate(Form_Element $elemObj) {
+   public function validate(Form_Element $elemObj)
+   {
       $values = $elemObj->getUnfilteredValues();
-      if($values == false || empty($values)){
+      if ($values == false || empty($values)) {
          return true;
       }
-      switch (get_class($elemObj)) {
-         // input text
-         case 'Form_Element_File':
-            if($elemObj->isMultiple() OR $elemObj->isMultiLang()) {
-               foreach ($values as $file){
-                  if(!$this->checkExtension($file['name'])) {
-                     $this->errMsg()->addMessage(sprintf($this->errMessage, $elemObj->getLabel()));
-                     $this->isValid = false;
-                     return false;
-                  }
-               }
-            } else {
-               if(!$this->checkExtension($values['name'])){
+      if ($elemObj instanceof Form_Element_File && is_array($values)) {
+         if ($elemObj->isMultiple() OR $elemObj->isMultiLang()) {
+            foreach ($values as $file) {
+               if (!$this->checkExtension($file['name'])) {
                   $this->errMsg()->addMessage(sprintf($this->errMessage, $elemObj->getLabel()));
                   $this->isValid = false;
                   return false;
                }
             }
-            break;
-         default:
-            break;
+         } else {
+            if (!$this->checkExtension($values['name'])) {
+               $this->errMsg()->addMessage(sprintf($this->errMessage, $elemObj->getLabel()));
+               $this->isValid = false;
+               return false;
+            }
+         }
       }
       return true;
    }
@@ -147,11 +143,10 @@ class Form_Validator_FileExtension extends Form_Validator implements Form_Valida
       unset($extensions[0]); // první je název souboru
       // disallow all non secure extension
       foreach ($extensions as $ext) {
-         if($this->currentExtType != self::ALL_NO_SAFE && in_array($ext, $this->notAllowedExtensions)){
+         if ($this->currentExtType != self::ALL_NO_SAFE && in_array($ext, $this->notAllowedExtensions)) {
             return false;
          }
-         if($this->currentExtType != self::ALL && $this->currentExtType != self::ALL_NO_SAFE
-            && !in_array(strtolower($ext), $this->extensions)) {
+         if ($this->currentExtType != self::ALL && $this->currentExtType != self::ALL_NO_SAFE && !in_array(strtolower($ext), $this->extensions)) {
             return false;
          }
       }
