b04bf92b60f2721ee950a6d761f679ff8e9d1257
Jakub Matas <j.matas@cube-studio.cz>
2017-01-04 15:27:32 +0100

    Core: PHP compatibility

--- lib/nonvve/browser/Browser.php
+++ lib/nonvve/browser/Browser.php
@@ -205,7 +205,7 @@
 		
 		const OPERATING_SYSTEM_UNKNOWN = 'unknown';
 
-		public function Browser($useragent="") {
+		public function __construct($useragent="") {
 			$this->reset();
 			if( $useragent != "" ) {
 				$this->setUserAgent($useragent);
