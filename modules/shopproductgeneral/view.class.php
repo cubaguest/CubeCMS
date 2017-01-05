82f4dceeab1e8646a68a504d64813518fa7e6915
Jakub Matas <j.matas@cube-studio.cz>
2017-01-05 11:39:40 +0100

    CoreShop: Oddělení nasatvení produktu, dopočtů a skriptů do samostatných souborů

--- modules/shopproductgeneral/view.class.php
+++ modules/shopproductgeneral/view.class.php
@@ -1,20 +1,20 @@
 <?php
+
 /**
  * Třída pro vytvoření a obsluhu pohledů
  *
  */
-
 class ShopProductGeneral_View extends Shop_Product_View {
-   public function mainView() 
+
+   public function mainView()
    {
       $this->template()->addFile('tpl://list.phtml');
 
-      if($this->category()->getRights()->isWritable()) {
+      if ($this->category()->getRights()->isWritable()) {
          $toolbox = new Template_Toolbox2();
          $toolbox->setIcon(Template_Toolbox2::ICON_WRENCH);
 
-         $toolAdd = new Template_Toolbox2_Tool_PostRedirect('add_product', $this->tr('Přidat produkt'),
-                 $this->link()->route('add'));
+         $toolAdd = new Template_Toolbox2_Tool_PostRedirect('add_product', $this->tr('Přidat produkt'), $this->link()->route('add'));
          $toolAdd->setIcon('page_add.png')->setTitle($this->tr("Přidat nový produkt"));
          $toolbox->addTool($toolAdd);
 
@@ -34,31 +34,27 @@ class ShopProductGeneral_View extends Shop_Product_View {
       }
    }
 
-   public function detailView() 
+   public function detailView()
    {
       $this->template()->addFile('tpl://detail.phtml');
 
-      if($this->category()->getRights()->isWritable()) {
+      if ($this->category()->getRights()->isWritable()) {
          $toolbox = new Template_Toolbox2();
          $toolbox->setIcon(Template_Toolbox2::ICON_WRENCH);
 
-         $toolEdit = new Template_Toolbox2_Tool_PostRedirect('edit_product', $this->tr('Upravit produkt'),
-                 $this->link()->route('edit'));
+         $toolEdit = new Template_Toolbox2_Tool_PostRedirect('edit_product', $this->tr('Upravit produkt'), $this->link()->route('edit'));
          $toolEdit->setIcon('page_edit.png')->setTitle($this->tr("Upravit vybrané zboží"));
          $toolbox->addTool($toolEdit);
-         
-         $toolEditVariants = new Template_Toolbox2_Tool_PostRedirect('edit_product_variants', $this->tr('Upravit varianty'),
-                 $this->link()->route('editVariants'));
+
+         $toolEditVariants = new Template_Toolbox2_Tool_PostRedirect('edit_product_variants', $this->tr('Upravit varianty'), $this->link()->route('editVariants'));
          $toolEditVariants->setIcon('page_attach.png')->setTitle($this->tr("Upravit varianty vybraného produktu"));
          $toolbox->addTool($toolEditVariants);
-         
-         $toolEditImages = new Template_Toolbox2_Tool_PostRedirect('edit_product_images', $this->tr('Upravit obrázky'),
-                 $this->link()->route('editImages'));
+
+         $toolEditImages = new Template_Toolbox2_Tool_PostRedirect('edit_product_images', $this->tr('Upravit obrázky'), $this->link()->route('editImages'));
          $toolEditImages->setIcon(Template_Toolbox2::ICON_IMAGE)->setTitle($this->tr("Upravit obrázky vybraného produktu"));
          $toolbox->addTool($toolEditImages);
-         
-         $toolEditParams = new Template_Toolbox2_Tool_PostRedirect('edit_product_params', $this->tr('Upravit parametry'),
-                 $this->link()->route('editParams'));
+
+         $toolEditParams = new Template_Toolbox2_Tool_PostRedirect('edit_product_params', $this->tr('Upravit parametry'), $this->link()->route('editParams'));
          $toolEditParams->setIcon(Template_Toolbox2::ICON_COG)->setTitle($this->tr("Upravit parametry vybraného produktu"));
          $toolbox->addTool($toolEditParams);
 
@@ -68,7 +64,7 @@ class ShopProductGeneral_View extends Shop_Product_View {
 
          $tooldel = new Template_Toolbox2_Tool_Form($this->formDelete);
          $tooldel->setIcon('page_delete.png')->setTitle($this->tr('Smazat zboží'))
-            ->setConfirmMeassage($this->tr('Opravdu smazat toto zboží?'));
+                 ->setConfirmMeassage($this->tr('Opravdu smazat toto zboží?'));
          $toolbox->addTool($tooldel);
 
          $toolDuplicate = new Template_Toolbox2_Tool_Form($this->formDuplicate);
@@ -79,40 +75,40 @@ class ShopProductGeneral_View extends Shop_Product_View {
       }
       Template_Navigation::addItem($this->product->{Shop_Model_Product::COLUMN_NAME}, $this->link());
    }
-   
-   public function editView() 
+
+   public function editView()
    {
       $this->editProduct(true);
       Template_Navigation::addItem($this->product->{Shop_Model_Product::COLUMN_NAME}, $this->link()->route('detail'));
       Template_Navigation::addItem($this->tr('Úprava'), $this->link());
    }
-   
-   public function editVariantsView() 
+
+   public function editVariantsView()
    {
       $this->editProductVariants();
       Template_Navigation::addItem($this->product->{Shop_Model_Product::COLUMN_NAME}, $this->link()->route('detail'));
       Template_Navigation::addItem($this->tr('Úprava variant'), $this->link());
    }
-   
-   public function editImagesView() 
+
+   public function editImagesView()
    {
       $this->editImages();
       Template_Navigation::addItem($this->product->{Shop_Model_Product::COLUMN_NAME}, $this->link()->route('detail'));
       Template_Navigation::addItem($this->tr('Úprava obrázků'), $this->link());
    }
-   
-   public function editParamsView() 
+
+   public function editParamsView()
    {
       $this->editProductParams();
       Template_Module::setEdit(true);
       Template_Navigation::addItem($this->product->{Shop_Model_Product::COLUMN_NAME}, $this->link()->route('detail'));
       Template_Navigation::addItem($this->tr('Úprava parametrů'), $this->link());
    }
-   
-   public function addView() 
+
+   public function addView()
    {
       $this->editProduct(false);
       Template_Navigation::addItem($this->tr('Přidání produktu'), $this->link());
    }
-   
+
 }
