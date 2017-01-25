6363579e361388b125696c71c954ec53c9da11f5
Jakub Matas <j.matas@cube-studio.cz>
2017-01-25 16:34:02 +0100

    M_ShopProductList: Přepracování systému na použití vzhledu ze seznamu.

--- modules/shopproductlist/view.class.php
+++ modules/shopproductlist/view.class.php
@@ -7,6 +7,7 @@
 class ShopProductList_View extends Shop_Product_View {
 	public function mainView()
    {
-      $this->template()->addFile('tpl://shopproductgeneral:list.phtml');
+//      $this->template()->addFile('tpl://shopproductgeneral:list.phtml');
+      $this->template()->addFile('tpl://main.phtml');
    }
 }
