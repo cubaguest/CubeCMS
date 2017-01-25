3e64a6940dac412a48c96ce7dc2fb959b58f0075
Jakub Matas <j.matas@cube-studio.cz>
2017-01-25 16:34:52 +0100

    M_Text: Oprava SQL instalačního skriptu o chybějící části a změna na INT ze smallint

--- modules/text/install/install.sql
+++ modules/text/install/install.sql
@@ -1,34 +1,47 @@
 CREATE TABLE IF NOT EXISTS `{PREFIX}texts` (
-  `id_text` smallint(4) unsigned NOT NULL AUTO_INCREMENT,
-  `id_item` smallint(5) unsigned NOT NULL,
+  `id_text` int(6) unsigned NOT NULL AUTO_INCREMENT,
+  `id_item` int(6) unsigned NOT NULL,
+  `id_user` int(6) DEFAULT '0',
   `subkey` varchar(30) NOT NULL DEFAULT 'nokey',
+  `changed` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `label_cs` varchar(200) CHARACTER SET utf8 COLLATE utf8_czech_ci DEFAULT NULL,
   `text_cs` mediumtext CHARACTER SET utf8 COLLATE utf8_czech_ci,
   `text_clear_cs` mediumtext CHARACTER SET utf8 COLLATE utf8_czech_ci,
-  `label_sk` varchar(200) CHARACTER SET utf8 COLLATE utf8_slovak_ci DEFAULT NULL,
-  `text_sk` mediumtext CHARACTER SET utf8 COLLATE utf8_slovak_ci,
-  `text_clear_sk` mediumtext CHARACTER SET utf8 COLLATE utf8_slovak_ci,
   `label_en` varchar(200) DEFAULT NULL,
   `text_en` mediumtext,
   `text_clear_en` mediumtext,
   `label_de` varchar(200) DEFAULT NULL,
   `text_de` mediumtext,
   `text_clear_de` mediumtext,
-  `changed` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `label_sk` varchar(1000) CHARACTER SET utf8 COLLATE utf8_slovak_ci DEFAULT NULL,
+  `text_sk` mediumtext CHARACTER SET utf8 COLLATE utf8_slovak_ci,
+  `text_clear_sk` mediumtext CHARACTER SET utf8 COLLATE utf8_slovak_ci,
+  `data` text,
   PRIMARY KEY (`id_text`),
-  UNIQUE KEY `id_article` (`id_item`),
+  KEY `id_item` (`id_item`),
+  KEY `subkey` (`id_item`,`subkey`),
   FULLTEXT KEY `label_cs` (`label_cs`),
   FULLTEXT KEY `label_en` (`label_en`),
   FULLTEXT KEY `label_de` (`label_de`),
+  FULLTEXT KEY `label_sk` (`label_sk`),
   FULLTEXT KEY `text_clear_de` (`text_clear_de`),
   FULLTEXT KEY `text_clear_en` (`text_clear_en`),
-  FULLTEXT KEY `text_clear_cs` (`text_clear_cs`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
+  FULLTEXT KEY `text_clear_cs` (`text_clear_cs`),
+  FULLTEXT KEY `text_clear_sk` (`text_clear_sk`)
+) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `{PREFIX}texts_has_private_users` (
-  `id_user` smallint(6) NOT NULL,
-  `id_text` smallint(6) NOT NULL,
-  PRIMARY KEY (`id_user`,`id_article`),
+  `id_user` int(6) NOT NULL,
+  `id_text` int(6) NOT NULL,
+  PRIMARY KEY (`id_user`,`id_text`),
   KEY `fk_tb_users_id_user` (`id_user`),
+  KEY `fk_tb_texts_id_text` (`id_text`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+CREATE TABLE IF NOT EXISTS `{PREFIX}texts_has_private_groups` (
+  `id_group` int(6) NOT NULL,
+  `id_text` int(6) NOT NULL,
+  PRIMARY KEY (`id_group`,`id_text`),
+  KEY `fk_tb_groups_id_group` (`id_group`),
   KEY `fk_tb_texts_id_text` (`id_text`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
