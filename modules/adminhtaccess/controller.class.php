3c084060b27cb1f009ced40579857a94cd8c417d
Jakub Matas <j.matas@cube-studio.cz>
2017-08-16 12:01:02 +0200

    Orpava htacces základního pravidla

--- modules/adminhtaccess/controller.class.php
+++ modules/adminhtaccess/controller.class.php
@@ -115,7 +115,7 @@ RewriteCond %{REQUEST_FILENAME} -s [OR]
 RewriteCond %{REQUEST_FILENAME} -l [OR]
 RewriteCond %{REQUEST_FILENAME} -d
 RewriteRule ^.*$ - [NC,L]
-RewriteRule ^.*$ /index.php [NC,L]
+RewriteRule ^.*$ index.php [NC,L]
 \n";
       return $str;
    }
