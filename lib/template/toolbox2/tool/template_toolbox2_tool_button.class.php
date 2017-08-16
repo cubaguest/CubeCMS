02c99c093d467c8064c30a9e1bb113ae3899e436
Jakub Matas <j.matas@cube-studio.cz>
2017-08-16 11:59:02 +0200

    Přidání podpory pro data v tlačítku toolboxu

--- lib/template/toolbox2/tool/template_toolbox2_tool_button.class.php
+++ lib/template/toolbox2/tool/template_toolbox2_tool_button.class.php
@@ -10,4 +10,30 @@
  */
 
 class Template_Toolbox2_Tool_Button extends Template_Toolbox2_Tool implements Template_Toolbox2_Tool_Interface {
+   protected $data = array();
+   
+   public function setData($data)
+   {
+      $this->data = $data;
+      return $this;
+   }
+   
+   public function getData()
+   {
+      return $this->data;
+   }
+   
+   public function addData($key, $value)
+   {
+      $this->data[$key] = $value;
+      return $this;
+   }
+   
+   public function removeData($key)
+   {
+      if(isset($this->data[$key])){
+         unset($this->data[$key]);
+      }
+      return $this;
+   }
 }
