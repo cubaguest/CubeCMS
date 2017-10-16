0e38a3dc95dab07a13f0473eefcd2d035db06d87
Jakub Matas <j.matas@cube-studio.cz>
2017-10-16 10:24:58 +0200

    Core: Přepsání generování odkazů na kategorie v TinyMCE pluginu

--- lib/component/tinymce/list/component_tinymce_list_categories.class.php
+++ lib/component/tinymce/list/component_tinymce_list_categories.class.php
@@ -1,36 +1,48 @@
 <?php
+
 /**
  * Třída pro výpis odkazů na kategorie včetně jazykových mutací
  */
 class Component_TinyMCE_List_Categories extends Component_TinyMCE_List {
 
-   protected function  loadItems() {
+   protected function loadItems()
+   {
       $model = new Model_Category();
-      
-      $cats = $model->onlyWithAccess()
-            ->order(array(Model_Category::COLUMN_NAME => Model_ORM::ORDER_ASC))->records();
 
-      $linkObj = new Url_Link(true);
-      foreach($cats as $cat) {
-         
+
+      $struct = Category_Structure::getStructure(Category_Structure::ALL)->getCategoryPaths(' > ', null, function(Category $cat) {
+         return (string)$cat->getLink();
+      });
+
+      foreach ($struct as $link => $catName) {
+         $label = Locales::isMultilang() ? 
+                 sprintf($this->tr('Stránka[%s]: %s'), strtoupper(Locales::getLang()), $catName) 
+                 : sprintf($this->tr('Stránka: %s'), $catName);
+         $this->addItem($label, str_replace(Url_Link::getMainWebDir(), "/", $link));
+      }
+      
+      if(Locales::isMultilang()){
          foreach (Locales::getAppLangs() as $lang) {
-            if($cat[Model_Category::COLUMN_URLKEY][$lang] == null || $cat[Model_Category::COLUMN_NAME][$lang] == null){
+            if($lang == Locales::getLang()){
                continue;
             }
-            
-            $label = Locales::isMultilang() 
-               ? sprintf($this->tr('Stránka[%s]: %s'), strtoupper($lang), $cat[Model_Category::COLUMN_NAME][$lang] ) 
-               : sprintf($this->tr('Stránka: %s'), $cat[Model_Category::COLUMN_NAME][$lang] );
-         
-            $link = (string)$linkObj
-               ->lang($lang != Locales::getDefaultLang() ? $lang : null)
-               ->category($cat[Model_Category::COLUMN_URLKEY][$lang]);
-         
-            $this->addItem($label, 
-               str_replace(Url_Link::getMainWebDir(), "/", $link) );
+
+            $struct = Category_Structure::getStructure(Category_Structure::ALL)->getCategoryPaths(' > ', function(Category $cat) use ($lang) {
+               $data = $cat->getDataObj();
+               return ( isset($data[Model_Category::COLUMN_NAME][$lang]) && $data[Model_Category::COLUMN_NAME][$lang] != null) ? $data[Model_Category::COLUMN_NAME][$lang] : false;
+            }, function(Category $cat) use ($lang) {
+               $linkObj = new Url_Link(true);
+               $data = $cat->getDataObj();
+               if($data[Model_Category::COLUMN_URLKEY][$lang] == null){
+                  return false;
+               }
+               return (string)$linkObj->lang($lang)->category($data[Model_Category::COLUMN_URLKEY][$lang]);
+            });
+            foreach ($struct as $link => $catName) {
+               $label = sprintf($this->tr('Stránka[%s]: %s'), strtoupper($lang), $catName);
+               $this->addItem($label, str_replace(Url_Link::getMainWebDir(), "/", $link));
+            }
          }
-            
       }
    }
-
 }
